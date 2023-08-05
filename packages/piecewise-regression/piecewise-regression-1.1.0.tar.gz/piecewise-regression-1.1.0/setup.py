f8022ce9559f47266a1c5a1bad9ebe2d638b4e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:37 +0000

    Add package piecewise-regression/1.1.0/piecewise-regression-1.1.0.tar.gz

---
+++ packages/piecewise-regression/piecewise-regression-1.1.0.tar.gz/piecewise-regression-1.1.0/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+
+def readme():
+    with open('docs/PIP_DOC.rst') as f:
+        return f.read()
+
+
+# This call to setup() does all the work
+setuptools.setup(
+    name="piecewise-regression",
+    version="1.1.0",
+    description="piecewise (segmented) regression in python",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chasmani/piecewise-regression",
+    author="Charlie Pilgrim",
+    author_email="pilgrimcharlie2@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=["numpy", "matplotlib", "scipy", "statsmodels"],
+)
