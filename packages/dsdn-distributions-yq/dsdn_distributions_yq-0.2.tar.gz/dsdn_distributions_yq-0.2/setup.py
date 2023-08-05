71e2795225e36ced6f9bf91d4ec1e117870b14fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:18 +0000

    Add package dsdn-distributions-yq/0.2/dsdn_distributions_yq-0.2.tar.gz

---
+++ packages/dsdn-distributions-yq/dsdn_distributions_yq-0.2.tar.gz/dsdn_distributions_yq-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsdn_distributions_yq',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsdn_distributions_yq'],
+      zip_safe=False)
