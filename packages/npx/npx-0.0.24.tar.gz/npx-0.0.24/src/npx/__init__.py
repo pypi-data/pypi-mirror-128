35669981414e3d35675e4677498957113fd12985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:42 +0000

    Add package npx/0.0.24/npx-0.0.24.tar.gz

---
+++ packages/npx/npx-0.0.24.tar.gz/npx-0.0.24/src/npx/__init__.py
@@ -0,0 +1,17 @@
+from ._isin import isin_rows
+from ._main import add_at, dot, solve, subtract_at, sum_at
+from ._mean import mean
+from ._unique import unique, unique_rows
+
+__all__ = [
+    "dot",
+    "solve",
+    "sum_at",
+    "add_at",
+    "subtract_at",
+    "unique_rows",
+    "isin_rows",
+    "mean",
+    "unique",
+    "unique_rows",
+]
