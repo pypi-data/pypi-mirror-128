8161cacc54d51ed65937a52b05f72c7b3a410aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:55 +0000

    Add package atomic-operator/0.5.0/atomic_operator-0.5.0-py3-none-any.whl

---
+++ packages/atomic-operator/atomic_operator-0.5.0-py3-none-any.whl/atomic_operator/__main__.py
@@ -0,0 +1,12 @@
+import fire
+from atomic_operator import AtomicOperator
+
+def main():
+    atomic_operator = AtomicOperator()
+    fire.Fire({
+        'run': atomic_operator.run,
+        'get_atomics': atomic_operator.get_atomics
+    })
+
+if __name__ == "__main__":
+    main()
