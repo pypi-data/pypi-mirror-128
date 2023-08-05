cc8946814836f11fa485ab4ce0303643001b9c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:04 +0000

    Add package bioregistry/0.4.15/bioregistry-0.4.15.tar.gz

---
+++ packages/bioregistry/bioregistry-0.4.15.tar.gz/bioregistry-0.4.15/src/bioregistry/schema/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""Data structures."""
+
+from .struct import Author, Collection, Registry, Resource, get_json_schema  # noqa:F401
+from .utils import sanitize_mapping, sanitize_model  # noqa:F401
