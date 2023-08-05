56819f2d3d6944542beaa2653fcd711365c40cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:33 +0000

    Add package ungi-utils/0.1.7/ungi_utils-0.1.7.tar.gz

---
+++ packages/ungi-utils/ungi_utils-0.1.7.tar.gz/ungi_utils-0.1.7/README.md
@@ -0,0 +1,3 @@
+### utils for the UNGI project
+
+Nothing here should be ran as standalone code, to use ungi please use the  standalone "ungi"
---
+++ packages/ungi-utils/ungi_utils-0.1.7.tar.gz/ungi_utils-0.1.7/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pathlib
+from setuptools import setup
+
+CWD = pathlib.Path(__file__).parent
+README = (CWD / "README.md").read_text()
+
+setup(
+    name="ungi_utils",
+    version="0.1.7",
+    description="Utils needed for the UNGI project",
+    long_description=README,
+    author="Unseen Giants",
+    license="GPL 3.0",
+    packages=["ungi_utils"],
+    install_requires=["elasticsearch", "aiohttp", "requests", "bleach"],
+    author_email="incoming+unseen-giants-ungi-utils-26498131-issue-@incoming.gitlab.com",
+    )
