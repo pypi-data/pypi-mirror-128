de9af24e6aa6e267f6e831ac1e592196ecd8d5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:46 +0000

    Add package essmc2/0.0.3/essmc2-0.0.3.tar.gz

---
+++ packages/essmc2/essmc2-0.0.3.tar.gz/essmc2-0.0.3/essmc2/models/networks/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Alibaba Group Holding Limited. All Rights Reserved.
+
+from .train_module import TrainModule
+from .classifier import Classifier, VideoClassifier
+
+__all__ = [
+    'TrainModule',
+    'Classifier',
+    'VideoClassifier'
+]
