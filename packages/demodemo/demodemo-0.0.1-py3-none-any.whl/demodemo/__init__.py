9d13cb50b1621bbe297dfd5ded165d184b3af66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:39 +0000

    Add package demodemo/0.0.1/demodemo-0.0.1-py3-none-any.whl

---
+++ packages/demodemo/demodemo-0.0.1-py3-none-any.whl/demodemo/__init__.py
@@ -0,0 +1,13 @@
+import pyodbc
+import pandas as pd
+
+def get(Server,Database,Query):
+    ser=Server
+    db=Database
+    sql_query=Query
+    conn_string=f"Driver={{SQL Server}} ;Server={ser} ;Database={db} ; Trusted_Connection=yes;"
+    objConn = pyodbc.connect(conn_string)
+    df=pd.read_sql(sql_query, objConn)
+    print(df)
+
+
