b12e4dd505daab755510c659efee0adbafbbf607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:39 +0000

    Add package open-variant/0.2.3/open_variant-0.2.3-py3-none-any.whl

---
+++ packages/open-variant/open_variant-0.2.3-py3-none-any.whl/plugins/liftover/liftover.py
@@ -0,0 +1,9 @@
+#from liftover import get_lifter
+
+
+def liftover(text: str) -> str:
+    #converter = get_lifter('hg19', 'hg38')
+    #chrom = '1'
+    #pos = int(text)
+
+    return text #converter[chrom][pos][0][1]
