4ae496ce4a1f3f0a72b9e348bc8484c68e9b980e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:41 +0000

    Add package dict-tiny/1.1.1/dict-tiny-1.1.1.tar.gz

---
+++ packages/dict-tiny/dict-tiny-1.1.1.tar.gz/dict-tiny-1.1.1/dict_tiny/sources/deepl_trans.py
@@ -0,0 +1,34 @@
+import requests
+from plumbum import colors
+from dict_tiny.setting import DEEPL_TRANS_API_BASE_URL
+
+
+def deepl_trans(text, target_language=None):
+    """
+    :param text:
+    :param target_language:
+    :return:
+    """
+    try:
+        data = {
+            "text": text
+        }
+        if target_language:
+            data.update({"target": target_language})
+        resp = requests.post(DEEPL_TRANS_API_BASE_URL.format("translate"), json=data)
+        resp_json = resp.json()
+        if resp_json["code"] != 200:
+            # print("DeepL error: ", resp_json["msg"])
+            print("DeepL error, code: ", resp_json["code"])
+        else:
+            print(colors.green | ">>> DeepL Translator")
+            res = {
+                "detected language": resp_json["data"]["detected_source_lang"],
+                "input": text,
+                "output": resp_json["data"]["text"]
+            }
+            for k, v in res.items():
+                print("{}: {}".format(k, v))
+    except Exception as e:
+        # print("DeepL error: ", str(e))
+        print("DeepL error.")
