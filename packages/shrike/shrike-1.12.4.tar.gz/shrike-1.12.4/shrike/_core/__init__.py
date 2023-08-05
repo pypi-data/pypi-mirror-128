4a2fd11a3184ad17a36e64a5121039e7a7c82b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:12 +0000

    Add package shrike/1.12.4/shrike-1.12.4.tar.gz

---
+++ packages/shrike/shrike-1.12.4.tar.gz/shrike-1.12.4/shrike/_core/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+Internal utilities for testing, build, pipelines, and logging.
+"""
+
+from .testing import stream_handler  # noqa: F401
