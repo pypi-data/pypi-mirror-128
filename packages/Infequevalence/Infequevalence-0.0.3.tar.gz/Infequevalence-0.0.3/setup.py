475219145d957422502616c804f0fd7cfad7ada0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:15 +0000

    Add package Infequevalence/0.0.3/Infequevalence-0.0.3.tar.gz

---
+++ packages/Infequevalence/Infequevalence-0.0.3.tar.gz/Infequevalence-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'PythonTutorial'
+LONG_DESCRIPTION = 'A STUDY OF EXACT DESIGNS IN CUBIC REGRESSION MODEL  IN PERSPECTIVE OF DE LA GARZA PHENOMENON'
+
+# Setting up
+setup(
+    name="Infequevalence",
+    version=VERSION,
+    author="Developer Rohit",
+    author_email="143rohitbehera@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=['Infequevalence'],
+    install_requires=['numpy'],
+    keywords=['python', 'Information Equevalence', 'DE LA GARZA PHENOMENON', 'EXACT DESIGNS', 'developerrohit'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
