66ff1b93e1e9eab508d31b1d95fec4b165becb67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:53 +0000

    Add package IBSpy/0.3.1/IBSpy-0.3.1.tar.gz

---
+++ packages/IBSpy/IBSpy-0.3.1.tar.gz/IBSpy-0.3.1/IBSPy/__init__.py
@@ -0,0 +1,15 @@
+from .kmer_database import KmerDB, KmerBuilder, FastaChunkReader
+from .kmerGWAS_kmer import KmerGWASDB, KmerGWASDBBuilder
+from .jellyfish_kmer_database import JellyfishSDB, JellyfishBuilder
+from .kmc3_kmer_database import KMC3DB, KMCBuilder
+from .IBSpy_window_count import window_count, parse_arguments
+from .IBSpy_results import IBSpyResults
+from .IBSpy_plot import IBSpyPlots
+import argparse
+
+def main(): 
+	window_count(parse_arguments())
+
+if __name__ == '__main__':
+	main()
+
