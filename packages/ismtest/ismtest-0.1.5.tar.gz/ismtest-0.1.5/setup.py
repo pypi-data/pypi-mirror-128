39155be13640e9e4b03076d9ed15de97aee44228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:39 +0000

    Add package ismtest/0.1.5/ismtest-0.1.5.tar.gz

---
+++ packages/ismtest/ismtest-0.1.5.tar.gz/ismtest-0.1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ismtest',
+    version='0.1.5',    
+    description='A example Python package',
+    author='Ismail',
+    install_requires=['numpy<=1.18.0', "sklearn", "typing"]
+)
