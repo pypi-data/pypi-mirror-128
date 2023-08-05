65bd709b046b2c69c62e91ad35682ab33fb88004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:48 +0000

    Add package autoreduce-run-detection/22.0.0.dev2/autoreduce-run-detection-22.0.0.dev2.tar.gz

---
+++ packages/autoreduce-run-detection/autoreduce-run-detection-22.0.0.dev2.tar.gz/autoreduce-run-detection-22.0.0.dev2/setup.py
@@ -0,0 +1,28 @@
+# ##################################################################################### #
+# ISIS File Polling Repository : https://github.com/ISISSoftwareServices/ISISFilePolling
+#
+# Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
+# ##################################################################################### #
+# pylint:skip-file
+"""
+Setup for the project
+"""
+
+from setuptools import setup
+
+setup_requires = [
+    'autoreduce_utils>=22.0.0.dev7',
+    'filelock',
+    'requests',
+]
+
+setup(name='autoreduce-run-detection',
+      version='22.0.0.dev2',
+      description='A service to discover and ingest new files produced by the ISIS neutron and muon facility',
+      author='ISIS Autoreduction Team',
+      url='https://github.com/ISISScientificComputing/autoreduce-run-detection',
+      install_requires=setup_requires,
+      classifiers=[
+          "Programming Language :: Python :: 3 :: Only",
+      ],
+      entry_points={"console_scripts": ["autoreduce-run-detection = autoreduce_run_detection.run_detection:main"]})
