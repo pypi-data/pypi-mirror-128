779aaf6036e73ce3621ceb792fe01f8ba90df878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:48 +0000

    Add package funfact/0.7a1/funfact-0.7a1.tar.gz

---
+++ packages/funfact/funfact-0.7a1.tar.gz/funfact-0.7a1/funfact/lang/interpreter/_evaluation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import jax.numpy as np
+from ._base import ROOFInterpreter
+from ._einop import _einop
+
+
+class Evaluator(ROOFInterpreter):
+    '''The evaluation interpreter evaluates an initialized tensor expression.
+    '''
+
+    @staticmethod
+    def _binary_operator(reduction, pairwise, lhs, rhs, spec):
+        return _einop(spec, lhs, rhs, reduction, pairwise)
+
+    def literal(self, value, **kwargs):
+        # TODO: need to specialize for each literal type
+        # e.g. scalar, 1, 0, delta
+        return value
+
+    def tensor(self, abstract, data, **kwargs):
+        return data
+
+    def index(self, item, bound, **kwargs):
+        return None
+
+    def indices(self, items, **kwargs):
+        return None
+
+    def index_notation(self, tensor, indices, **kwargs):
+        return tensor
+
+    def call(self, f, x, **kwargs):
+        return getattr(np, f)(x)
+
+    def pow(self, base, exponent, **kwargs):
+        return np.power(base, exponent)
+
+    def neg(self, x, **kwargs):
+        return -x
+
+    def ein(self, lhs, rhs, precedence, reduction, pairwise, outidx, einspec,
+            **kwargs):
+        return self._binary_operator(reduction, pairwise, lhs, rhs, einspec)
---
+++ packages/funfact/funfact-0.7a1.tar.gz/funfact-0.7a1/funfact/lang/test_tsrex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+from ._tsrex import index, indices, tensor
+
+def test_abstract_index():
+    pass
+
+
+    # u = AbstractTensor(np.ones(3))
+    # v = AbstractTensor('v', np.eye(4))
+    # w = AbstractTensor(4, 2, 5)
+    # x = AbstractTensor('x', 9, 2)
+    # assert u.ndim == 1
+    # assert v.ndim == 2
+    # assert w.ndim == 3
+    # assert x.ndim == 2
+    # assert u.shape == (3,)
+    # assert v.shape == (4, 4)
+    # assert w.shape == (4, 2, 5)
+    # assert x.shape == (9, 2)
+    
