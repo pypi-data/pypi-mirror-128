b89f4fb3862e3501bfa720c9c49d8d3f7329a508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:47 +0000

    Add package dapr-ext-grpc/1.4.1/dapr_ext_grpc-1.4.1-py3-none-any.whl

---
+++ packages/dapr-ext-grpc/dapr_ext_grpc-1.4.1-py3-none-any.whl/dapr/ext/grpc/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) Microsoft Corporation and Dapr Contributors.
+Licensed under the MIT License.
+"""
+
+from dapr.clients.grpc._request import InvokeMethodRequest, BindingRequest
+from dapr.clients.grpc._response import InvokeMethodResponse
+
+from dapr.ext.grpc.app import App, Rule   # type:ignore
+
+
+__all__ = [
+    'App',
+    'Rule',
+    'InvokeMethodRequest',
+    'InvokeMethodResponse',
+    'BindingRequest',
+]
