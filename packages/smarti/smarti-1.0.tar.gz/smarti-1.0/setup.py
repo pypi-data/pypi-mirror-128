90a21ec46e099a657082b6b02cbdee42b9ef95e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:12 +0000

    Add package smarti/1.0/smarti-1.0.tar.gz

---
+++ packages/smarti/smarti-1.0.tar.gz/smarti-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="smarti",
+    version="1.0",
+    description="A smart and lightweight dependency injector",
+    author="optzGuitar",
+    url="https://github.com/optzGuitar/smarti",
+    packages=["smarti"],
+    long_description="README.md",
+    long_description_content_type='text/markdown'
+)
