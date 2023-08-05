9f38c09c2d7fd78d7e7b3fd9ceefdb169636aa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:24 +0000

    Add package nowcasting-dataset/3.0.11/nowcasting_dataset-3.0.11-py3-none-any.whl

---
+++ packages/nowcasting-dataset/nowcasting_dataset-3.0.11-py3-none-any.whl/nowcasting_dataset/data_sources/metadata/metadata_model.py
@@ -0,0 +1,11 @@
+""" Model for output of general/metadata data, useful for a batch """
+from nowcasting_dataset.data_sources.datasource_output import DataSourceOutput
+
+
+class Metadata(DataSourceOutput):
+    """Class to store metedata data as a xr.Dataset with some validation"""
+
+    __slots__ = ()
+    _expected_dimensions = ("t0_dt",)
+
+    # TODO: Add validation here - https://github.com/openclimatefix/nowcasting_dataset/issues/233
