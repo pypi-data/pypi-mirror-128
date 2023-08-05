7283f5314e067899b3b8e8c09cde62a7e486b6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:27 +0000

    Add package PythonStrings/0.1.6/PythonStrings-0.1.6.tar.gz

---
+++ packages/PythonStrings/PythonStrings-0.1.6.tar.gz/PythonStrings-0.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='PythonStrings',
+    packages=find_packages(include=['PyString']),
+    version='0.1.6',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    description='A library made to make string easier to use in python',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='test',
+    url='https://github.com/Sas2k/PythonStrings',
+    author='Sasen Perera',
+    author_email='sasen.learnings@gmail.com'
+)
