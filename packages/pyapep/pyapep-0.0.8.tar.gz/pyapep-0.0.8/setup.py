1412f00493371d434c398bd629554c722b3ab2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:41 +0000

    Add package pyapep/0.0.8/pyapep-0.0.8.tar.gz

---
+++ packages/pyapep/pyapep-0.0.8.tar.gz/pyapep-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyapep",
+    version="0.0.8",
+    author="Seongbin Ga",
+    author_email="sebyga@gmail.com",
+    keywords = ['adsorption','PSA','process simulation','breakthrough curve'],
+    description="Python package for adsorption simulations",
+    long_descrption= long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sebyga/pyAPEP",
+    packages=setuptools.find_packages(),
+    license="PNU_Seongbin_Ga",
+    python_requires= ">=3.5",
+    install_requires = ['numpy','scipy']
+)
+
+print(long_description)
+
