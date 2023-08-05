d07a4baa4947cc8a40faee828160e656218f5e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:50 +0000

    Add package pytest-embedded/0.4.4/pytest-embedded-0.4.4.tar.gz

---
+++ packages/pytest-embedded/pytest-embedded-0.4.4.tar.gz/pytest-embedded-0.4.4/pytest_embedded/dut.py
@@ -0,0 +1,47 @@
+import logging
+from typing import Match, Optional
+
+import pexpect
+
+from .app import App
+from .log import PexpectProcess
+from .utils import ProcessContainer
+
+
+class Dut(ProcessContainer):
+    """
+    Device under test (DUT) base class
+    """
+
+    def __init__(self, app: Optional[App] = None, pexpect_proc: Optional[PexpectProcess] = None, **kwargs) -> None:
+        """
+        Args:
+            app: `App` instance
+            pexpect_proc: `PexpectProcess` instance
+        """
+        super().__init__()
+
+        self.app = app
+        self.pexpect_proc = pexpect_proc
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def write(self, *args, **kwargs) -> None:
+        """
+        Write to `pexpect_proc`. All arguments would pass to `pexpect.spawn.write()`
+        """
+        self.pexpect_proc.write(*args, **kwargs)
+
+    def expect(self, *args, **kwargs) -> Match:
+        """
+        Expect from `pexpect_proc`. All arguments would pass to `pexpect.expect()`. Return the current `re.Match`
+        object if matched.
+        """
+        try:
+            self.pexpect_proc.expect(*args, **kwargs)
+        except (pexpect.EOF, pexpect.TIMEOUT):
+            logging.error(f'Not found {args}, {kwargs}')
+            raise
+        else:
+            return self.pexpect_proc.match
