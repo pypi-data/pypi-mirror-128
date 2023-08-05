580d43740c115ba9ba7136299e0b8b84fce42ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:01 +0000

    Add package redisbench-admin/0.5.13/redisbench-admin-0.5.13.tar.gz

---
+++ packages/redisbench-admin/redisbench-admin-0.5.13.tar.gz/redisbench-admin-0.5.13/redisbench_admin/utils/redisgraph_benchmark_go.py
@@ -0,0 +1,48 @@
+#  Apache License Version 2.0
+#
+#  Copyright (c) 2021., Redis Labs Modules
+#  All rights reserved.
+#
+
+from redisbench_admin.utils.remote import (
+    execute_remote_commands,
+)
+
+
+def setup_remote_benchmark_agent(
+    client_public_ip, username, private_key, client_ssh_port
+):
+    commands = [
+        "apt install python3-pip -y",
+        "pip3 install redisbench-admin>=0.5.12",
+        "perf-daemon start",
+    ]
+    execute_remote_commands(
+        client_public_ip, username, private_key, commands, client_ssh_port
+    )
+
+
+def setup_remote_benchmark_tool_redisgraph_benchmark_go(
+    client_public_ip, username, private_key, redisbenchmark_go_link, client_ssh_port
+):
+    commands = [
+        "wget {} -q -O /tmp/redisgraph-benchmark-go".format(redisbenchmark_go_link),
+        "chmod 755 /tmp/redisgraph-benchmark-go",
+    ]
+    execute_remote_commands(
+        client_public_ip, username, private_key, commands, client_ssh_port
+    )
+
+
+def setup_remote_benchmark_tool_ycsb_redisearch(
+    client_public_ip, username, private_key, tool_link, client_ssh_port
+):
+    commands = [
+        "rm -rf /tmp/ycsb*",
+        "wget {} -q -O /tmp/ycsb.tar.gz".format(tool_link),
+        "tar -xvf /tmp/ycsb.tar.gz -C /tmp",
+        "mv /tmp/ycsb-* /tmp/ycsb",
+    ]
+    execute_remote_commands(
+        client_public_ip, username, private_key, commands, client_ssh_port
+    )
