117ffa61b441d4c0df62afbba6ab225daf5aa972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:36 +0000

    Add package Famcy/0.2.45/Famcy-0.2.45-py3-none-any.whl

---
+++ packages/Famcy/Famcy-0.2.45-py3-none-any.whl/Famcy/_elements_/div/div.py
@@ -0,0 +1,21 @@
+import Famcy
+import json
+
+class div(Famcy.FamcyElement):
+    def __init__(self):
+        super(div, self).__init__()
+
+    def render_inner(self):
+        html = "<div" + self.setAttrTag() + ">"
+
+        if self.innerHTML and self.innerHTML != "":
+            html += self.innerHTML
+            self.children = []
+        else:
+            for child in self.children:
+                html += child.render_inner()
+                child.parentElement = self
+
+        self.html = html
+        html += "</div>"
+        return html
