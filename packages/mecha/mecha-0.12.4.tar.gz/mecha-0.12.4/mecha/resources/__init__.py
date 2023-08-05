5db681d1bd392d68b945d41d323b97c1df2a4902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:47 +0000

    Add package mecha/0.12.4/mecha-0.12.4.tar.gz

---
+++ packages/mecha/mecha-0.12.4.tar.gz/mecha-0.12.4/mecha/resources/__init__.py
@@ -0,0 +1,26 @@
+__all__ = [
+    "get_argument_examples",
+    "get_command_examples",
+    "get_multiline_command_examples",
+]
+
+
+import json
+from importlib.resources import read_text
+
+from beet import Function
+from beet.core.utils import JsonDict
+
+
+def get_argument_examples() -> JsonDict:
+    return json.loads(read_text("mecha.resources", "argument_examples.json"))
+
+
+def get_command_examples() -> Function:
+    return Function(read_text("mecha.resources", "command_examples.mcfunction"))
+
+
+def get_multiline_command_examples() -> Function:
+    return Function(
+        read_text("mecha.resources", "multiline_command_examples.mcfunction")
+    )
