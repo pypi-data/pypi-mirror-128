2ec7578db8ba41048fec73a8c02e8d2862c52dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package genbase/0.1.1/genbase-0.1.1-py3-none-any.whl

---
+++ packages/genbase/genbase-0.1.1-py3-none-any.whl/genbase/__init__.py
@@ -0,0 +1,18 @@
+"""Default classes for all to inherit from."""
+
+from genbase.data import import_data, train_test_split
+from genbase.internationalization import (LOCALE_MAP, get_locale, set_locale,
+                                          translate_list, translate_string)
+from genbase.mixin import CaseMixin, SeedMixin
+from genbase.model import from_sklearn
+
+
+class Readable:
+    """Ensure that a class has a readable representation."""
+
+    def __repr__(self):
+        public_vars = ', '.join([f'{k}={v}' for k, v in vars(self).items() if not k.startswith('_')])
+        return f'{self.__class__.__name__}({public_vars})'
+
+
+__version__ = '0.1.1'
