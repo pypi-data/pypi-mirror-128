f949db2b9b853be90e50d839d67acb2cd44a8fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:25 +0000

    Add package wgconf/0.4.0/wgconf-0.4.0.tar.gz

---
+++ packages/wgconf/wgconf-0.4.0.tar.gz/wgconf-0.4.0/src/wgconf/WireGuardConfiguration.py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+from ipaddress import IPv4Interface
+from pathlib import Path
+from shutil import rmtree
+import qrcode as qr
+import ipaddress
+
+
+class WireGuardConfiguration(ABC):
+    def __init__(self, private_key: str, ip_address: IPv4Interface, config_path: Path):
+        self.private_key = private_key
+        self.ip_address = ip_address
+        self.path = config_path
+
+        if not self.path.exists():
+            self.path.mkdir()
+
+    @abstractmethod
+    def make_conf(self):
+        """abstract method implemented in subclasses"""
+        return
+
+    def generate_qr_code(self, filename: str) -> None:
+        """generate qr code from configuration file"""
+        with open(self.path / f"{filename}.conf", "r") as input_data:
+            img = qr.make(input_data.read())
+            img.save(self.path / f"{filename}.png")
+
+    def write_conf_to_file(self, filename: str, conf: str) -> None:
+        """write configuration to file"""
+        with open(self.path / filename, "w") as f:
+            f.write(conf.strip())
+
+
+class ServerConf(WireGuardConfiguration):
+    def __init__(
+        self,
+        private_key: str,
+        ip_address: ipaddress.IPv4Interface,
+        port: int,
+        peers: str,
+        path: Path,
+    ):
+
+        super().__init__(private_key, ip_address, path)
+        self.port = port
+        self.peers = peers
+
+    def make_conf(self) -> str:
+        return (
+            f"[Interface]\n"
+            f"Address = {self.ip_address}\n"
+            f"ListenPort = {self.port}\n"
+            f"PrivateKey = {self.private_key}\n\n"
+            f"{self.peers}"
+        )
+
+
+class ClientConf(WireGuardConfiguration):
+    def __init__(
+        self,
+        private_key: str,
+        ip_address: ipaddress.IPv4Interface,
+        server_public_key: str,
+        allowed_ips: ipaddress.IPv4Network,
+        endpoint: str,
+        port: int,
+        path: Path,
+    ):
+
+        super().__init__(private_key, ip_address, path)
+        self.server_public_key = server_public_key
+        self.allowed_ips = allowed_ips
+        self.endpoint = endpoint
+        self.port = port
+
+    def make_conf(self) -> str:
+        return (
+            f"[Interface]\n"
+            f"Address = {self.ip_address}\n"
+            f"PrivateKey = {self.private_key}\n\n"
+            f"[Peer]\n"
+            f"PublicKey = {self.server_public_key}\n"
+            f"AllowedIPs = {self.allowed_ips}\n"
+            f"Endpoint = {self.endpoint}:{self.port}\n\n"
+        )
---
+++ packages/wgconf/wgconf-0.4.0.tar.gz/wgconf-0.4.0/src/wgconf/prompt.py
@@ -0,0 +1,95 @@
+from InquirerPy import inquirer
+from InquirerPy.validator import NumberValidator
+from ipaddress import IPv4Interface
+from pathlib import Path
+from typing import List
+from datetime import datetime
+from wgconf.utils import (
+    exit_if_no_wg_tools,
+    gen_key_pair,
+    gen_client_keys,
+    create_confs,
+    is_valid_network,
+    has_enough_addresses,
+)
+
+
+def prompts() -> List[str]:
+    num_clients = inquirer.text(
+        message="Enter number of devices:",
+        validate=NumberValidator(),
+        filter=lambda result: int(result),
+    ).execute()
+
+    def network_validator(result) -> bool:
+        """Ensure the input is not empty."""
+        return is_valid_network(result) and has_enough_addresses(result, num_clients)
+
+    network = inquirer.text(
+        message="Enter a network in CIDR Notation",
+        validate=network_validator,
+        filter=lambda result: IPv4Interface(result),
+        invalid_message="Either invalid CIDR or not enough address space. The default of 10.0.0.0/24 should work for most configurations.",
+        default="10.0.0.0/24",
+    ).execute()
+
+    endpoint = inquirer.text(
+        message="Enter an endpoint for your Wireguard server (e.g. an IP address or hostname):",
+    ).execute()
+
+    port = inquirer.text(
+        message="Enter a port for your endpoint. Press Enter for default:",
+        validate=NumberValidator(),
+        filter=lambda result: int(result),
+        default="51820",
+    ).execute()
+
+    allowed_ips = inquirer.text(
+        message="Which IP addresses are allowed to communicate with your client?",
+        default=str(network),
+    ).execute()
+
+    config_path = inquirer.filepath(
+        message="Save path:",
+        default=str(Path(".").resolve() / f"configs_{datetime.today()}"),
+        filter=lambda result: Path(result).expanduser(),
+    ).execute()
+
+    return [num_clients, network, endpoint, port, allowed_ips, config_path]
+
+
+def main():
+    exit_if_no_wg_tools()
+    num_clients, network, endpoint, port, allowed_ips, config_path = prompts()
+
+    server_key_pair = gen_key_pair()
+    client_keys_dict = gen_client_keys(num_clients)
+
+    server_conf, client_confs = create_confs(
+        server_key_pair["private_key"],
+        server_key_pair["public_key"],
+        client_keys_dict,
+        network,
+        endpoint,
+        port,
+        allowed_ips,
+        config_path,
+    )
+
+    server_conf.write_conf_to_file("01_server.conf", server_conf.make_conf())
+    if num_clients <= 20:
+        server_conf.generate_qr_code("01_server")
+
+    for i, conf in enumerate(client_confs, start=1):
+        conf.write_conf_to_file(f"{i + 1:02d}_client_{i}.conf", conf.make_conf())
+        conf.generate_qr_code(f"{i+1:02d}_client_{i}")
+
+    print(
+        f"\nWireguard Server: {conf.endpoint}:{server_conf.port}\n"
+        f"{i} clients\n"
+        f"Configuration files and QR codes stored in {config_path.resolve()}"
+    )
+
+
+if __name__ == "__main__":
+    main()
