99ad02d85ebe848b615bd78042a2cd03549ab0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:36 +0000

    Add package whitehat/1.0/whitehat-1.0.tar.gz

---
+++ packages/whitehat/whitehat-1.0.tar.gz/whitehat-1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        content = req.read()
+        requirements = content.split('\n')
+
+    return requirements
+
+
+#####################################
+VERSION = "1.0"
+#####################################
+
+setup(
+    name='whitehat',
+    version=VERSION,
+    author='LyQuid',
+    author_email='lyquidpersonal@gmail.com',
+    description = 'Python Packages For Ethical Hacker',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    url='https://github.com/EterNomm/Whitehat',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    keywords=["python", "hacker", "whitehat", "hacker tool", "ethical hacker"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Android',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python'
+    ]
+)
