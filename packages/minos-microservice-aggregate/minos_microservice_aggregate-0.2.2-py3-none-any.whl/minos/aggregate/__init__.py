3494ba657ceed2e7cc7a030de18e49337276fdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:42 +0000

    Add package minos-microservice-aggregate/0.2.2/minos_microservice_aggregate-0.2.2-py3-none-any.whl

---
+++ packages/minos-microservice-aggregate/minos_microservice_aggregate-0.2.2-py3-none-any.whl/minos/aggregate/__init__.py
@@ -0,0 +1,69 @@
+__author__ = """Clariteia Devs"""
+__email__ = "devs@clariteia.com"
+__version__ = "0.2.2"
+
+from .events import (
+    SUBMITTING_EVENT_CONTEXT_VAR,
+    EventEntry,
+    EventRepository,
+    InMemoryEventRepository,
+    PostgreSqlEventRepository,
+)
+from .exceptions import (
+    AggregateException,
+    AggregateNotFoundException,
+    DeletedAggregateException,
+    EventRepositoryConflictException,
+    EventRepositoryException,
+    SnapshotRepositoryConflictException,
+    SnapshotRepositoryException,
+    TransactionNotFoundException,
+    TransactionRepositoryConflictException,
+    TransactionRepositoryException,
+    ValueObjectException,
+)
+from .models import (
+    Action,
+    Aggregate,
+    AggregateDiff,
+    AggregateRef,
+    Entity,
+    EntitySet,
+    FieldDiff,
+    FieldDiffContainer,
+    FieldRef,
+    IncrementalFieldDiff,
+    IncrementalSet,
+    IncrementalSetDiff,
+    IncrementalSetDiffEntry,
+    ModelRef,
+    ModelRefExtractor,
+    ModelRefInjector,
+    ModelRefResolver,
+    ValueObject,
+    ValueObjectSet,
+)
+from .queries import (
+    Condition,
+    Ordering,
+)
+from .snapshots import (
+    InMemorySnapshotRepository,
+    PostgreSqlSnapshotQueryBuilder,
+    PostgreSqlSnapshotReader,
+    PostgreSqlSnapshotRepository,
+    PostgreSqlSnapshotSetup,
+    PostgreSqlSnapshotWriter,
+    SnapshotEntry,
+    SnapshotRepository,
+    SnapshotService,
+)
+from .transactions import (
+    TRANSACTION_CONTEXT_VAR,
+    InMemoryTransactionRepository,
+    PostgreSqlTransactionRepository,
+    TransactionEntry,
+    TransactionRepository,
+    TransactionService,
+    TransactionStatus,
+)
---
+++ packages/minos-microservice-aggregate/minos_microservice_aggregate-0.2.2-py3-none-any.whl/minos/aggregate/models/__init__.py
@@ -0,0 +1,33 @@
+from .actions import (
+    Action,
+)
+from .aggregates import (
+    Aggregate,
+)
+from .collections import (
+    IncrementalSet,
+    IncrementalSetDiff,
+    IncrementalSetDiffEntry,
+)
+from .diffs import (
+    AggregateDiff,
+    FieldDiff,
+    FieldDiffContainer,
+    IncrementalFieldDiff,
+)
+from .entities import (
+    Entity,
+    EntitySet,
+)
+from .refs import (
+    AggregateRef,
+    FieldRef,
+    ModelRef,
+    ModelRefExtractor,
+    ModelRefInjector,
+    ModelRefResolver,
+)
+from .value_objects import (
+    ValueObject,
+    ValueObjectSet,
+)
