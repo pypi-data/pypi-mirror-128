d1c8eb13938a7dbca709ff9959857b1b608e6bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package PRESCIENT.nb/0.0.0/PRESCIENT.nb-0.0.0.tar.gz

---
+++ packages/PRESCIENT.nb/PRESCIENT.nb-0.0.0.tar.gz/PRESCIENT.nb-0.0.0/README.md
@@ -0,0 +1,7 @@
+# PRESCIENT.nb
+
+A notebook-based, interactive implementation of PRESCIENT. 
+
+### LARRY Dataset
+
+### Veres 2019
---
+++ packages/PRESCIENT.nb/PRESCIENT.nb-0.0.0.tar.gz/PRESCIENT.nb-0.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import re, os, sys
+
+setup(
+    name="PRESCIENT.nb",
+    version="0.0.0",
+    python_requires=">3.8.0",
+    author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
+    author_email="mvinyard@broadinstitute.org",
+    url="https://github.com/mvinyard/PRESCIENT.nb",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="PRESCIENT.nb - a notebook-based, interactive implementation of PRESCIENT.",
+    packages=[
+        "PRESCIENTnb",
+    ],
+    install_requires=[
+        "prescient"
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)
