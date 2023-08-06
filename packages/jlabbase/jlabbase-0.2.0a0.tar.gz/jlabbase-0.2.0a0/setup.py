a2ee83852cb300d134077f192c4d38b7545b1fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:59 +0000

    Add package jlabbase/0.2.0a0/jlabbase-0.2.0a0.tar.gz

---
+++ packages/jlabbase/jlabbase-0.2.0a0.tar.gz/jlabbase-0.2.0a0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jlabbase']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['jupyterlab', 'numpy', 'pandas']
+
+setup_kwargs = {
+    'name': 'jlabbase',
+    'version': '0.2.0a0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
