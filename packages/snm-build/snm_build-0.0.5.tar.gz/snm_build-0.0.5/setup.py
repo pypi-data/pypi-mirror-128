e3dcf4502bf2cbdd35b904d39ea8e95e74f59104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:37 +0000

    Add package snm-build/0.0.5/snm_build-0.0.5.tar.gz

---
+++ packages/snm-build/snm_build-0.0.5.tar.gz/snm_build-0.0.5/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(name='snm_build',
+                 version='0.0.5',
+                 description='Building and release project',
+                 packages=setuptools.find_packages(),
+                 install_requires=['pyinstaller>=4.5.1'],
+                 author_email='serheos@gmail.com',
+                 zip_safe=False)
