b04d309db5c28d28d8e4332b185a1e196f76e776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:59 +0000

    Add package datels/0.1.0/datels-0.1.0.tar.gz

---
+++ packages/datels/datels-0.1.0.tar.gz/datels-0.1.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+LONG_DESCRIPTION = """
+`datels` is a simple CLI that displays a list of dates line by line.""".strip()
+
+SHORT_DESCRIPTION ="""
+`datels` is a simple CLI that displays a list of dates line by line.""".strip()
+
+VERSION = '0.1.0'
+URL = 'https://github.com/joe-yama/datels'
+
+DEPENDENCIES = []
+TEST_DEPENDENCIES = [
+    'fire==0.4.0',
+    'numpy==1.21.4',
+    'pandas==1.3.4',
+    'python-dateutil==2.8.2',
+    'pytz==2021.3',
+    'six==1.16.0',
+    'termcolor==1.1.0',
+]
+
+setup(
+    name='datels',
+    version=VERSION,
+    description=SHORT_DESCRIPTION,
+    author='joe-yama',
+    
+    packages=['datels'],
+    install_requires=DEPENDENCIES,
+    entry_points={
+        "console_scripts": [
+            "datels=datels.core:main",
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
