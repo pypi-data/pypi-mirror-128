a872484850c18db3d7e276cd1e7fc43927b9154a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:55 +0000

    Add package htmloverpdf/0.0.1/htmloverpdf-0.0.1.tar.gz

---
+++ packages/htmloverpdf/htmloverpdf-0.0.1.tar.gz/htmloverpdf-0.0.1/docs/conf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Sphinx config."""
+
+extensions = ["sphinxcontrib.autoprogram", "sphinx.ext.autodoc"]
+
+project = "Render a HTML overlay over existing PDF files."
+copyright = "2021, Barnaby Shearer"
+author = "Barnaby Shearer"
+
+master_doc = "index"
---
+++ packages/htmloverpdf/htmloverpdf-0.0.1.tar.gz/htmloverpdf-0.0.1/docs/install.rst
@@ -0,0 +1,8 @@
+Installation
+============
+
+::
+
+    sudo apt install python3-gi-cairo gir1.2-poppler-0.18 python3-cairocffi python3-lxml gir1.2-pango-1.0
+    pip install htmloverpdf
+
