c85351d602c9e13a87b9fe5ac8394b9506601db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:19 +0000

    Add package terralabgeoapi/0.0.13/terralabgeoapi-0.0.13-py3-none-any.whl

---
+++ packages/terralabgeoapi/terralabgeoapi-0.0.13-py3-none-any.whl/GeoAPI/Data/_local_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import csv
+from importlib import resources
+import os
+ 
+def load_csv (data_name, as_frame = False):
+    """
+        Load 'data_name' from the local library
+        
+        Parameters 
+        ----------
+        data_name : str
+            Name of csv file to be loaded :
+            'data_pelias.csv'
+            'data.csv'
+            'data2.csv'
+            'id29-clientes-mapeado_consolidado.csv'
+            'out.csv'
+            'Tabela_abreviações.csv'
+
+        as_frame : bool
+            Load the data as a DataFrame.
+            Default : dict
+
+        Returns: 
+        --------
+        data : dict or DataFrame
+            A container with the data
+    """
+    
+    data_path = os.path.abspath("Data/" + data_name)
+    file_ = open(data_path, 'r')
+    data = csv.reader(file_)
+    columns = next(data)
+    values = []
+    for row in data :
+        values.append(row)
+    data_frame = pd.DataFrame(values, columns=columns)
+
+    if as_frame :
+        return data_frame
+    else:
+        dict_ = data_frame.to_dict()
+        return dict_
