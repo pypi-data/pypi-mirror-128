4849c71632c9084cbc023ecfd3fe0abc9f0d6319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:05 +0000

    Add package ramodels/3.13.0/ramodels-3.13.0-py3-none-any.whl

---
+++ packages/ramodels/ramodels-3.13.0-py3-none-any.whl/ramodels/mo/details/__init__.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------------
+from typing import Union
+
+from .address import Address
+from .address import AddressRead
+from .address import AddressWrite
+from .association import Association
+from .association import AssociationBase
+from .association import AssociationRead
+from .association import AssociationWrite
+from .engagement import Engagement
+from .engagement import EngagementAssociation
+from .engagement import EngagementRead
+from .engagement import EngagementWrite
+from .it_system import ITSystemBinding
+from .it_system import ITSystemBindingRead
+from .it_system import ITSystemBindingWrite
+from .kle import KLEBase
+from .kle import KLERead
+from .kle import KLEWrite
+from .leave import Leave
+from .leave import LeaveBase
+from .leave import LeaveRead
+from .leave import LeaveWrite
+from .manager import Manager
+from .manager import ManagerBase
+from .manager import ManagerRead
+from .manager import ManagerWrite
+from .role import Role
+
+# --------------------------------------------------------------------------------------
+# All
+# --------------------------------------------------------------------------------------
+Details = Union[
+    Association, Engagement, EngagementAssociation, Manager, ITSystemBinding, Role
+]
+EmployeeDetails = Union[Details, Address, Leave]
+OrgUnitDetails = Details
+
+__all__ = [
+    "EmployeeDetails",
+    "OrgUnitDetails",
+    "Address",
+    "AddressRead",
+    "AddressWrite",
+    "Association",
+    "AssociationBase",
+    "AssociationRead",
+    "AssociationWrite",
+    "Engagement",
+    "EngagementRead",
+    "EngagementWrite",
+    "EngagementAssociation",
+    "ITSystemBinding",
+    "ITSystemBindingRead",
+    "ITSystemBindingWrite",
+    "KLEBase",
+    "KLERead",
+    "KLEWrite",
+    "Manager",
+    "ManagerBase",
+    "ManagerRead",
+    "ManagerWrite",
+    "Role",
+    "Leave",
+    "LeaveBase",
+    "LeaveRead",
+    "LeaveWrite",
+]
