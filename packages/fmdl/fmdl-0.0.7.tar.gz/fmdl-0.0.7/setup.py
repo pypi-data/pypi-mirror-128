615cb738341990b5971930fbc4ce965b46895bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:33 +0000

    Add package fmdl/0.0.7/fmdl-0.0.7.tar.gz

---
+++ packages/fmdl/fmdl-0.0.7.tar.gz/fmdl-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+  name='fmdl',
+  version='0.0.7',
+  description = "Failure Mode Definition Language Compiler",
+  author = "Michael A. Hicks",
+  author_email = "michael@mahicks.org",
+  url = "https://mahicks.org",
+  scripts=['fmc'],
+  packages=['pytransform'],
+  py_modules=['fmc','csv2fmdlrequirements'],
+  package_data={'pytransform':['platforms/darwin/x86_64/_pytransform.dylib','platforms/windows/x86/_pytransform.dll','platforms/windows/x86_64/_pytransform.dll','platforms/linux/x86/_pytransform.so','platforms/linux/x86_64/_pytransform.so']},
+  license = "Proprietary",
+  python_requires=">3.8,<3.10"
+)
