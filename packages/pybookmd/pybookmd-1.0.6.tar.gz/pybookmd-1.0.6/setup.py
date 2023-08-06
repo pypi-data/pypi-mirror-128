561fb4c0f0596da45d31d9e6bd1157b52b335302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:33 +0000

    Add package pybookmd/1.0.6/pybookmd-1.0.6.tar.gz

---
+++ packages/pybookmd/pybookmd-1.0.6.tar.gz/pybookmd-1.0.6/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pybookmd', 'pybookmd.generators']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0',
+ 'fastcore==1.3.19',
+ 'packaging==20.9',
+ 'pypandoc==1.5',
+ 'pyparsing==2.4.7']
+
+entry_points = \
+{'console_scripts': ['greet = pybookmd.cli:build_book']}
+
+setup_kwargs = {
+    'name': 'pybookmd',
+    'version': '1.0.6',
+    'description': '"Simple book building CLI for markdown based books"',
+    'long_description': None,
+    'author': 'Dylan Kirby',
+    'author_email': 'dylankirbydev@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
