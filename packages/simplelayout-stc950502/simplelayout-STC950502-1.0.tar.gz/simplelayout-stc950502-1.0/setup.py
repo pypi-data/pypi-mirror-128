48956f0698618375f9d5ab35d4bcd6a12abb615c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:54 +0000

    Add package simplelayout-stc950502/1.0/simplelayout-STC950502-1.0.tar.gz

---
+++ packages/simplelayout-stc950502/simplelayout-STC950502-1.0.tar.gz/simplelayout-stc950502-1.0/setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+setuptools.setup(
+      name='simplelayout-stc950502',  # 程序名称
+      version='1.0',  # 程序版本号
+      author='stc',  # 程序作者
+      description='generator',  # 程序描述
+      packages= setuptools.find_namespace_packages(),
+      install_requires=[
+      'simplelayout'],
+      entry_points = {
+        'console_scripts': [
+            'simplelayout = src.simplelayout:main',
+        ]}
+      )
