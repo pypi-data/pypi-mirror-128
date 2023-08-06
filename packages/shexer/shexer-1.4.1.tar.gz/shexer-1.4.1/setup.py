8d640a557cd170fabb2c3eac4f13db98003d29b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:55 +0000

    Add package shexer/1.4.1/shexer-1.4.1.tar.gz

---
+++ packages/shexer/shexer-1.4.1.tar.gz/shexer-1.4.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'shexer',
+  packages = find_packages(exclude=["*.local_code.*"]), # this must be the same as the name above
+  version = '1.4.1',
+  description = 'Automatic schema extraction for RDF graphs',
+  author = 'Daniel Fernandez-Alvarez',
+  author_email = 'danifdezalvarez@gmail.com',
+  url = 'https://github.com/DaniFdezAlvarez/shexer',
+  download_url = 'https://github.com/DaniFdezAlvarez/shexer/archive/1.4.1.tar.gz',
+  keywords = ['testing', 'shexer', 'shexerp3', "rdf", "shex", "shacl", "schema"],
+  classifiers = [],
+  install_requires=[            
+          'Flask',
+          'Flask-Cors',
+		  'rdflib',
+		  'SPARQLWrapper',
+		  'rdflib-jsonld',
+      'wlighter'
+      ],
+)
