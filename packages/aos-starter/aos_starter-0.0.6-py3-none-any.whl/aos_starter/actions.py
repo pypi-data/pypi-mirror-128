8bc8fec12f39b5a3e7434443e71773e8b6b7a705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:49 +0000

    Add package aos-starter/0.0.6/aos_starter-0.0.6-py3-none-any.whl

---
+++ packages/aos-starter/aos_starter-0.0.6-py3-none-any.whl/aos_starter/actions.py
@@ -0,0 +1,13 @@
+from pathlib import PurePath, Path
+
+from aos_keys.actions import new_token_user, UserType
+from aos_prov.actions import create_new_unit, install_vbox_sdk
+
+
+def do_oem_user(domain: str, user_token: str):
+    try:
+        new_token_user(domain, str(PurePath(Path.home() / '.aos' / 'security')), user_token, UserType.OEM.value, False)
+    except Exception:
+        print('User certificate already exists... skipping')
+    install_vbox_sdk()
+    create_new_unit('My first Aos Unit')
