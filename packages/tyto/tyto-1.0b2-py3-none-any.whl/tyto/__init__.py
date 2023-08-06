d4065dd501c1145f6a9f15430c2867b49671324d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:49 +0000

    Add package tyto/1.0b2/tyto-1.0b2-py3-none-any.whl

---
+++ packages/tyto/tyto-1.0b2-py3-none-any.whl/tyto/__init__.py
@@ -0,0 +1,7 @@
+from .tyto import Ontology, URI, configure_cache_size
+from .endpoint import Ontobee, EBIOntologyLookupService
+from .sbo import SBO
+from .so import SO
+from .ncit import NCIT
+from .om import OM
+from .ncbi_taxon import NCBITaxon
