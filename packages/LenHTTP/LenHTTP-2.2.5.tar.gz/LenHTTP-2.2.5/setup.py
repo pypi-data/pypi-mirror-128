5e7ee868b8059e88201f6660286d677615148d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:23 +0000

    Add package LenHTTP/2.2.5/LenHTTP-2.2.5.tar.gz

---
+++ packages/LenHTTP/LenHTTP-2.2.5.tar.gz/LenHTTP-2.2.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+if os.name == "nt":
+	raise RuntimeError("You can't install this package on windows machine!")
+
+setuptools.setup(
+    name="LenHTTP",
+    version="2.2.5",
+    author="lenforiee",
+    author_email="lenforiee@gmail.com",
+    description="An powerful web framework written from scratch!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lenforiee/LenHTTP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
