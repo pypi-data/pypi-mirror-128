1e5305589cafe18e9d91033149890809c924e067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package v2man/0.1.1/v2man-0.1.1.tar.gz

---
+++ packages/v2man/v2man-0.1.1.tar.gz/v2man-0.1.1/v2man/__init__.py
@@ -0,0 +1,6 @@
+from .helper import generate_config
+from .man import Manager, Receiver, Sender
+
+__version__ = "0.1.1"
+
+__all__ = ("Manager", "Receiver", "Sender", "generate_config")
---
+++ packages/v2man/v2man-0.1.1.tar.gz/v2man-0.1.1/v2man/helper.py
@@ -0,0 +1,59 @@
+def generate_config(api_addr: str = "::1", api_port: int = 1118) -> str:
+    """ Generate basic V2ray configuration file with gRPC api enabled"""
+    return """{
+  "inbounds": [{
+      "port": %(port)d,
+      "listen": "%(addr)s",
+      "protocol": "dokodemo-door",
+      "settings": {
+        "address": "127.0.0.1"
+      },
+      "tag": "api"
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {},
+      "tag": "direct"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "0": {
+        "statsUserUplink": true,
+        "statsUserDownlink": true
+      }
+    },
+    "system": {
+      "statsInboundUplink": true,
+      "statsInboundDownlink": true
+    }
+  },
+  "stats": {},
+  "api": {
+    "tag": "api",
+    "services": [
+      "HandlerService",
+      "StatsService"
+    ]
+  },
+  "routing": {
+    "settings": {
+      "rules": [
+        {
+          "inboundTag": [
+            "api"
+          ],
+          "outboundTag": "api",
+          "type": "field"
+        }
+      ]
+    },
+    "strategy": "rules"
+  }
+}
+""" % {
+        "port": api_port,
+        "addr": api_addr
+    }
