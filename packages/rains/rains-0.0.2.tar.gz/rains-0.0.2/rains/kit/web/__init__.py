457809d02c5d18403115901d8f2ace4205c4d213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:26 +0000

    Add package rains/0.0.2/rains-0.0.2.tar.gz

---
+++ packages/rains/rains-0.0.2.tar.gz/rains-0.0.2/rains/kit/web/__init__.py
@@ -0,0 +1,7 @@
+from rains.kit.web.task import WebTask
+from rains.kit.web.core import WebCore
+from rains.kit.web.plant import WebPlant
+from rains.kit.web.plant import BY
+from rains.kit.web.model import WebModel
+
+__all__ = ['WebTask', 'WebCore', 'WebPlant', 'BY', 'WebModel']
