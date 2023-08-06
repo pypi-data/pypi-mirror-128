164c6983dbe8d8cb5ca8baefe16f092470a6dcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:41 +0000

    Add package file-io-utilities/0.0.1.12/file_io_utilities-0.0.1.12-py3-none-any.whl

---
+++ packages/file-io-utilities/file_io_utilities-0.0.1.12-py3-none-any.whl/fileioutilities/filesystem.py
@@ -0,0 +1,20 @@
+import os
+from .file_io import FileIO
+from distutils.dir_util import copy_tree
+
+
+
+class Filesystem(FileIO):
+
+    storage_type = os.path.basename(__file__).split('.py')[0]
+    
+    def __init__(self, storage_type=None):
+        super().__init__()
+
+    def upload(self, local_path, remote_path, overwrite=True):
+        copy_tree(local_path, remote_path)
+        print("Faking upload because it's running locally.")
+
+    def download(self, remote_path, local_path, overwrite=True):
+        copy_tree(remote_path, local_path)
+        print("Faking download because it's running locally.")
