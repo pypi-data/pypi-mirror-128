b87e2fd6dd794a80a82a6e49db623333470231a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:26 +0000

    Add package cicdtest/4.57/cicdtest-4.57-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-4.57-py3-none-any.whl/buildpan/setting.py
@@ -0,0 +1,24 @@
+"""
+    Title: setting.py
+    Author: Kushagra A.
+    Language: Python
+    Date Created: 05-09-2021
+    Date Modified: 14-10-2021
+    Description:
+        ###############################################################
+        ## Create a environment variable in dictionary   ## 
+         ###############################################################
+ """
+
+
+info = {
+    # "HOST" : "34.125.40.10",
+    "HOST": "localhost:5000",
+    "PUSH_COMMIT_URL" : "http://34.125.40.10/push_commit",
+    "FETCH_LOG_URL" : "http://localhost:5000",
+    "GET_SHA_URL" : "http://34.125.40.10/get_sha",
+    "key" : "HB2xRCZSyKYMcTS9rm",
+    "secret_key" : "pQA5FMShe6deqr2b8Eq3GfDBmhEnn77C",
+    "version": "4.57"
+}
+
