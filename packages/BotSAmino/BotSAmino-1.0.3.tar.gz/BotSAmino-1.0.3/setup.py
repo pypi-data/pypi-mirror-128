2a52e8449063dffc09f918474fa62ac647fa85b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:02 +0000

    Add package BotSAmino/1.0.3/BotSAmino-1.0.3.tar.gz

---
+++ packages/BotSAmino/BotSAmino-1.0.3.tar.gz/BotSAmino-1.0.3/setup.py
@@ -0,0 +1,48 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="BotSAmino",
+    version="1.0.3",
+    url="https://github.com/SirLez/BotSAmino",
+    download_url="https://github.com/SirLez/BotSAmino/archive/refs/heads/main.zip",
+    description="Amino Bots with python!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="SirLez",
+    author_email="SirLezDV@gmail.com",
+    license="MIT",
+    keywords=[
+        "aminoapps",
+        "amino-py",
+        "amino",
+        "amino-bot",
+        "narvii",
+        "api",
+        "python",
+        "python3",
+        "python3.x",
+        "ThePhoenix78",
+        "AminoBot",
+        "BotAmino",
+        "botamino",
+        "aminobot",
+        "botaminew",
+        "samino",
+        "SAmino",
+        "SirLez",
+        "sirlez"
+    ],
+    include_package_data=True,
+    install_requires=[
+        "setuptools",
+        "requests",
+        "six",
+        "websocket-client",
+    ],
+    setup_requires=["wheel"],
+    packages=find_packages(),
+)
