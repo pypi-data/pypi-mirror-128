fe26b4fcd28f76874c3a4a47d524bc322d9f36ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:28 +0000

    Add package MDOF-toybox/0.2.2/MDOF-toybox-0.2.2.tar.gz

---
+++ packages/MDOF-toybox/MDOF-toybox-0.2.2.tar.gz/MDOF-toybox-0.2.2/src/toybox/__init__.py
@@ -0,0 +1,9 @@
+'''
+Init.py
+'''
+__version__ = '0.2.2'
+
+from toybox.premade import *
+import toybox.integrators as integrators
+import toybox.nonlinearities as nonlinearities
+import toybox.forcings as forcings
