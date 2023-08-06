8c0551a9d4654ddb6e32eb34d7b87cf05db62716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:07 +0000

    Add package ml4vision-py/0.0.2/ml4vision-py-0.0.2.tar.gz

---
+++ packages/ml4vision-py/ml4vision-py-0.0.2.tar.gz/ml4vision-py-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import re
+from pathlib import Path
+
+import setuptools
+
+with open("README.md", "rb") as f:
+    long_description = f.read().decode("utf-8")
+
+with open(Path(__file__).parent / "ml4vision" / "__init__.py", "r") as f:
+    content = f.read()
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
+
+setuptools.setup(
+    name="ml4vision-py",
+    version=version,
+    author="ml4vision",
+    author_email="info@ml4vision.com",
+    description="Python sdk and cli for ml4vision",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ml4vision/ml4vision-py",
+    setup_requires=["wheel"],
+    install_requires=[
+        "tqdm",
+        "requests"
+    ],
+    packages=[
+        "ml4vision"
+    ],
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License"],
+    python_requires=">=3.6",
+)
