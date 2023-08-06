2a949fd337f7ba1edfc47dbf0dfe9049ee293484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:49 +0000

    Add package jaqpotpy/1.0.16/jaqpotpy-1.0.16.tar.gz

---
+++ packages/jaqpotpy/jaqpotpy-1.0.16.tar.gz/jaqpotpy-1.0.16/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# python setup.py bdist_wheel
+# twine upload dist/*
+
+setup(name='jaqpotpy',
+      version='1.0.16',
+      description='Python client for Jaqpot',
+      url='https://github.com/KinkyDesign/jaqpotpy',
+      author='Pantelis Karatzas',
+      author_email='pantelispanka@gmail.com',
+      license='GNU General Public License v3.0',
+      packages=['jaqpotpy', 'jaqpotpy.api', 'jaqpotpy.mappers'
+                , 'jaqpotpy.entities', 'jaqpotpy.dto'
+                , 'jaqpotpy.helpers', 'jaqpotpy.colorlog', 'jaqpotpy.models'],
+      install_requires=[
+            'pandas', 'pyjwt', 'requests', 'pydantic'
+      ],
+      zip_safe=False)
