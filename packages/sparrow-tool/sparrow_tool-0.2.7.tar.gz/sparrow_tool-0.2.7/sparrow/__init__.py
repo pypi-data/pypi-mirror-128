01a6b1e6e98976f7e557eb6aa547c8cb91bab0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:35 +0000

    Add package sparrow-tool/0.2.7/sparrow_tool-0.2.7.tar.gz

---
+++ packages/sparrow-tool/sparrow_tool-0.2.7.tar.gz/sparrow_tool-0.2.7/sparrow/__init__.py
@@ -0,0 +1,8 @@
+from .file_ops import yaml_load, yaml_dump, ppath, save, load, rm
+from .decorator import benchmark
+from .color_str import rgb_string
+
+
+_version_config = yaml_load(ppath("version-config.yaml", __file__))
+__version__= _version_config['version']
+print(f"{rgb_string(_version_config['name'], color='#34A853')} version: {rgb_string(__version__, color='#C9E8FF')}")
