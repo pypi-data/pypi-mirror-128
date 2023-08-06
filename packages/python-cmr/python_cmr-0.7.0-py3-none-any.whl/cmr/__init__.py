075742ed59dda326fac6131ea4864386c29ff8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:42 +0000

    Add package python-cmr/0.7.0/python_cmr-0.7.0-py3-none-any.whl

---
+++ packages/python-cmr/python_cmr-0.7.0-py3-none-any.whl/cmr/__init__.py
@@ -0,0 +1,3 @@
+from .queries import GranuleQuery, CollectionQuery, ToolQuery, ServiceQuery, VariableQuery
+
+__all__ = ["GranuleQuery", "CollectionQuery", "ToolQuery", "ServiceQuery", "VariableQuery"]
