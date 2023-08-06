236a59c36a061bc47345654289ffcf91d56ed9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:34 +0000

    Add package sycc/0.6.54/sycc-0.6.54.tar.gz

---
+++ packages/sycc/sycc-0.6.54.tar.gz/sycc-0.6.54/__init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+# .
+import os
+from colorama import init
+from datetime import datetime as sj
+from time import sleep as dd
+from math import pi as pai1
+from sys import exit as tc
+from k.test import now_path,size
+start=sj.now()
+from p.pdsys import *
+from v import *
+import socket
+
+#pypi_org()
