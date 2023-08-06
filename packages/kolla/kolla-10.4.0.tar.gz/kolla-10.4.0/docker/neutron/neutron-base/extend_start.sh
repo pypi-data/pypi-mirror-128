642114afa0e872b660ab519fa4a22aa5125e932f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:36 +0000

    Add package kolla/10.4.0/kolla-10.4.0.tar.gz

---
+++ packages/kolla/kolla-10.4.0.tar.gz/kolla-10.4.0/docker/neutron/neutron-base/extend_start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ ! -d "/var/log/kolla/neutron" ]]; then
+    mkdir -p /var/log/kolla/neutron
+fi
+if [[ $(stat -c %a /var/log/kolla/neutron) != "755" ]]; then
+    chmod 755 /var/log/kolla/neutron
+fi
+if [[ ${KOLLA_BASE_DISTRO} == "centos" ]]; then
+    export UPDATE_ALTERNATIVES="/usr/sbin/update-alternatives"
+else
+    export UPDATE_ALTERNATIVES="/usr/bin/update-alternatives"
+fi
+
+# set legacy iptables to allow kernels not supporting iptables-nft
+if $UPDATE_ALTERNATIVES --query iptables; then
+    if [[ $KOLLA_LEGACY_IPTABLES == "true" ]]; then
+        sudo $UPDATE_ALTERNATIVES --set iptables /usr/sbin/iptables-legacy
+        sudo $UPDATE_ALTERNATIVES --set ip6tables /usr/sbin/ip6tables-legacy
+    else
+        sudo $UPDATE_ALTERNATIVES --auto iptables
+        sudo $UPDATE_ALTERNATIVES --auto ip6tables
+    fi
+fi
+
+. /usr/local/bin/kolla_neutron_extend_start
