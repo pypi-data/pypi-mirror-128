f0af247d6a80e5ed1eb88b0e512379e2f6ce57c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:02 +0000

    Add package pip-tree/2.0.0/pip-tree-2.0.0.tar.gz

---
+++ packages/pip-tree/pip-tree-2.0.0.tar.gz/pip-tree-2.0.0/pip_tree/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from pip_tree.tree import generate_pip_tree, get_package_details, get_pip_package_list
+
+__all__ = [
+    generate_pip_tree,
+    get_package_details,
+    get_pip_package_list,
+]
