a659286251c73355d09dfbd09dba0bcb8860bdef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:17 +0000

    Add package digitalguide/0.0.113/digitalguide-0.0.113.tar.gz

---
+++ packages/digitalguide/digitalguide-0.0.113.tar.gz/digitalguide-0.0.113/digitalguide/db_objects.py
@@ -0,0 +1,34 @@
+import mongoengine
+from telegram.constants import MAX_MESSAGE_LENGTH
+import datetime
+
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+
+user_dbname = config["bot"]["bot_name"] + "_user"
+class User(mongoengine.Document):
+    user_id = mongoengine.StringField(required=True)
+    first_name = mongoengine.StringField(required=True, max_length=50)
+    last_name = mongoengine.StringField(max_length=50)
+    username = mongoengine.StringField(max_length=50)
+    language_code = mongoengine.StringField(max_length=10)
+    entry_time = mongoengine.DateTimeField(default=datetime.datetime.utcnow)
+    meta = {'db_alias': user_dbname}
+
+interaction_dbname = config["bot"]["bot_name"] + "_interaction"
+class Interaction(mongoengine.Document):
+    user = mongoengine.ReferenceField(User)
+    state=mongoengine.StringField(max_length=50)
+    update_id = mongoengine.IntField()
+    first_name = mongoengine.StringField(max_length=50)
+    last_name = mongoengine.StringField(max_length=50)
+    username = mongoengine.StringField(max_length=50)
+    interaction_full = mongoengine.DictField()
+    interaction_text = mongoengine.StringField(max_length=MAX_MESSAGE_LENGTH)
+    interaction_data = mongoengine.StringField(max_length=MAX_MESSAGE_LENGTH)
+    interaction_id = mongoengine.IntField()
+    date = mongoengine.DateTimeField(default=datetime.datetime.utcnow)
+    meta = {'db_alias': interaction_dbname}
+
