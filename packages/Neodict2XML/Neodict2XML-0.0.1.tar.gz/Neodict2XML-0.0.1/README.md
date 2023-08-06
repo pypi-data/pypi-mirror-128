2ccfae107ae2fbe54678ec2a7092b639559ad3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:36 +0000

    Add package Neodict2XML/0.0.1/Neodict2XML-0.0.1.tar.gz

---
+++ packages/Neodict2XML/Neodict2XML-0.0.1.tar.gz/Neodict2XML-0.0.1/README.md
@@ -0,0 +1,7 @@
+# Neodict2XML
+
+Neomyte's dict to XML converter
+
+# Contributors
+
+ * Emmanuel Pluot (aka. Neomyte)
---
+++ packages/Neodict2XML/Neodict2XML-0.0.1.tar.gz/Neodict2XML-0.0.1/tests/test_neodict2xml.py
@@ -0,0 +1,47 @@
+from xml.etree.ElementTree import Element, SubElement
+
+from neodict2xml import dict2xml
+
+def test_all():
+    # Given
+    test_dict = {
+        'test': {
+            'plop': ({'attr': 'brrr'}, 'lol'),
+            'lol': [
+                'hello',
+                'world'
+            ],
+            'deep': {
+                'deeper': 1
+            },
+            'test2': [
+                { 'foo': 'bar' },
+                ( { 'id': 2 }, { 'foo': 'rab' } )
+            ]
+        }
+    }
+
+    # When
+    xml = dict2xml.from_dict(test_dict)
+
+    # Then
+    top = Element('test')
+    c1 = SubElement(top, 'plop')
+    c1.set('attr', 'brrr')
+    c1.text = 'lol'
+    c21 = SubElement(top, 'lol')
+    c21.text = 'hello'
+    c22 = SubElement(top, 'lol')
+    c22.text = 'world'
+    c3 = SubElement(top, 'deep')
+    c31 = SubElement(c3, 'deeper')
+    c31.text = '1'
+    c41 = SubElement(top, 'test2')
+    c411 = SubElement(c41, 'foo')
+    c411.text = 'bar'
+    c42 = SubElement(top, 'test2', {'id': '2'})
+    c421 = SubElement(c42, 'foo')
+    c421.text = 'rab'
+    print(dict2xml.prettify(top))
+    print(dict2xml.prettify(xml))
+    assert dict2xml.prettify(top) == dict2xml.prettify(xml)
