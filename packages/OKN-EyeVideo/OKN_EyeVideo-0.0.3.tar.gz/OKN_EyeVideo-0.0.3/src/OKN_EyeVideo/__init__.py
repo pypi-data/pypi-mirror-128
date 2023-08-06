2e15a676ad784ee095f52ee0a5b60a39db66d5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package OKN-EyeVideo/0.0.3/OKN_EyeVideo-0.0.3.tar.gz

---
+++ packages/OKN-EyeVideo/OKN_EyeVideo-0.0.3.tar.gz/OKN_EyeVideo-0.0.3/src/OKN_EyeVideo/__init__.py
@@ -0,0 +1,6 @@
+# Help link: https://www.youtube.com/watch?v=v4bkJef4W94
+# Package on test: https://test.pypi.org/project/OKN-EyeVideo/0.0.1/
+
+# Package: https://pypi.org/project/OKN-EyeVideo/0.0.2/
+
+# print("Welcome to ML OKN_detection!")
---
+++ packages/OKN-EyeVideo/OKN_EyeVideo-0.0.3.tar.gz/OKN_EyeVideo-0.0.3/src/OKN_EyeVideo/print_mn.py
@@ -0,0 +1,12 @@
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('OKN_ML_classifier')
+
+def print():
+    print('Hi MNF')  # Press Ctrl+F8 to toggle the breakpoint.
+print()
