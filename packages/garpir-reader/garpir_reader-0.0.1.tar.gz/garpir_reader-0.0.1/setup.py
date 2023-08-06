4d30eb64b212c9ae9329eee518b98a05fa6e6bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:57 +0000

    Add package garpir-reader/0.0.1/garpir_reader-0.0.1.tar.gz

---
+++ packages/garpir-reader/garpir_reader-0.0.1.tar.gz/garpir_reader-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='garpir_reader',
+    packages=['text_reader'],
+    version='0.0.1',
+    author='Kostya_Serebryakov',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
