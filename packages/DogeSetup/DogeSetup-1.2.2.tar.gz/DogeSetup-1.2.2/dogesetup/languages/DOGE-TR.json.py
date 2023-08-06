77d36a73678f3d3b35a1b0e37852eae54e3f4444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:28 +0000

    Add package DogeSetup/1.2.2/DogeSetup-1.2.2.tar.gz

---
+++ packages/DogeSetup/DogeSetup-1.2.2.tar.gz/DogeSetup-1.2.2/dogesetup/languages/DOGE-TR.json.py
@@ -0,0 +1,105 @@
+{
+    "__JSON__": true,
+    "LANGUAGE": "Türkçe",
+    "LANGCODE": "tr",
+    "AUTHOR": "@Mutlcc",
+    "STRINGS": {
+
+        "HEROKU": {
+            "KEY": "https://dashboard.heroku.com/account adresindeki Heroku hesabınızın API'sini yapıştırın",
+            "LOGINING": "Heroku API'nizi kontrol ediyorum",
+            "INVALID_KEY": "Heroku API'niz geçersiz",
+            "RETRYING_KEY": "Kurulumu tekrar başlatıyorum",
+            "HEROKU_LOGGED": "Heroku'ya başarıyla bağlandım",
+
+            "CREATING_APP": "Heroku hesabınızda uygulama oluşturuyorum",
+            "MOSTAPP": "Heroku hesabınızda çok fazla uygulama var ve benim için hiç yer yok",
+            "FOR_MOSTAPP_SOLVE": "Benim kurulabilmem için;\n🚮 Bazı uygulamaları Heroku hesabınızdan silmeniz,\n🔢 Ya da farklı bir Heroku API Key'i girmeniz gerekiyor",
+            "IF_SOLVE_TRY_AGAIN": "Eğer sorunu çözdüyseniz, kurulum kodunu tekrar yapıştırarak kurulumu yeniden başlatın!\n🔹 Herhangi bir fikriniz yoksa @DogeSup Telegram grubumuza #herokusil yazın",
+            "SUCCESS_APP": "Uygulamayı başarıyla oluşturdum",
+            "DELETING_APP": "Uygulamanızı siliyorum",
+            "DELETED_APP": "Uygulamanızı sildim",
+            "ERROR_DELETED_APP": "Uygulamanızı silerken bir hata oluştu",
+
+            "ERROR": "Hay aksi!\n😪 Bir şeyler ters gitti.\n🚩 Hata",
+            "TRY_AGAIN_SETUP": "Lütfen kurulum kodunu tekrar yapıştırarak kurulumu yeniden başlatın",
+            "IF_SEE_ERROR": "Eğer bu hatayı tekrar görüyorsanız @DogeSup Telegram grubumuzdan yardım isteyin",
+
+            "POSTGRE": "PostgreSQL veritabanını Doge'a entegre ediyorum",
+            "SUCCESS_POSTGRE": "Veritabanını başarıyla ayarladım",
+
+            "OPENING_DYNO": "Dyno anahtarını açıyorum",
+            "ERROR_DYNO": "Dyno anahtarını açarken bir hata oluştu",
+            "OPENSELF_DYNO": "Dyno anahtarınızı dashboard.heroku.com sitesinden açın"
+        },
+
+        "MAIN": {
+            "SUCCESS_SESSION": "Telegram'da başarıyla oturum açtım",
+
+            "GETTING_READY": "Repo'yu indirmek için hazırlanıyorum",
+
+            "DOWNLOADING": "Repo'yu indiriyorum",
+
+            "DEPLOYING": "Deploy ediyorum...\n\n📍 Bu işlem, maksimum 5 dakika sürecek",
+
+            "WRITING_CONFIG": "Temel yapılandırma verilerini Heroku hesabınız ile senkronize ediyorum",
+            "SUCCESS_CONFIG": "Senkronize işlemini başarıyla bitirdim",
+
+            "END": "{} SANİYEDE KURULUMU TAMAMLADINIZ",
+
+            "ASK_ALIVENAME": "Size farklı bir isimde seslenmemi istiyorsanız Y yazın",
+            "ASKING_ALIVENAME": "Size nasıl bir isimde seslenmeliyim",
+            "SUCCESS_ALIVENAME": "WOW {} harikasın kanka",
+
+            "ASK_PMLOGGER": "PM ve grup etiketli mesajları otomatik olarak depolamamı istiyor musunuz?\n\n🔹 Çalıştığım sürece, bu özellik ile silinen ve düzenlenen mesajları görebilirsiniz.\n💠️ Eğer açarsanız bir gizli grup oluşturacağım.\n\n💭 Açmak için Y yazın",
+            "SUCCESS_PMLOGGER": "WOW dostum artık bir hackersın",
+
+            "ASK_DOGEPLUGIN": "Ekstra pluginleri aktif etmek istiyor musunuz?\n\n🔹 Animasyon, eğlence, vb. pluginleri içerir.\n💠️ Eğer açarsanız @DogePlugin kanalımızdan otomatik olarak tüm pluginleri kuracağım.\n\n💭 Açmak için Y yazın",
+            "SUCCESS_DOGEPLUGIN": "Artık @DogePlugin'deki tüm pluginlere sahipsin",
+            "FALSE_DOGEPLUGIN": "Ekstra pluginlere @DogePlugin kanalımızdan ulaşabilirsiniz",
+
+            "ASK_DOGEHUB": "Ekstra +18 müstehcen pluginleri aktif etmek istiyor musunuz?\n\n🔹 H*ntai, p*rno, vb. pluginleri içerir.\n💠️ Eğer açarsanız @DogeHUB kanalımızdan otomatik olarak tüm +18 pluginleri kuracağım.\n\n💭 Açmak için Y yazın",
+            "SUCCESS_DOGEHUB": "Artık @DogeHUB'daki tüm +18 pluginlere sahipsin!\n\n😘 Dikkatli kullan kanka",
+            "FALSE_DOGEHUB": "Ekstra +18 pluginlere @DogeHUB kanalımızdan ulaşabilirsiniz",
+
+            "SLOGAN": "Telegram'da bir köpeğiniz olsun!",
+            "SEEYOU": "@SohbetDoge grubumuza bekleriz!\n\n⚡ Herhangi bir sohbete .alive yazarak durumumu kontrol edebilirsiniz.\n\n🧶 Ayrıca .doge komutuyla sahip olduğum tüm pluginlerin nasıl kullanılacağını size öğretebilirim.\n\n🐶 Size hizmet etmek için sabırsızlanıyorum!\n\n🐾 Görüşürüz"
+        },
+
+        "SESSION": {
+            "IS_DOGE_RELIABLE": "Telefon numaranızı neden istiyorum?\n\n🐶 Çünkü ben, Telegram hesabınızda bir oturum ile çalışırım.\n\n🐾 Verilerinize değer veririm, asla;\nverilerinize ve hesabınıza zarar vermem",
+            "SAMPLE_NUMBER": "Örnek: +905556667788",
+            "PHONE_NUMBER": "Telefon numaranızın başında ülke kodu olacak şekilde, örnekteki gibi yazın",
+
+            "CONNECTING": "Telegram'a bağlanıyorum",
+            "CANT_CONNECT": "Telegram'a bağlanamadım",
+
+            "WRITE_CODE_FROM_TG": "Telegram'dan gelen kodu yazın",
+            "INVALID_CODE_MY": "Büyük ihtimalle kodu yanlış yazdınız",
+            "TRY_AGAIN_SETUP": "Lütfen kurulum kodunu tekrar yapıştırarak kurulumu yeniden başlatın",
+
+            "NEW_APP": "Daha önce hiç uygulama oluşturmamışsınız.\n⏳ Hemen bi'tane oluşturuyorum",
+            "CANT_CREATE_APP": "Telegram otomatik uygulama oluşturmamı engelledi",
+            "CREATED": "Telegram API ID & HASH için uygulamanız hazır",
+
+            "GETTING_API": "API ID & HASH'i getiriyorum",
+            "INFOS": "İsterseniz herhangi bir yere not edebilirsiniz",
+
+            "SESSION_GENERATING": "Telegram hesabınızda oturum açıyorum",
+            "ERROR": "Hay aksi!\n😪 Bir şeyler ters gitti",
+            "ANY_ERROR_SOLVING": "Farklı bir yöntem ile işleme devam ediyorum",
+            "RETRYING": "Bağlanırken bir hata oluştu!\n\n🔄 Tekrar deniyorum",
+
+            "ERROR_FW": "Hesabınız bu işlem için {} saniye boyunca Telegram tarafından kısıtlandı",
+            "TRY_AGAIN_FW": "İşleme devam etmek için {} saniye sonra tekrar deneyin",
+            "INVALID_CODE": "Kodu yanlış yazdınız",
+            "TRY_AGAIN": "Lütfen tekrar deneyin",
+            "WARNING": "Fazla denemeniz hesabınızın kısıtlanmasına sebep olabilir",
+
+            "EXPIRED": "Beni başka birine kuruyor gibisin.\n\n🪐 Kodu iletmemesini ve ekran görüntüsü göndermesini söyleyin.\n\n🔁 Fakat bu kod artık geçersiz olduğundan, kurulum kodunu tekrar yapıştırarak kurulumu yeniden başlatın",
+
+            "PASS": "2 aşamalı doğrulama şifrenizi yazın",
+            "INVALID_2FA": "2 aşamalı şifrenizi yanlış yazdınız"
+        }
+    }
+}
