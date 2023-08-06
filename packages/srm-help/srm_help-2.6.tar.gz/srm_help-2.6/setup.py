feb36b1f3706e904a331eac68395609371cba84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:00 +0000

    Add package srm-help/2.6/srm_help-2.6.tar.gz

---
+++ packages/srm-help/srm_help-2.6.tar.gz/srm_help-2.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  name="srm_help",
+  version="2.6",
+  author="flhsjjk",
+  author_email="flhsjjk@mail.ustc.edu.cn",
+  description="面向SRM团队的辅助模块，本模块内存在了部分常用的操作或者计算方法,希望能为提升数据处理效率上有所增益.",
+  long_description= "待续",
+  long_description_content_type="text/markdown",
+  url="",
+  packages = ["srm_help"],
+  classifiers=[
+  "Programming Language :: Python :: 3.8",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
