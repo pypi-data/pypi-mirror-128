0c7b015d9ce8799e619279707f4506dbb714b9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:06 +0000

    Add package windchill-metric-config/0.0.2/windchill_metric_config-0.0.2-py3-none-any.whl

---
+++ packages/windchill-metric-config/windchill_metric_config-0.0.2-py3-none-any.whl/windchill_metric_config/configurator.py
@@ -0,0 +1,59 @@
+from json import dumps, loads
+
+from ruamel.yaml import YAML, scalarstring, CommentedMap
+
+from description import Description
+from system.system import SystemMetrics
+from windchill.windchill import WindchillMetrics
+
+
+class Metrics:
+    comment_indent = 80
+
+    def __init__(self):
+        self.system = SystemMetrics()
+        self.windchill = WindchillMetrics()
+
+    def __str__(self):
+        return dumps(self.as_dict())
+
+    def as_dict(self):
+        return {
+            'system': self.system.as_dict(),
+            'windchill': self.windchill.as_dict()
+        }
+
+    def as_yaml_dict(self):
+        return {
+            'system': self.system.as_yaml_dict(),
+            'windchill': self.windchill.as_yaml_dict()
+        }
+
+    def save_as_yaml(self, config_yaml: str):
+        yaml = YAML()
+        data = loads(dumps(self.as_yaml_dict()), object_pairs_hook=CommentedMap)
+        scalarstring.walk_tree(data)
+        self.windchill.generate_yaml(data['windchill'], self.comment_indent)
+        self.system.generate_yaml(data['system'], self.comment_indent)
+        with open(config_yaml, 'w', encoding='utf-8') as f:
+            yaml.dump(data, f)
+
+    def load_config(self, config_yaml: str):
+        yaml = YAML()
+        with open(config_yaml, 'r', encoding='utf-8') as f:
+            data = yaml.load(f)
+        for key in self.__dict__.keys():
+            if key in data:
+                if key == 'system':
+                    self.__getattribute__(key).set_config(data[key])
+
+    def metrics_as_list(self):
+        metric_list = []
+        for item in self.__dict__.keys():
+            child = self.__getattribute__(item)
+            if type(child) == Description:
+                metric_list.append(child.id)
+            else:
+                child.metrics_as_list(metric_list)
+
+        return metric_list
