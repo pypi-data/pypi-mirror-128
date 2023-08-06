45759b323d5bfae4ee962367399f791b2c6a707e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:26 +0000

    Add package simbots/0.0.4/simbots-0.0.4.tar.gz

---
+++ packages/simbots/simbots-0.0.4.tar.gz/simbots-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "simbots",
+    version = "0.0.4",
+    description = "Simple bots or Simbots is a library designed to create simple bots using the power of python. This library utilises Intent,Entity,Relation and Context model to create bots . It uses a multinomial NB classifier to create intent classifiers.",
+    packages =find_packages(),
+    #package_dir = {'':'src'},
+    classifiers = ["Development Status :: 1 - Planning","Operating System :: OS Independent","License :: OSI Approved :: MIT License","Programming Language :: Python :: 3"],
+    install_requires =['sklearn','objectpath'],
+    license_files = ('LICENSE.txt'),
+    py_modules =['Bot']
+
+)
