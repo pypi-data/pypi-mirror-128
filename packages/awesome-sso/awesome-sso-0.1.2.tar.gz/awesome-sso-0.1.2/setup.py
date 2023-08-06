4f6d71e9f5dc48695d5a884537127a4c477d2bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:01 +0000

    Add package awesome-sso/0.1.2/awesome-sso-0.1.2.tar.gz

---
+++ packages/awesome-sso/awesome-sso-0.1.2.tar.gz/awesome-sso-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['awesome_sso', 'awesome_sso.exceptions']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'awesome-sso',
+    'version': '0.1.2',
+    'description': 'sso general utility for services connected to sso',
+    'long_description': '# Usage\n1. to install poetry: `pip instala poetry`\n2. project setup: `poetry install`\n3. update version in `pyproject.toml`\n4. build: `poetry build`\n5. publish to pypi: `poetry publish`\n\n',
+    'author': 'Schwannden Kuo',
+    'author_email': 'schwannden@mobagel.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'http://gitlab.ct.mobagel.com:7979/MOD/awesome-sso',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
