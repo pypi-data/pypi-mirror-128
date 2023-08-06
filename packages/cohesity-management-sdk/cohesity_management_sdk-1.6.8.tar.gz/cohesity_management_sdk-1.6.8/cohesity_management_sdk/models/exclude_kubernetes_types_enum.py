034bbb77a5cc2f1350e1d329c4cace60d40a2f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:48 +0000

    Add package cohesity-management-sdk/1.6.8/cohesity_management_sdk-1.6.8.tar.gz

---
+++ packages/cohesity-management-sdk/cohesity_management_sdk-1.6.8.tar.gz/cohesity_management_sdk-1.6.8/cohesity_management_sdk/models/exclude_kubernetes_types_enum.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Cohesity Inc.
+
+class ExcludeAwsTypesEnum(object):
+
+    """Implementation of the 'ExcludeAwsTypes' enum.
+
+    TODO: type enum description here.
+
+    Attributes:
+        KSERVICE: TODO: type description here.
+
+    """
+
+    KSERVICE = 'kService'
+
