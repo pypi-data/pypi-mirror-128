655c8a2b36def6bb9e975bc71875fee039a38311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:53 +0000

    Add package snewpy/1.1/snewpy-1.1.tar.gz

---
+++ packages/snewpy/snewpy-1.1.tar.gz/snewpy-1.1/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function
+#
+# Standard imports
+#
+from glob import glob
+import os
+import re
+import sys
+#
+from distutils.command.sdist import sdist as DistutilsSdist
+from setuptools import setup, find_packages
+#
+# Git-based version info. Remove?
+#
+from python.snewpy._git import get_version, SetVersion
+#
+# Begin setup
+#
+setup_keywords = dict()
+#
+setup_keywords['name'] = 'snewpy'
+setup_keywords['description'] = 'SNEWS2 supernova simulation package'
+setup_keywords['author'] = 'SNEWS2 Collaboration'
+setup_keywords['author_email'] = 'snews2.0@lists.bnl.gov'
+setup_keywords['license'] = 'BSD'
+setup_keywords['url'] = 'https://github.com/SNEWS2/supernova_models'
+setup_keywords['version'] = get_version()
+#
+# Use README.md as a long_description.
+#
+setup_keywords['long_description'] = ''
+if os.path.exists('README.md'):
+    with open('README.md') as readme:
+        setup_keywords['long_description'] = readme.read()
+    setup_keywords['long_description_content_type'] = 'text/markdown'
+#
+# Set other keywords for the setup function.
+#
+# Use entry_points to let `pip` create executable scripts for each target platform.
+# See https://setuptools.readthedocs.io/en/latest/userguide/entry_point.html
+# setup_keywords['entry_points'] = {'console_scripts': ['to_snowglobes = snewpy.to_snowglobes:generate_time_series', ], },
+setup_keywords['provides'] = [setup_keywords['name']]
+setup_keywords['requires'] = ['Python (>3.3.0)']
+setup_keywords['zip_safe'] = False
+setup_keywords['use_2to3'] = False
+setup_keywords['packages'] = find_packages('python')
+setup_keywords['package_dir'] = {'': 'python'}
+setup_keywords['cmdclass'] = {'version': SetVersion, 'sdist': DistutilsSdist}
+setup_keywords['test_suite']='snewpy.test.snewpy_test_suite.snewpy_test_suite'
+
+requires = []
+with open('requirements.txt', 'r') as f:
+    for line in f:
+        if line.strip():
+            requires.append(line.strip())
+setup_keywords['install_requires'] = requires
+#
+# Internal data directories.
+#
+#setup_keywords['data_files'] = [('snewpy/data/config', glob('data/config/*')),
+#                                ('snewpy/data/spectra', glob('data/spectra/*'))]
+#
+# Run setup command.
+#
+setup(**setup_keywords)
