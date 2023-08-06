9603c248fdac4adb2021d02d981742182f3a020a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:57 +0000

    Add package escriptorium-connector/0.0.19/escriptorium-connector-0.0.19.tar.gz

---
+++ packages/escriptorium-connector/escriptorium-connector-0.0.19.tar.gz/escriptorium-connector-0.0.19/src/escriptorium_connector/models/__init__.py
@@ -0,0 +1,12 @@
+from .document_models import (
+    PostDocument,
+    PutDocument,
+    GetDocument,
+    GetDocuments,
+    ReadDirection,
+    LineOffset,
+)
+
+from .line_models import GetLineType
+from .region_models import GetRegionType
+from .transcription_models import GetTranscription
