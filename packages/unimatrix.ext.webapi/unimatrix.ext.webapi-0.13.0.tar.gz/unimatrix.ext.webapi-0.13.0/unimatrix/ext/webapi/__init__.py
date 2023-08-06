eaacfec862edf1c48f8be159e03f65c637d396e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:53 +0000

    Add package unimatrix.ext.webapi/0.13.0/unimatrix.ext.webapi-0.13.0.tar.gz

---
+++ packages/unimatrix.ext.webapi/unimatrix.ext.webapi-0.13.0.tar.gz/unimatrix.ext.webapi-0.13.0/unimatrix/ext/webapi/__init__.py
@@ -0,0 +1,37 @@
+# pylint: skip-file
+from .asgi import Application
+from .decorators import action
+from .exceptions import UpstreamServiceNotAvailable
+from .exceptions import UpstreamConnectionFailure
+from .keytrustpolicy import KeyTrustPolicy
+from .resourceendpointset import ResourceEndpointSet
+from .resourceendpointset import PublicResourceEndpointSet
+from .service import Service
+
+
+__all__ = [
+    'action',
+    'Application',
+    'EndpointSet',
+    'PublicEndpointSet',
+    'Service',
+    'UpstreamConnectionFailure',
+    'UpstreamServiceNotAvailable',
+]
+
+
+EndpointSet = ResourceEndpointSet
+PublicEndpointSet = PublicResourceEndpointSet
+
+
+def policy(tags: list) -> KeyTrustPolicy:
+    """Declares a policy for an endpoint to determine which public keys
+    it wants to trust.
+
+    Args:
+        tags (list): The list of tags that this policy accepts.
+
+    Returns:
+        A :class:`KeyTrustPolicy` instance.
+    """
+    return KeyTrustPolicy(tags)
