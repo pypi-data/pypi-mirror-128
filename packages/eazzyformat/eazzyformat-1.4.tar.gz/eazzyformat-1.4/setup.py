6f856cdfb92d8c4661967ff93fa8ee558ae14e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:58 +0000

    Add package eazzyformat/1.4/eazzyformat-1.4.tar.gz

---
+++ packages/eazzyformat/eazzyformat-1.4.tar.gz/eazzyformat-1.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="eazzyformat",
+    packages=["eazzyformat"],
+    version="1.4",
+    license="GNU General Public License v3",
+    author="megahomyak",
+    author_email="g.megahomyak@gmail.com",
+    url="https://github.com/megahomyak/eazzyformat",
+    download_url=(
+        "https://github.com/megahomyak/eazzyformat/archive/v1.4.tar.gz"
+    ),
+    install_requires=[]
+)
