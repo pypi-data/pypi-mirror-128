3efee0b62b48a3ed8388369e3d8b766e5c1f9c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:15 +0000

    Add package SlyAPI/0.0.1/SlyAPI-0.0.1.tar.gz

---
+++ packages/SlyAPI/SlyAPI-0.0.1.tar.gz/SlyAPI-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='SlyAPI',
+    version='0.0.1',
+    description='No-boilerplate, async web api access with oauth1/2.',
+    py_modules=["SlyAPI"],
+    package_dir={'': 'src'}
+)
