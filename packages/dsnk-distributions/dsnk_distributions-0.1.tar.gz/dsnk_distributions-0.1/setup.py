0d598dea9131ae3a0d78b31659fba4349dff7d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:01 +0000

    Add package dsnk-distributions/0.1/dsnk_distributions-0.1.tar.gz

---
+++ packages/dsnk-distributions/dsnk_distributions-0.1.tar.gz/dsnk_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnk_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnk_distributions'],
+      author = 'ASIFIWE Emmanuel',
+      author_email ='emmanuel2koestner@gmail.com',
+      zip_safe=False)
