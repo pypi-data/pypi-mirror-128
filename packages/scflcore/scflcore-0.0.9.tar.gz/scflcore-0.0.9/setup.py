351c44e02fcec668dd4770840134b3fd5261efe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:28 +0000

    Add package scflcore/0.0.9/scflcore-0.0.9.tar.gz

---
+++ packages/scflcore/scflcore-0.0.9.tar.gz/scflcore-0.0.9/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+long_description = """# About
+
+
+Functional package
+
+```py
+from scflcore import CoreManager
+
+core = coreManager(
+	name="MyCore"
+)
+
+print(core.request(url="https://example.com", json=True))
+```
+"""
+setuptools.setup(
+    name="scflcore", # Put your username here!
+    version="0.0.9", # The version of your package!
+    author="SScefaLI", # Your name here!
+    author_email="birka11@list.ru", # Your e-mail here!
+    description="Function core package by SScefaLI", # A short description here!
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="", # Link your package website here! (most commonly a GitHub repo)
+    packages=setuptools.find_packages(), # A list of all packages for Python to distribute!
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests'
+    ], # Enter meta data into the classifiers list!
+    python_requires='>=3.8', # The version requirement for Python to run your package!
+)
