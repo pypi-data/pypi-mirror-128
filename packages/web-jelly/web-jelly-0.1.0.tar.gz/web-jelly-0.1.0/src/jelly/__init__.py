a634eba22245fedba2bb686063b4a85279b664dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:59 +0000

    Add package web-jelly/0.1.0/web-jelly-0.1.0.tar.gz

---
+++ packages/web-jelly/web-jelly-0.1.0.tar.gz/web-jelly-0.1.0/src/jelly/__init__.py
@@ -0,0 +1,32 @@
+from .client import JellyClient
+
+
+global __jelly__
+__jelly__ = None
+def init (*path, **config):
+  global __jelly__
+  route = []
+  for p in path:
+    if not isinstance(p, str):
+      raise ValueError('path arguments must be string')
+    for r in p.split('/'):
+      if r:
+        route.append(r)
+  
+  if not __jelly__:
+    __jelly__ = JellyClient()
+  
+  scope = '.'.join(route[:3])
+  __jelly__.session(scope)
+  if config:
+    __jelly__.configure(**config)
+  return __jelly__
+
+def serve ():
+  global __jelly__
+  __jelly__.serve()
+
+def render (obj):
+  global __jelly__
+  __jelly__.render(obj)
+
