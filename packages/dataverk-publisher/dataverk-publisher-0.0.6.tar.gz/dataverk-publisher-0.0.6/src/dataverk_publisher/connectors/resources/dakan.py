d03be1e530d8b623189e7919b30fafc1c70d4e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:50 +0000

    Add package dataverk-publisher/0.0.6/dataverk-publisher-0.0.6.tar.gz

---
+++ packages/dataverk-publisher/dataverk-publisher-0.0.6.tar.gz/dataverk-publisher-0.0.6/src/dataverk_publisher/connectors/resources/dakan.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+import requests
+
+from dataverk_publisher.connectors.resources.base import StorageBase
+
+
+class DakanConnector(StorageBase):
+    def __init__(self, host: str):
+        super().__init__()
+        self._host = host
+
+    def write(
+        self, file: Any, dp_id: str, resource_path: str, fmt: str = "csv", **kwargs
+    ):
+        try:
+            res = requests.put(
+                url=f"{self._host}/v1/datapackage/{dp_id}/attachments",
+                files=[("files", (f"{resource_path}", file))],
+            )
+            res.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            self.logger.error(
+                f"Unable to write object {dp_id}/{resource_path} to api {self._host}: {str(err)}"
+            )
+            raise
+        except requests.exceptions.RequestException as err:
+            self.logger.error(f"Connection error {self._host}: {str(err)}")
+            raise
+        else:
+            self.logger.info(
+                f"Successfully wrote {dp_id}/{resource_path} to api {self._host}"
+            )
+            return res.json()
