d387cefd642f9e5f115844da0503064946f3c5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:30 +0000

    Add package DidemTaha/0.0.1/DidemTaha-0.0.1.tar.gz

---
+++ packages/DidemTaha/DidemTaha-0.0.1.tar.gz/DidemTaha-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(name="DidemTaha",
+                 version="0.0.1",
+                 license="MIT",
+                 author="Taha Çakır",
+                 author_mail="cakirtaha97@gmail.com",
+                 description="Data Science Tools",
+                 url="https://github.com/Taha-Cakir",
+                 keywords=["data science", "machine learning"],
+                 classifiers=[
+                     "Development Status :: 4 - Beta",
+                     "License :: OSI Approved :: MIT License",
+                     "Programming Language :: Python",
+                     "Programming Language :: Python :: 2.7",
+                     "Programming Language :: Python :: 3.5",
+                     "Topic :: Scientific/Engineering",
+                 ],
+                 packages= setuptools.find_packages()
+                 )
