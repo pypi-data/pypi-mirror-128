eca4b0e6431aa3330f033b0cbdacccff3bf0606a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:58 +0000

    Add package pythonic-bert/1.7.6/pythonic-bert-1.7.6.tar.gz

---
+++ packages/pythonic-bert/pythonic-bert-1.7.6.tar.gz/pythonic-bert-1.7.6/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from io import open
+from setuptools import setup, find_packages
+setup(
+    name='pythonic-bert',
+    version='1.7.6',
+    description='a package for your bert using',
+    long_description='使用pytorch实现你的bert项目',
+    author='xiaoguzai',
+    author_email='474551240@qq.com',
+    license='Apache License 2.0',
+    url='https://github.com/boss2020/pytorch-transformer',
+    download_url='https://github.com/boss2020/pytorch-transformer/master.zip',
+    packages=find_packages(),
+)
