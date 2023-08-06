d287713910ea51abed144ea4d34f1fc62f87d993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:30 +0000

    Add package dmp-general-utils/0.0.1/dmp_general_utils-0.0.1.tar.gz

---
+++ packages/dmp-general-utils/dmp_general_utils-0.0.1.tar.gz/dmp_general_utils-0.0.1/dmp_utils/__init__.py
@@ -0,0 +1,5 @@
+import binary_uuid
+import func_date
+import abstract_model
+
+__all__ = ['binary_uuid', 'func_date', 'abstract_model']
---
+++ packages/dmp-general-utils/dmp_general_utils-0.0.1.tar.gz/dmp_general_utils-0.0.1/dmp_utils/abstract_model.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+import pytz
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class BaseModal:
+    created_at = db.Column(db.Integer, server_default=int(datetime.now().astimezone(pytz.utc).timestamp()))
+    updated_at = db.Column(db.Integer, server_default=int(datetime.now().astimezone(pytz.utc).timestamp()))
