8b33ab4d2749014647565e8428ddd0f4c1c4d30e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:46 +0000

    Add package bdbf/1.2.2/bdbf-1.2.2.tar.gz

---
+++ packages/bdbf/bdbf-1.2.2.tar.gz/bdbf-1.2.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from bdbf import __version__
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../bdbf'))
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bdbf",  # Replace with your own username
+    version=__version__,
+    author="Bertik23",
+    author_email="bertikxxiii@gmail.com",
+    description="Bertik23's discord bot framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bertik23/bdbf",
+    packages=["bdbf"],  # setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["discord.py>=1.4.1"],
+    keywords="discord, bot, framework"
+)
