c1eaa43526a8194f86408e2a460c8e1c1701f926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:57 +0000

    Add package lp2name/0.1/lp2name-0.1-py3-none-any.whl

---
+++ packages/lp2name/lp2name-0.1-py3-none-any.whl/lp2name/__init__.py
@@ -0,0 +1,60 @@
+from lp2name.models import *
+
+
+class User:
+    def __init__(
+        self,
+        db,
+        uuid,
+        username) -> None:
+        self.db = db
+        self.uuid = uuid
+        self.username = username
+
+
+class LuckPerms:
+    def __init__(
+        self,
+        database: DB,
+        columns: Columns = Columns(),
+    ):
+        self.db = database.database
+        self.columns = columns
+        self.model: list[str] = []
+        self.describe_table()
+
+    def describe_table(self):
+        with self.db.cursor() as cursor:
+            cursor.execute(f"DESCRIBE {self.columns.table}")
+            result = cursor.fetchall()
+            for i in result:
+                self.model.append(i[0])
+
+    def get_user_by_name(self, name: str):
+        name = name.lower()
+        with self.db.cursor() as cursor:
+            cursor.execute(
+                f"SELECT * FROM `{self.columns.table}` WHERE `{self.columns.username}` = %s",
+                [name],
+            )
+            result = cursor.fetchall()
+            result = result[0]
+            print(result)
+            return User(
+                db=self,
+                uuid=result[0],
+                username=name)
+    
+    def get_user_by_uuid(self, uuid: str):
+        uuid = uuid.lower()
+        with self.db.cursor() as cursor:
+            cursor.execute(
+                f"SELECT * FROM `{self.columns.table}` WHERE `{self.columns.uuid}` = %s",
+                [uuid],
+            )
+            result = cursor.fetchall()
+            result = result[0]
+            return User(
+                db=self,
+                uuid=uuid,
+                username=result[1])
