2ee09b8de0215352623bfd8da8d38f88465c45d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:38 +0000

    Add package pywapor-test/2.3.0/pywapor_test-2.3.0.tar.gz

---
+++ packages/pywapor-test/pywapor_test-2.3.0.tar.gz/pywapor_test-2.3.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pywapor_test',
+    version = '2.3.0',
+    url = 'https://bitbucket.org/cioapps/wapor-et-look/src/master/',
+    author = "FAO",
+    author_email = "bert.coerver@fao.org",
+    license = "Apache",
+    packages = find_packages(include = ['pywapor', 'pywapor.*'], exclude=["tests.*"]),
+    include_package_data=True,
+    install_requires = [
+        'gdal<=3.1.4',
+        'aiohttp==3.7.4.post0',
+        'numpy',
+        'pandas',
+        'requests',
+        'matplotlib',
+        'netcdf4',
+        'pyproj',
+        'scipy',
+        'fiona',
+        'pycurl',
+        'pyshp',
+        'joblib',
+        'bs4',
+        'paramiko',
+        'rasterio',
+        'xarray',
+        'geojson',
+        'vito_download',
+        'nest_asyncio',
+        'tqdm',
+        'dask',
+        'rioxarray',
+        'ipympl',
+    ]
+)
