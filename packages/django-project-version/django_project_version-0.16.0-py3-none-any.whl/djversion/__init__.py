fc8f2c874950c4fa016109b1cc7fea9a41fa7541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:19 +0000

    Add package django-project-version/0.16.0/django_project_version-0.16.0-py3-none-any.whl

---
+++ packages/django-project-version/django_project_version-0.16.0-py3-none-any.whl/djversion/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# django-project-version
+# djversion/__init__.py
+
+
+from typing import List
+
+
+__all__: List[str] = ["default_app_config"]
+
+
+default_app_config: str = "djversion.apps.DjangoDjversionConfig"
---
+++ packages/django-project-version/django_project_version-0.16.0-py3-none-any.whl/djversion/urls.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# django-project-version
+# djversion/urls.py
+
+
+from typing import List, Union
+
+from django.urls import re_path
+from django.urls.resolvers import URLPattern, URLResolver
+
+from djversion.rest import VersionView
+
+
+__all__: List[str] = ["urlpatterns"]
+
+
+urlpatterns: List[Union[URLPattern, URLResolver]] = [
+    re_path(r"^$", VersionView.as_view(), name="version-view")
+]
