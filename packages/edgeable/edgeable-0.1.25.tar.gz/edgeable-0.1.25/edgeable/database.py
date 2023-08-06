4b1bd10e86b8bdb2d704e45aa51d5fc7d57e5bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:20 +0000

    Add package edgeable/0.1.25/edgeable-0.1.25.tar.gz

---
+++ packages/edgeable/edgeable-0.1.25.tar.gz/edgeable-0.1.25/edgeable/database.py
@@ -0,0 +1,64 @@
+import pickle
+import logging
+import gzip
+import types
+import os
+import tempfile
+
+from edgeable import GraphNode, GraphModifyLock, GraphReadLock
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("edgeable")
+
+
+class GraphDatabase:
+    def __init__(self, filename="graph.db"):
+        if type(filename) is not str:
+            raise RuntimeError("Filename must be a string.")
+        self._graph = {}
+        self._filename = filename
+
+    def get_nodes(self, criteria=lambda node: True):
+        if type(criteria) is not types.FunctionType:
+            raise RuntimeError("Criteria must be a function.")
+
+        return [node for node in self._graph.values() if criteria(node)]
+
+    def has_node(self, id):
+        return id in self._graph
+
+    # Retrieves or creates a node with the provide id
+    @GraphModifyLock
+    def put_node(self, id, properties={}):
+        if type(id) is not str:
+            raise RuntimeError("Node id must be a string.")
+
+        if not self.has_node(id):
+            logger.info("create node '%s'", id)
+            self._graph[id] = GraphNode(self, id)
+            self._graph[id]._properties = properties.copy()
+        else:
+            self._graph[id]._properties = {**self._graph[id]._properties, **properties}
+
+        return self._graph[id]
+
+    def get_node_count(self):
+        return len(self._graph)
+
+    def get_edge_count(self):
+        return sum([len(self._graph[node]._edges) for node in self._graph])
+
+    def load(self):
+        logger.info("load from file '%s'", self._filename)
+        with gzip.open(self._filename, "rb") as f:
+            self._graph = pickle.load(f)
+
+    @GraphReadLock
+    def save(self):
+        logger.info("save to file '%s'", self._filename)
+        temp_file = tempfile.NamedTemporaryFile(
+            dir=os.path.dirname(self._filename), delete=False
+        )
+        with gzip.open(temp_file, "wb") as f:
+            pickle.dump(self._graph, f, protocol=4)
+        os.replace(temp_file.name, self._filename)
