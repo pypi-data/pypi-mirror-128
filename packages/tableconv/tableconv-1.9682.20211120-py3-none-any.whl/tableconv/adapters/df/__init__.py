5687d6551927c0e6855ab09c04cc816ca03c2dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:09 +0000

    Add package tableconv/1.9682.20211120/tableconv-1.9682.20211120-py3-none-any.whl

---
+++ packages/tableconv/tableconv-1.9682.20211120-py3-none-any.whl/tableconv/adapters/df/__init__.py
@@ -0,0 +1,17 @@
+from .base import write_adapters, read_adapters, adapters
+
+from .ascii import *
+from .aws_athena import *
+from .aws_dynamodb import *
+from .gsheets import *
+from .jira import *
+from .json import *
+from .nested_list import *
+from .pandas_io import *
+from .python import *
+from .smart_sheet import *
+from .sql import *
+from .sql_literal import *
+from .sumo_logic import *
+from .text_array import *
+from .yaml import *
