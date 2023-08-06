ba8fd9c74082915ea8a07afd5b37f86b285c2f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package django-xls2po/0.9.0/django_xls2po-0.9.0-py3-none-any.whl

---
+++ packages/django-xls2po/django_xls2po-0.9.0-py3-none-any.whl/xls2po/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# django-xls2po
+# xls2po/__init__.py
+
+
+from typing import List
+
+
+__all__: List[str] = ["default_app_config"]
+
+
+default_app_config = "xls2po.apps.DjangoXlsToPoConfig"
---
+++ packages/django-xls2po/django_xls2po-0.9.0-py3-none-any.whl/xls2po/management/commands/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# django-xls2po
+# xls2po/management/commands/__init__.py
+
+
+from typing import List
+
+
+__all__: List[str] = []
