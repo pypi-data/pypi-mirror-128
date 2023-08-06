50b6c91c5c7b1ab486b8658e2f389d30efb6b3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:10 +0000

    Add package high-order-layers-torch/1.0.5/high-order-layers-torch-1.0.5.tar.gz

---
+++ packages/high-order-layers-torch/high-order-layers-torch-1.0.5.tar.gz/high-order-layers-torch-1.0.5/high_order_layers_torch/layers.py
@@ -0,0 +1,59 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from pytorch_lightning.metrics.functional import accuracy
+from .PolynomialLayers import *
+from .ProductLayer import *
+from .FunctionalConvolution import *
+
+"""
+def linear_wrapper(in_features: int, out_features: int, bias: bool = True, **kwargs):
+    return nn.Linear(in_features, out_features, bias)
+"""
+
+fc_layers = {
+    "continuous": PiecewisePolynomial,
+    "continuous_prod": PiecewisePolynomialProd,
+    "discontinuous": PiecewiseDiscontinuousPolynomial,
+    "discontinuous_prod": PiecewiseDiscontinuousPolynomialProd,
+    "polynomial": Polynomial,
+    "polynomial_prod": PolynomialProd,
+    "product": Product,
+    "fourier": FourierSeries,
+}
+
+convolutional_layers = {
+    "continuous": PiecewisePolynomialConvolution2d,
+    "continuous1d": PiecewisePolynomialConvolution1d,
+    "continuous_prod": None,  # PiecewisePolynomialProd,
+    "discontinuous": PiecewiseDiscontinuousPolynomialConvolution2d,
+    "discontinuous1d": PiecewiseDiscontinuousPolynomialConvolution1d,
+    "discontinuous_prod": None,  # PiecewiseDiscontinuousPolynomialProd,
+    "polynomial": PolynomialConvolution2d,
+    "polynomial1d": PolynomialConvolution1d,
+    "polynomial_prod": None,  # PolynomialConvolutionProd2d,
+    "product": None,  # ProductConvolution2d,
+    "fourier": FourierConvolution2d,
+    "fourier1d": FourierConvolution1d,
+}
+
+
+def high_order_fc_layers(layer_type: str, **kwargs):
+
+    if layer_type in fc_layers.keys():
+        return fc_layers[layer_type](**kwargs)
+
+    raise ValueError(
+        f"Fully connected layer type {layer_type} not recognized.  Must be one of {list(fc_layers.keys())}"
+    )
+
+
+def high_order_convolution_layers(layer_type: str, **kwargs):
+
+    if layer_type in convolutional_layers.keys():
+        return convolutional_layers[layer_type](**kwargs)
+
+    raise ValueError(
+        f"Convolutional layer type {layer_type} not recognized.  Must be one of {list(convolutional_layers.keys())}"
+    )
