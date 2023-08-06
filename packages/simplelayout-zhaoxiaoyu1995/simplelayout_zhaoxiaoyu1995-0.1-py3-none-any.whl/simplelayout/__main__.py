dba4d9556ebb79c869bbc9d50dbc6b883d26ad56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:40 +0000

    Add package simplelayout-zhaoxiaoyu1995/0.1/simplelayout_zhaoxiaoyu1995-0.1-py3-none-any.whl

---
+++ packages/simplelayout-zhaoxiaoyu1995/simplelayout_zhaoxiaoyu1995-0.1-py3-none-any.whl/simplelayout/__main__.py
@@ -0,0 +1,16 @@
+from simplelayout.cli import get_options
+from simplelayout.generator.core import generate_matrix
+from simplelayout.generator.utils import save_matrix, save_fig, make_dir
+
+
+def main():
+    options = get_options()
+    layout = generate_matrix(options.board_grid, options.unit_grid,
+                             options.unit_n, options.positions)
+    make_dir(options.outdir)
+    save_matrix(layout, options.outdir + '/' + options.file_name)
+    save_fig(layout, options.outdir + '/' + options.file_name)
+
+
+if __name__ == "__main__":
+    main()
