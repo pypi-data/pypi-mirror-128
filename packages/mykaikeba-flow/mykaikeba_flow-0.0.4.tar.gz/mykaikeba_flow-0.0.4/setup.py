fa486cc1cfe99eaad82929672351341179e76e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:42 +0000

    Add package mykaikeba-flow/0.0.4/mykaikeba_flow-0.0.4.tar.gz

---
+++ packages/mykaikeba-flow/mykaikeba_flow-0.0.4.tar.gz/mykaikeba_flow-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mykaikeba_flow", # Replace with your own username
+    version="0.0.4",
+    author="zidanewenqsh",
+    author_email="zidanewenqsh@163.com",
+    description="This is a mini but function completed neural networks framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
