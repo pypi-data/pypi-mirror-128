3b57917d0724c24f9d81a1f929d9613ac9830958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:33 +0000

    Add package tabby-connection-gateway/0.3.0/tabby-connection-gateway-0.3.0.tar.gz

---
+++ packages/tabby-connection-gateway/tabby-connection-gateway-0.3.0.tar.gz/tabby-connection-gateway-0.3.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['websockets>=9.1']
+
+setup(
+    name='tabby-connection-gateway',
+    version='0.3.0',
+    author='Eugene Pankov',
+    author_email='e@ajenti.org',
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'tabby-connection-gateway=tabby_connection_gateway.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license='MIT license',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='tabby',
+    packages=['tabby_connection_gateway'],
+    url='https://github.com/eugeny/tabby-connection-gateway',
+)
