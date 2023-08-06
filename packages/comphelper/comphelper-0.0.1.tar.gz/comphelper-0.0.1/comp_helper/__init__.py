cf3341f51b44de53c0ac07a9397325eb68427fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:19 +0000

    Add package comphelper/0.0.1/comphelper-0.0.1.tar.gz

---
+++ packages/comphelper/comphelper-0.0.1.tar.gz/comphelper-0.0.1/comp_helper/__init__.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def view():
+    pd.options.display.float_format = '{:,.2f}'.format  # shows cents!
+    return pd.set_option("display.width", None), pd.set_option("max_rows", None), pd.options.display.float_format
+
+
+def format_cols(df):
+    cols = df.columns
+    formatted_headers = [x.lower().replace(' ', '_') for x in cols]
+    return formatted_headers
