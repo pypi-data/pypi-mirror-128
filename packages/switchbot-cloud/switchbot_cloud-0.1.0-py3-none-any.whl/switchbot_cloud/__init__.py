6d498e279c2f59d1974d2d0f8ad007aa588d3284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:34 +0000

    Add package switchbot-cloud/0.1.0/switchbot_cloud-0.1.0-py3-none-any.whl

---
+++ packages/switchbot-cloud/switchbot_cloud-0.1.0-py3-none-any.whl/switchbot_cloud/__init__.py
@@ -0,0 +1,17 @@
+from typing import List
+from switchbot.devices import Device
+
+from switchbot.client import SwitchBotClient
+
+
+class SwitchBot:
+    def __init__(self, token: str):
+        self.client = SwitchBotClient(token)
+
+    def devices(self) -> List[Device]:
+        response = self.client.get('devices')
+        return [self.device(device['device_id'], **device)
+                for device in response['body']['device_list']]
+
+    def device(self, id: str, **extra) -> Device:
+        return Device.create(client=self.client, id=id, **extra)
