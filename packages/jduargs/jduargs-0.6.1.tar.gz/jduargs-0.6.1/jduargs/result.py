d116e9812254c55e6b8e544cc7b4af964896f2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:16 +0000

    Add package jduargs/0.6.1/jduargs-0.6.1.tar.gz

---
+++ packages/jduargs/jduargs-0.6.1.tar.gz/jduargs-0.6.1/jduargs/result.py
@@ -0,0 +1,11 @@
+class Result:
+    """Class description."""
+
+    def __init__(self):
+        """
+        Initialisation of the class.
+        """
+        pass
+
+    def __str__(self):
+        return
