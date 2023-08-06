5433f9766b67d195e7f812ec7d6409108530ed0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:10 +0000

    Add package verified-url/0.0.1/verified_url-0.0.1.tar.gz

---
+++ packages/verified-url/verified_url-0.0.1.tar.gz/verified_url-0.0.1/unittest/test_package_publication.py
@@ -0,0 +1,7 @@
+from checkurl import checkurl
+
+def main():
+    checkurl.check_urls_by_json_file('E:/Users/Maxime/Documents/PYTHON - URL_VALID/unitTest/input_url.json', 'E:/Users/Maxime/Documents/PYTHON - URL_VALID/unitTest')
+
+if __name__ == '__main__':
+    main()
