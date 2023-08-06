e5b06b955c26e10de9ac06250f4b4b75dd2e5293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:22 +0000

    Add package miniworldmaker/1.7.9/miniworldmaker-1.7.9.tar.gz

---
+++ packages/miniworldmaker/miniworldmaker-1.7.9.tar.gz/miniworldmaker-1.7.9/miniworldmaker/__init__.py
@@ -0,0 +1,16 @@
+import pkgutil
+from inspect import isclass
+from pkgutil import iter_modules
+from pathlib import Path
+from importlib import import_module
+
+__all__ = []
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    _module = loader.find_module(module_name).load_module(module_name)
+    globals()[module_name] = _module
+    for attribute_name in dir(_module):
+        attribute = getattr(_module, attribute_name)
+        if isclass(attribute):  
+            globals()[attribute_name] = attribute
+            __all__.append(attribute.__name__)
