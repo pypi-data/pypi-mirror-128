767552eb438ecef76667937ecf9dd24dd497c3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:53 +0000

    Add package USEofCS/0.1.10.1/USEofCS-0.1.10.1.tar.gz

---
+++ packages/USEofCS/USEofCS-0.1.10.1.tar.gz/USEofCS-0.1.10.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='USEofCS',
+      version='0.1.10.1',
+      author='zef',
+      author_email='zef.report.pypi@mail.ru',
+      descripdion='Python module to help you write your USE in computer science',
+      long_descripdion='Python module to help you write your USE in computer science',
+      packages=['USEofCS'])
