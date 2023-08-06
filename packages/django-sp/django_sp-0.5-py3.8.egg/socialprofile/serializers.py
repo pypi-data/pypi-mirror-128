7f0ebcf7b95316ecda7acf6175f8506f50c1aedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:44 +0000

    Add package django-sp/0.5/django_sp-0.5-py3.8.egg

---
+++ packages/django-sp/django_sp-0.5-py3.8.egg/socialprofile/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import authenticate
+from django.utils.translation import ugettext_lazy as _
+
+# from socialprofile.models import SocialProfile, Group
+from socialprofile.models import SocialProfile
+
+from rest_framework import serializers
+
+
+# Serializers define the API representation.
+class SocialProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = SocialProfile
+        fields = ("url", "username", "email", "is_staff")
+
+
+# class GroupSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Group
