bdd4f97a927affcb7593b041b004a442db988b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package pytorch-adapt/0.0.30/pytorch-adapt-0.0.30.tar.gz

---
+++ packages/pytorch-adapt/pytorch-adapt-0.0.30.tar.gz/pytorch-adapt-0.0.30/src/pytorch_adapt/adapters/mcd.py
@@ -0,0 +1,22 @@
+from ..containers import KeyEnforcer
+from ..hooks import MCDHook
+from .base_adapter import BaseGCAdapter
+
+
+class MCD(BaseGCAdapter):
+    """
+    Wraps [MCDHook][pytorch_adapt.hooks.mcd].
+    """
+
+    hook_cls = MCDHook
+
+    def inference_default(self, x, domain=None):
+        features = self.models["G"](x)
+        logits_list = self.models["C"](features)
+        logits = sum(logits_list)
+        return features, logits
+
+    def init_hook(self, hook_kwargs):
+        self.hook = self.hook_cls(
+            g_opts=[self.optimizers["G"]], c_opts=[self.optimizers["C"]], **hook_kwargs
+        )
