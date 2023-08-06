a7f82f889cdd57433dda77beb8fc007d41754179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:33 +0000

    Add package redbrick-sdk/0.6.12/redbrick_sdk-0.6.12-py3-none-any.whl

---
+++ packages/redbrick-sdk/redbrick_sdk-0.6.12-py3-none-any.whl/redbrick/__init__.py
@@ -0,0 +1,47 @@
+"""
+RedBrick SDK to enable powerful use cases.
+
+- To begin use: redbrick.get_project(api_key, org_id, project_id)
+- Start at https://app.redbrickai.com to create a project.
+- Learn more at https://docs.redbrickai.com
+
+
+"""
+
+from redbrick.common.context import RBContext
+from redbrick.common.enums import LabelType, StorageMethod
+from redbrick.project import RBProject
+from redbrick.organization import RBOrganization
+from redbrick.utils import version_check  # pylint: disable=cyclic-import
+from redbrick.repo import (
+    ExportRepo,
+    LabelingRepo,
+    LearningRepo,
+    Learning2Repo,
+    UploadRepo,
+    ProjectRepo,
+)
+
+version_check.version_check()
+
+
+def _populate_context(context: RBContext) -> RBContext:
+    context.export = ExportRepo(context.client)
+    context.labeling = LabelingRepo(context.client)
+    context.learning = LearningRepo(context.client)
+    context.learning2 = Learning2Repo(context.client)
+    context.upload = UploadRepo(context.client)
+    context.project = ProjectRepo(context.client)
+    return context
+
+
+def get_org(api_key: str, url: str, org_id: str) -> RBOrganization:
+    """Get redbrick organization object."""
+    context = _populate_context(RBContext(api_key, url))
+    return RBOrganization(context, org_id)
+
+
+def get_project(api_key: str, url: str, org_id: str, project_id: str) -> RBProject:
+    """Get project object."""
+    context = _populate_context(RBContext(api_key, url))
+    return RBProject(context, org_id, project_id)
---
+++ packages/redbrick-sdk/redbrick_sdk-0.6.12-py3-none-any.whl/redbrick/common/client.py
@@ -0,0 +1,83 @@
+"""Graphql Client responsible for make API requests."""
+
+
+from typing import Dict, Any
+import requests
+import aiohttp
+
+
+MAX_CONCURRENCY = 30
+
+
+class RBClient:
+    """Client to communicate with RedBrick AI GraphQL Server."""
+
+    def __init__(
+        self,
+        api_key: str,
+        url: str,
+    ) -> None:
+        """Construct RBClient."""
+        assert (
+            len(api_key) == 43
+        ), "Invalid Api Key length, make sure you've copied it correctly"
+        self.api_key = api_key
+        self.url = url.rstrip("/") + "/graphql/"
+        self.session = requests.Session()
+
+    def __del__(self) -> None:
+        """Garbage collect and close session."""
+        self.session.close()
+
+    def execute_query(self, query: str, variables: Dict[str, Any]) -> Any:
+        """Execute a graphql query."""
+        headers = {"ApiKey": self.api_key}
+
+        try:
+            response = self.session.post(
+                self.url, headers=headers, json={"query": query, "variables": variables}
+            )
+            res = {}
+
+            if response.status_code == 500:
+                raise ValueError(
+                    "Internal Server Error: You are probably using an invalid API key"
+                )
+            if response.status_code == 403:
+                raise PermissionError("Problem authenticating with Api Key")
+            if "errors" in response.json():
+                raise ValueError(response.json()["errors"][0]["message"])
+            if "data" in response.json():
+                res = response.json()["data"]
+            else:
+                res = response.json()
+            return res
+        except ValueError as error:
+            raise error
+
+    async def execute_query_async(
+        self, aio_client: aiohttp.ClientSession, query: str, variables: Dict[str, Any]
+    ) -> Any:
+        """Execute a graphql query using asyncio."""
+        headers = {"ApiKey": self.api_key}
+
+        try:
+            async with aio_client.post(
+                self.url, headers=headers, json={"query": query, "variables": variables}
+            ) as response:
+
+                response_data = await response.json()
+                res = {}
+                if response.status == 500:
+                    raise ValueError(
+                        "Internal Server Error: You are probably using an invalid API key"
+                    )
+                if "errors" in response_data:
+                    raise ValueError(response_data["errors"][0]["message"])
+                if "data" in response_data:
+                    res = response_data["data"]
+                else:
+                    res = response_data
+                return res
+        except ValueError as error:
+            raise error
