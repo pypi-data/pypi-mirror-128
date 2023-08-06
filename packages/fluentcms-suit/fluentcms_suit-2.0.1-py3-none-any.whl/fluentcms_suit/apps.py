b387aa58cada5d1f9ca74badb17d96d8df2cfe55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:47 +0000

    Add package fluentcms-suit/2.0.1/fluentcms_suit-2.0.1-py3-none-any.whl

---
+++ packages/fluentcms-suit/fluentcms_suit-2.0.1-py3-none-any.whl/fluentcms_suit/apps.py
@@ -0,0 +1,34 @@
+from django.apps import AppConfig
+from django.conf import settings
+
+
+class DefaultConfig(AppConfig):
+    label = name = "fluentcms_suit"
+
+    def ready(self):
+        try:
+            if "fluent_pages" in settings.INSTALLED_APPS:
+                from fluent_contents.extensions import plugin_pool
+                from fluent_pages.extensions import page_type_pool
+
+                from .admin import SuitAdminMixin
+
+                for p in page_type_pool.get_plugins():
+
+                    class PageTypeAdmin(SuitAdminMixin, p.model_admin):
+                        pass
+
+                    p.model_admin = PageTypeAdmin
+
+                for p in plugin_pool.get_plugins():
+                    try:
+
+                        class PluginAdmin(SuitAdminMixin, p.model_admin):
+                            pass
+
+                        p.model_admin = PluginAdmin
+                    except AttributeError:
+                        pass
+
+        except:
+            pass  # shit happens
