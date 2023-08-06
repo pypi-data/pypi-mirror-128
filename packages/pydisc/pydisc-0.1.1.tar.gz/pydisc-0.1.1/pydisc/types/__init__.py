694872b74eb38468edbb14161bb36ef79a31c3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:11 +0000

    Add package pydisc/0.1.1/pydisc-0.1.1.tar.gz

---
+++ packages/pydisc/pydisc-0.1.1.tar.gz/pydisc-0.1.1/pydisc/types/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .message import Message
+from .interaction import Interaction
---
+++ packages/pydisc/pydisc-0.1.1.tar.gz/pydisc-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    readme = f.read()
+setup(
+    name="pydisc",
+    version="0.1.1",
+    description="a discord bot wrapper for python have slash command",
+    url="https://github.com/pydisc/pydisc",
+    author="cutebear0123",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["pydisc", "pydisc.types", "pydisc.setup"],
+    install_requires=["websockets", "requests"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
