2ba8e0d1acc9a8aaa2253f39e64695dcb09dcd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:58 +0000

    Add package pydorks/0.6/pydorks-0.6.tar.gz

---
+++ packages/pydorks/pydorks-0.6.tar.gz/pydorks-0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name="pydorks",
+    version='0.6',
+    license='Eclipse Public License 2.0',
+    author="billythegoat356",
+    author_email="<billythegoat356356@gmail.com>",
+    description='Recherchez du contenu sur Google efficacement.',
+    long_description='Bientot ;)',
+    keywords=['python', 'google', 'google dorks', 'dorks', 'gdorks'],
+    packages=['pydorks']
+)
