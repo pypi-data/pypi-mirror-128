39a664c3913279b2f512952a7f38b81d0ccde355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:02 +0000

    Add package dicom-validator/0.3.3/dicom_validator-0.3.3-py3-none-any.whl

---
+++ packages/dicom-validator/dicom_validator-0.3.3-py3-none-any.whl/dcm_spec_tools/tests/test_utils.py
@@ -0,0 +1,17 @@
+import os
+
+
+def fixture_path():
+    return os.path.join(os.path.dirname(__file__), 'fixtures')
+
+
+def spec_fixture_path():
+    return os.path.join(fixture_path(), 'docbook')
+
+
+def json_fixture_path():
+    return os.path.join(fixture_path(), 'json')
+
+
+def dicom_fixture_path():
+    return os.path.join(fixture_path(), 'dicom')
