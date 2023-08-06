b4c15ccbad5e987f8355f80c8ccfff30b2d68fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:42 +0000

    Add package gepics/2021.11.2/gepics-2021.11.2.tar.gz

---
+++ packages/gepics/gepics-2021.11.2.tar.gz/gepics-2021.11.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='gepics',
+    version='2021.11.2',
+    url="https://github.com/michel4j/gepics",
+    license='MIT',
+    author='Michel Fodje',
+    author_email='michel4j@gmail.com',
+    description='Python GObject Wrapper for EPICS Process Variables',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='epics gobject development',
+    py_modules=['gepics'],
+    install_requires=requirements,
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
