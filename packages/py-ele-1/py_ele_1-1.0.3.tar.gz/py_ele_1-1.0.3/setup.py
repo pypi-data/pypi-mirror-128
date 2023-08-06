494cc7e761c4f336f3d005bf7d694de75424a753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:35 +0000

    Add package py-ele-1/1.0.3/py_ele_1-1.0.3.tar.gz

---
+++ packages/py-ele-1/py_ele_1-1.0.3.tar.gz/py_ele_1-1.0.3/setup.py
@@ -0,0 +1,35 @@
+# try:
+#     from setuptools import setup
+# except:
+#     from distutils.core import setup
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+install_requires = [
+    # 'numpy>=1.11.1',
+    # 'pandas>=0.19.0'
+    'requests'
+]
+platforms = ['linux/Windows']
+# 模块相关的元数据
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+setuptools.setup(
+    name='py_ele_1',  # 对外我们模块的名字
+    version='1.0.3',  # 版本号
+    description='测试本地发布模块py_ele_1',  # 描述
+    author='peter.peng',  # 作者
+    author_email='peng.peter@qq.com',
+    py_modules=['py_ele_1.Person'],  # 要发布的模块
+    url="https://gitee.com/xx/xxx.git",
+    license="MIT",
+    platforms=platforms,
+    classifiers=classifiers,
+    include_package_data=True,
+    install_requires=install_requires,
+    packages=setuptools.find_packages()
+)
