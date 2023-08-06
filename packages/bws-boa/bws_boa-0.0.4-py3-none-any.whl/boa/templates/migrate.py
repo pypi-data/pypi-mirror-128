915924b87e71af68443fdd5eb02a99ccb67e2d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:41 +0000

    Add package bws-boa/0.0.4/bws_boa-0.0.4-py3-none-any.whl

---
+++ packages/bws-boa/bws_boa-0.0.4-py3-none-any.whl/boa/templates/migrate.py
@@ -0,0 +1,12 @@
+template="""
+from app import app, migrate
+from boa.admin.bootstrap import bootstrap
+
+
+bootstrap(app, None, ["migrations"])
+
+
+if __name__== '__main__':
+    migrate.run()
+
+"""
