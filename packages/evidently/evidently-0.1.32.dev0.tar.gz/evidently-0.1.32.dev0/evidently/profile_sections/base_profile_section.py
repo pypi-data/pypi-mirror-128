7f7e553b0443c675d269bbc041021eb6013cbefc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:02 +0000

    Add package evidently/0.1.32.dev0/evidently-0.1.32.dev0.tar.gz

---
+++ packages/evidently/evidently-0.1.32.dev0.tar.gz/evidently-0.1.32.dev0/evidently/profile_sections/base_profile_section.py
@@ -0,0 +1,18 @@
+import abc
+from typing import List
+
+from evidently.analyzers.base_analyzer import Analyzer
+
+
+class ProfileSection:
+    @abc.abstractmethod
+    def analyzers(self) -> List[Analyzer]:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def part_id(self) -> str:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def calculate(self, analyzers_results):
+        raise NotImplementedError()
