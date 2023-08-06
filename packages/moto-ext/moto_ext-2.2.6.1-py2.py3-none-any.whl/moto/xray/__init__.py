428745966da25ef6527b2b8605000ceeed06c63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:26 +0000

    Add package moto-ext/2.2.6.1/moto_ext-2.2.6.1-py2.py3-none-any.whl

---
+++ packages/moto-ext/moto_ext-2.2.6.1-py2.py3-none-any.whl/moto/xray/__init__.py
@@ -0,0 +1,6 @@
+from .models import xray_backends
+from ..core.models import base_decorator
+from .mock_client import mock_xray_client, XRaySegment  # noqa
+
+xray_backend = xray_backends["us-east-1"]
+mock_xray = base_decorator(xray_backends)
