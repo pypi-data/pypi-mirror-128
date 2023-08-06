701036ae2e26c34e4dabe5b0e43a11000bc39f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:53 +0000

    Add package check-jsonschema/0.7.0/check-jsonschema-0.7.0.tar.gz

---
+++ packages/check-jsonschema/check-jsonschema-0.7.0.tar.gz/check-jsonschema-0.7.0/src/check_jsonschema/__init__.py
@@ -0,0 +1,17 @@
+from .checker import SchemaChecker
+from .parse_cli import parse_args
+
+
+def main(cli_args=None):
+    args = parse_args(cli_args)
+    checker = SchemaChecker(
+        args.schemafile,
+        args.instancefiles,
+        cache_filename=args.cache_filename,
+        disable_cache=args.no_cache,
+        format_enabled=not args.disable_format,
+        default_instance_filetype=args.default_filetype,
+        traceback_mode=args.traceback_mode,
+    )
+    checker.run()
+    print("ok -- validation done")
