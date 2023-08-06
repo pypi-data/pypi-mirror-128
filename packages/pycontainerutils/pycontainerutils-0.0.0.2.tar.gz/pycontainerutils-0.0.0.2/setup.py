8687b8380ae7024c6ebc4e3c06c7a40e2ed168e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:47 +0000

    Add package pycontainerutils/0.0.0.2/pycontainerutils-0.0.0.2.tar.gz

---
+++ packages/pycontainerutils/pycontainerutils-0.0.0.2.tar.gz/pycontainerutils-0.0.0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+      name="pycontainerutils",    # 패키지 명
+      version='0.0.0.2',
+      license='MIT',
+      author='greenrain',
+      author_email='kdwkdw0078@gmail.com',
+      description='docker container utilities',
+      long_description=open('README.md').read(),
+      url="https://github.com/greenrain78",
+      packages=setuptools.find_packages(),
+)
