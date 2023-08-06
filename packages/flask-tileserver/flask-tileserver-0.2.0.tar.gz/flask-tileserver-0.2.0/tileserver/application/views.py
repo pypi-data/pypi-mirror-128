d86aee76e572b06524b24c7a74ca81c199e5e6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:46 +0000

    Add package flask-tileserver/0.2.0/flask-tileserver-0.2.0.tar.gz

---
+++ packages/flask-tileserver/flask-tileserver-0.2.0.tar.gz/flask-tileserver-0.2.0/tileserver/application/views.py
@@ -0,0 +1,29 @@
+import logging
+
+from flask import render_template
+from flask.views import View
+
+from tileserver import utilities
+from tileserver.application import app
+from tileserver.examples import get_data_path
+
+logger = logging.getLogger(__name__)
+
+
+class Viewer(View):
+    def dispatch_request(self):
+        return render_template("tileviewer.html")
+
+
+@app.context_processor
+def inject_context():
+    try:
+        filename = app.config["filename"]
+    except KeyError:
+        logger.error("No filename set in app config. Using sample data.")
+        filename = get_data_path("bahamas_rgb.tif")
+    tile_source = utilities.get_tile_source(filename)
+    context = utilities.get_meta_data(tile_source)
+    context["bounds"] = utilities.get_tile_bounds(tile_source, projection="EPSG:4326")
+    context["filename"] = filename
+    return context
