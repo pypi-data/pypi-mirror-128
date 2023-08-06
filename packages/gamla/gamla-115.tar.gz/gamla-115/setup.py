b57a9276fd47f8517ac43909249ec86770674a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:30 +0000

    Add package gamla/115/gamla-115.tar.gz

---
+++ packages/gamla/gamla-115.tar.gz/gamla-115/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    _LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name="gamla",
+    version="115",
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_namespace_packages(),
+    install_requires=[
+        "async-timeout",
+        "dataclasses_json",
+        "heapq_max",
+        "httpx",
+        "pytest-asyncio",
+        "pytest",
+        "requests",
+        "tabulate",
+        "toolz",
+        "yappi",
+    ],
+)
