7534063ec13bfb8367cbca3abec8c47c3357f416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:52 +0000

    Add package cryptol/2.12.0/cryptol-2.12.0.tar.gz

---
+++ packages/cryptol/cryptol-2.12.0.tar.gz/cryptol-2.12.0/cryptol/__init__.py
@@ -0,0 +1,37 @@
+"""Cryptol bindings from Python. Use :py:func:`cryptol.connect` to start a new connection."""
+
+from __future__ import annotations
+
+import base64
+import os
+from enum import Enum
+from dataclasses import dataclass
+from distutils.spawn import find_executable
+from typing import Any, List, NoReturn, Optional, Union
+from typing_extensions import Literal
+
+import argo_client.interaction as argo
+from argo_client.interaction import HasProtocolState
+from argo_client.connection import DynamicSocketProcess, ServerConnection, ServerProcess, StdIOProcess, HttpProcess
+from . import cryptoltypes
+from . import solver
+from .bitvector import BV
+from .commands import *
+from .connection import *
+# import everything from `.synchronous` except `connect` and `connect_stdio`
+#  (since functions with those names were already imported from `.connection`)
+from .synchronous import Qed, Safe, Counterexample, Satisfiable, Unsatisfiable, CryptolSyncConnection
+# and add an alias `sync` for the `synchronous` module
+from . import synchronous
+sync = synchronous
+
+__all__ = ['bitvector', 'commands', 'connection', 'cryptoltypes', 'opaque', 'single_connection', 'solver', 'synchronous']
+
+
+def fail_with(x : Exception) -> NoReturn:
+    "Raise an exception. This is valid in expression positions."
+    raise x
+
+
+def cry(string : str) -> cryptoltypes.CryptolCode:
+    return cryptoltypes.CryptolLiteral(string)
