3ccbe562ea738ecc8f9adbe14a983a638abdaa02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:38 +0000

    Add package makezip/1.0/makezip-1.0.tar.gz

---
+++ packages/makezip/makezip-1.0.tar.gz/makezip-1.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding=utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='makezip',
+    version='1.0',
+    author='wp',
+    author_email='627618076@qq.com',
+    description='A repackage zip module, base on zipfile in standard library.',
+    py_modules = ['makezip'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ]
+)
