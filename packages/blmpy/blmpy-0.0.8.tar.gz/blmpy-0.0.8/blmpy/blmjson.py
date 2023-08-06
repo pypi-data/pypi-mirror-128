557daf594e93de96508767b4c1ab575e973b8e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:29 +0000

    Add package blmpy/0.0.8/blmpy-0.0.8.tar.gz

---
+++ packages/blmpy/blmpy-0.0.8.tar.gz/blmpy-0.0.8/blmpy/blmjson.py
@@ -0,0 +1,19 @@
+# import os
+
+# class blmos:
+#     def get_files_from_directory(self, dir_path, extension_type=''):
+#         files = [f.path for f in os.scandir(dir_path) if f.is_file()]
+#         if len(extension_type) > 0:
+#             files = [f.path for f in os.scandir(dir_path) if f.is_file() and f.path.endswith('.'+extension_type)]
+#         return files
+import json
+
+def load_json_file(filename):
+    with open(filename) as f:
+        tmp_data = f.read()
+        while not (tmp_data[0].startswith('[') or tmp_data[0].startswith('{')):
+            tmp_data = tmp_data[1:]
+    return json.loads(tmp_data)
+
+
+        
