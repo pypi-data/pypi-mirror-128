f29ca907106df49a3569439d1630403e90d409af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:08 +0000

    Add package toolchest-client/0.7.27/toolchest_client-0.7.27-py3-none-any.whl

---
+++ packages/toolchest-client/toolchest_client-0.7.27-py3-none-any.whl/toolchest_client/api/exceptions.py
@@ -0,0 +1,24 @@
+"""
+toolchest_client.api.exceptions
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module contains custom exceptions used for the Toolchest client.
+"""
+
+
+class ToolchestException(OSError):
+    """There was an unknown exception that occurred during your
+    Toolchest job.
+    """
+
+
+class ToolchestKeyError(ToolchestException):
+    """Invalid Toolchest auth key."""
+
+
+class DataLimitError(ToolchestException):
+    """Data limit for Toolchest exceeded."""
+
+
+class ToolchestJobError(ToolchestException):
+    """An error occurred when running the job instance."""
