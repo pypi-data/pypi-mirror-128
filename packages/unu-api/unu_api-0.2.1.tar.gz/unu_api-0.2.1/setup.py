132917d23e397be3671ab05bf16096fc233d52e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:41 +0000

    Add package unu-api/0.2.1/unu_api-0.2.1.tar.gz

---
+++ packages/unu-api/unu_api-0.2.1.tar.gz/unu_api-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['unu_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'unu-api',
+    'version': '0.2.1',
+    'description': 'Библиотека для интеграции с биржой микрозадач unu.im',
+    'long_description': None,
+    'author': 'Egorov Egor',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
