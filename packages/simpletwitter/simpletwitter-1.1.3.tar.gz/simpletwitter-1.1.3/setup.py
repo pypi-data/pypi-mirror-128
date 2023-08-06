3903bafe4c2ca1c8c46728a37fd80f650177e82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:21 +0000

    Add package simpletwitter/1.1.3/simpletwitter-1.1.3.tar.gz

---
+++ packages/simpletwitter/simpletwitter-1.1.3.tar.gz/simpletwitter-1.1.3/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.3'
+DESCRIPTION = 'Python package for making twitter bot with less code'
+LONG_DESCRIPTION = '''
+This package can be used to make your own twitter bot with less code
+
+import the package using pip install
+
+usage:
+
+    bot = simpletwitter(email, password, no_of_tweets, username)
+    bot.login()
+    bot.like_tweet(hashtags) #like the twitte
+    bot.Unlike_liked_tweets(5) #unlike the liked tweet
+
+This bot module was created by Abipravi
+any one who want to ugrate and add more feature to the module can create an issue in github repository and make your PR's
+'''
+
+setup(
+    name="simpletwitter",
+    version=VERSION,
+    author="Abipravi",
+    author_email="darkparadise877@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'first package', 'twitter',
+              'twitterbot_abipravi', 'make you own bot'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    project_urls={
+        'Homepage': 'https://github.com/pravee42/Twitter_bot_module',
+    },
+    install_requires=[
+        'APScheduler',
+        'selenium',
+        'requests',
+        'tweepy',
+        'webdriver-manager',
+    ]
+
+)
