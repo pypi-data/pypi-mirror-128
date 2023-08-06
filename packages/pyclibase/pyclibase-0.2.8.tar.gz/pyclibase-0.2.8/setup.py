7e3f1e78e26f2c328f4ec2533331c44e740fa657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:01 +0000

    Add package pyclibase/0.2.8/pyclibase-0.2.8.tar.gz

---
+++ packages/pyclibase/pyclibase-0.2.8.tar.gz/pyclibase-0.2.8/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyclibase",
+    version="0.2.8",
+    author="Sungkeun Kim",
+    author_email="danguria@gmail.com",
+    description = "A base class of python cli",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ksungkeun84/pyclibase",
+    project_urls={
+        "Bug Tracker": "https://github.com/ksungkeun84/pyclibase/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires = [
+        "setuptools>=42",
+        "wheel",
+        "rich",
+        "pyfiglet"
+    ],
+)
