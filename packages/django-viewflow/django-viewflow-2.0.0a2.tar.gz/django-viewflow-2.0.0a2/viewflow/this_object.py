70d5ce7829fc6aea852845c9bd9929b33d07353d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:32 +0000

    Add package django-viewflow/2.0.0a2/django-viewflow-2.0.0a2.tar.gz

---
+++ packages/django-viewflow/django-viewflow-2.0.0a2.tar.gz/django-viewflow-2.0.0a2/viewflow/this_object.py
@@ -0,0 +1,57 @@
+"""Forward-reference for class-body declarations"""
+
+# Copyright (c) 2017-2020, Mikhail Podgurskiy
+# All Rights Reserved.
+
+# This work is dual-licensed under AGPL defined in file 'LICENSE' with
+# LICENSE_EXCEPTION and the Commercial licence defined in file 'COMM_LICENSE',
+# which is part of this source code package.
+from typing import Any
+
+
+class ThisMethod(object):
+    """Reference to a method"""
+    def __init__(self, propname, methodname):
+        self._propname = propname
+        self._methodname = methodname
+
+    def resolve(self, instance):
+        # TODO meaningfull exception
+        prop = getattr(instance, self._propname)
+        method = getattr(instance, f'_this_{self._methodname}')
+        return method(prop)
+
+
+class ThisObject(object):
+    """Helper for forward references"""
+
+    def __init__(self, name):  # noqa D102
+        self.name = name
+
+    def resolve(self, instance: object) -> Any:
+        # TODO meaningfull exception
+        return getattr(instance, self.name)
+
+    def __getattr__(self, name):
+        return ThisMethod(self.name, name)
+
+
+class This(object):
+    """Helper for building forward references.
+
+    The rationale is ability to specify references to the class
+    attributes and methods before they are declared.
+
+    `this` is like a `self` but for the class body.
+    """
+    def resolve(self, instance, this_ref):
+        if isinstance(this_ref, (ThisObject, ThisMethod)):
+            return this_ref.resolve(instance)
+        else:
+            return this_ref
+
+    def __getattr__(self, name):
+        return ThisObject(name)
+
+
+this = This()
