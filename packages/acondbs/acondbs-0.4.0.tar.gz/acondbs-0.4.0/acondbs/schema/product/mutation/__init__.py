2da5bc57b3a9951dfa3079aea7bd32d5ea2715a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:59 +0000

    Add package acondbs/0.4.0/acondbs-0.4.0.tar.gz

---
+++ packages/acondbs/acondbs-0.4.0.tar.gz/acondbs-0.4.0/acondbs/schema/product/mutation/__init__.py
@@ -0,0 +1,25 @@
+from .product import CreateProduct, UpdateProduct, DeleteProduct  # noqa: F401
+from .product_file_path import (  # noqa: F401
+    CreateProductFilePath,
+    UpdateProductFilePath,
+    DeleteProductFilePath,
+)
+from .product_type import (  # noqa: F401
+    CreateProductType,
+    UpdateProductType,
+    DeleteProductType,
+)
+from .product_relation_type import (  # noqa: F401
+    CreateProductRelationTypes,
+    UpdateProductRelationType,
+    DeleteProductRelationTypes,
+)
+from .product_relation import (  # noqa: F401
+    CreateProductRelation,
+    DeleteProductRelation,
+)
+from .field import (  # noqa: F401
+    CreateField,
+    UpdateField,
+    DeleteField,
+)
