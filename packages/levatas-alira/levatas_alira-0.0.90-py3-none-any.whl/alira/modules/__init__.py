d730f79029da6215da48eaeb515d281423d937a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:18 +0000

    Add package levatas-alira/0.0.90/levatas_alira-0.0.90-py3-none-any.whl

---
+++ packages/levatas-alira/levatas_alira-0.0.90-py3-none-any.whl/alira/modules/__init__.py
@@ -0,0 +1,5 @@
+from alira.modules.map import Map
+from alira.modules.dashboard import Dashboard
+from alira.modules.s3 import S3
+from alira.modules.selection import Selection
+from alira.modules.flagging import Flagging, CostSensitiveFlagging
