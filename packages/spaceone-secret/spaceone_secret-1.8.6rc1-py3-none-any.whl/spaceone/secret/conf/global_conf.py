f9f672ba0456dc34bd3f62119099f0329f9a168c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package spaceone-secret/1.8.6rc1/spaceone_secret-1.8.6rc1-py3-none-any.whl

---
+++ packages/spaceone-secret/spaceone_secret-1.8.6rc1-py3-none-any.whl/spaceone/secret/conf/global_conf.py
@@ -0,0 +1,50 @@
+DATABASE_AUTO_CREATE_INDEX = True
+DATABASE_CASE_INSENSITIVE_INDEX = False
+DATABASES = {
+    'default': {
+        'db': 'secret',
+        'host': 'localhost',
+        'port': 27017,
+        'username': '',
+        'password': ''
+    }
+}
+
+CACHES = {
+    'default': {},
+    'local': {
+        'backend': 'spaceone.core.cache.local_cache.LocalCache',
+        'max_size': 128,
+        'ttl': 300
+    }
+}
+
+HANDLERS = {}
+
+BACKEND = ""
+CONNECTORS = {
+    'IdentityConnector': {},
+    'AWSSecretManagerConnector': {},
+    'VaultConnector': {
+       # 'url': 'http://vault:8200',
+       # 'token': 'myroot'
+    },
+    'ConsulConnector': {
+        'host': 'consul',
+        'port': 8500
+    }
+}
+
+ENDPOINTS = {}
+
+LOG = {
+    'filters': {
+        'masking': {
+            'rules': {
+                'Secret.create': [
+                    'data'
+                ]
+            }
+        }
+    }
+}
