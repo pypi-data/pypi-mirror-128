5e23eee9fc0fa61377561925be34e5fa5aeb5e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:37 +0000

    Add package vanoma-api-utils/0.5.0/vanoma-api-utils-0.5.0.tar.gz

---
+++ packages/vanoma-api-utils/vanoma-api-utils-0.5.0.tar.gz/vanoma-api-utils-0.5.0/vanoma_api_utils/django/fields.py
@@ -0,0 +1,17 @@
+import uuid
+from typing import Any
+from django.db import models
+
+
+class PrimaryKeyField(models.UUIDField):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        kwargs["editable"] = False
+        kwargs["primary_key"] = True
+        kwargs["default"] = uuid.uuid4
+        super().__init__(*args, **kwargs)
+
+
+class StringField(models.CharField):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        kwargs["max_length"] = 150
+        super().__init__(*args, **kwargs)
