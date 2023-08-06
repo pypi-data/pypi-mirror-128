164e643987720d262a028bc26279edd56db583db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:04 +0000

    Add package urbandictionary-fixed/2.0/urbandictionary-fixed-2.0.tar.gz

---
+++ packages/urbandictionary-fixed/urbandictionary-fixed-2.0.tar.gz/urbandictionary-fixed-2.0/setup.py
@@ -0,0 +1,21 @@
+import multiprocessing
+from setuptools import setup
+
+setup(name='urbandictionary-fixed',
+    version='2.0',
+    py_modules=['urbandictionary'],
+    author='Endlessz',
+    author_email='lckrep@protonmail.com',
+    license='MIT',
+    description='Simple Python wrapper for Urban Dictionary API. Forked and fixed from Bocong',
+    url='https://github.com/Endlesszombiez/urbandictionary-py',
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ],
+    keywords='urban dictionary python api wrapper',
+    )
