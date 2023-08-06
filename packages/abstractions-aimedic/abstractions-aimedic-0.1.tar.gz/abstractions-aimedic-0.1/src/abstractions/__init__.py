97f7a40d5a506b85865fa40f20d0ea1230a3b814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:37 +0000

    Add package abstractions-aimedic/0.1/abstractions-aimedic-0.1.tar.gz

---
+++ packages/abstractions-aimedic/abstractions-aimedic-0.1.tar.gz/abstractions-aimedic-0.1/src/abstractions/__init__.py
@@ -0,0 +1,6 @@
+from .base_class import BaseClass
+from .model_builder import ModelBuilderBase
+from .data_loader import DataLoaderBase
+from .evaluator import EvaluatorBase
+from .preprocessor import PreprocessorBase
+from .augmentor import AugmentorBase
