4430248c253989925fd36cddc439b3a86b1ce812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:26 +0000

    Add package looker-ingestion/0.0.2/looker_ingestion-0.0.2.tar.gz

---
+++ packages/looker-ingestion/looker_ingestion-0.0.2.tar.gz/looker_ingestion-0.0.2/looker_ingestion/load_s3.py
@@ -0,0 +1,48 @@
+""" To be imported to extraction scripts to push data to S3. """
+
+import os
+import shutil
+import tempfile
+import boto3
+import json
+import logging
+
+BUCKET_NAME = os.getenv("bucket_name")
+
+def load_s3_json(dataframe, local_file_name, output_filename, s3_bucket=BUCKET_NAME):
+    """ This saves a json locally to a temporary file and then uploads it """
+
+    temp_dir = tempfile.TemporaryDirectory()
+    input_filename = os.path.join(temp_dir.name, local_file_name)
+    if isinstance(dataframe, dict) or isinstance(dataframe, list):
+        with open(input_filename, 'w') as f:
+            json.dump(dataframe, f)
+    else:
+        print("Invalid instance type; please use only JSON or Dataframe")
+        return
+    load_s3(s3_bucket, input_filename, output_filename)
+    # remove temp directory if done
+    if os._exists(temp_dir.name):
+        shutil.rmtree(temp_dir.name)
+
+def load_s3(s3_bucket, input_filename, output_filename):
+    """ Pushes file to S3. """
+
+    s3_storage = boto3.resource("s3")
+    s3_storage.meta.client.upload_file(input_filename, s3_bucket, output_filename)
+    logging.info(
+        f"COMPLETE: {input_filename} loaded into \
+    s3:// {s3_bucket} as {output_filename}"
+    )
+
+def read_json(prefix, s3_bucket=BUCKET_NAME):
+    """ Given a key within an S3 buckets, reads through all files and returns content"""
+    s3_storage = boto3.resource("s3")
+    my_bucket = s3_storage.Bucket(s3_bucket)
+    json_objects = []
+    for object_summary in my_bucket.objects.filter(Prefix=prefix):
+        content_object = s3_storage.Object(s3_bucket, object_summary.key)
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        json_content = [json.loads(line) for line in file_content.splitlines()]
+        json_objects.extend(json_content)
+    return json_objects
---
+++ packages/looker-ingestion/looker_ingestion-0.0.2.tar.gz/looker_ingestion-0.0.2/tests/test_sync_data.py
@@ -0,0 +1,34 @@
+import sys
+import os
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+looker_ingestion_dir = os.path.join(parentdir, 'looker_ingestion')
+sys.path.append(parentdir)
+sys.path.append(looker_ingestion_dir)
+
+from looker_ingestion import sync_data
+from datetime import datetime, timedelta
+
+def test_extract_query_details():
+    test_query_name, test_query_body = sync_data.extract_query_details("../tests/test_query.json")
+
+    assert test_query_name == "test_query_name"
+    assert test_query_body["model"] == "test_model"
+    assert test_query_body["sorts"][1] == "sort_by_name"
+
+
+def test_find_date_range():
+    def convert_string(actual_datetime):
+        return actual_datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+    today = datetime.now().replace(microsecond=0)
+    one_day_ago = (today - timedelta(days = 1)).replace(microsecond=0)
+    two_days_ago = (today - timedelta(days = 2)).replace(microsecond=0)
+    fortynine_days_ago = (today - timedelta(days = 49)).replace(microsecond=0)
+    fifty_days_ago = (today - timedelta(days = 50)).replace(microsecond=0)
+    today_string = today.strftime('%Y-%m-%d %H:%M:%S')
+
+    assert sync_data.find_date_range(today_string) == None
+    assert sync_data.find_date_range(convert_string(one_day_ago)) == [one_day_ago, today]
+    assert sync_data.find_date_range(convert_string(two_days_ago)) == [two_days_ago, one_day_ago]
+    assert sync_data.find_date_range(convert_string(fifty_days_ago)) == [fifty_days_ago, fortynine_days_ago]
