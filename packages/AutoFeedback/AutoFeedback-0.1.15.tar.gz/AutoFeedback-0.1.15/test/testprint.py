e976e9051a94934186294c948d3f2b20ced21df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:27 +0000

    Add package AutoFeedback/0.1.15/AutoFeedback-0.1.15.tar.gz

---
+++ packages/AutoFeedback/AutoFeedback-0.1.15.tar.gz/AutoFeedback-0.1.15/test/testprint.py
@@ -0,0 +1,11 @@
+import unittest
+import numpy as np
+import AutoFeedback.variable_error_messages as vc
+
+
+class UnitTests(unittest.TestCase) :
+    def test_print(self):
+        assert(vc.output_check("this\\nand that",executable="test/printtest.py"))
+
+    def test_notprint(self):
+        assert(not vc.output_check("this and that",executable="test/printtest.py"))
