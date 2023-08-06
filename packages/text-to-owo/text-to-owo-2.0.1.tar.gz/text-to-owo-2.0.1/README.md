6256ed025e1a35be11466d7760e0b35a89bf74bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:45 +0000

    Add package text-to-owo/2.0.1/text-to-owo-2.0.1.tar.gz

---
+++ packages/text-to-owo/text-to-owo-2.0.1.tar.gz/text-to-owo-2.0.1/README.md
@@ -0,0 +1,83 @@
+# OwO
+
+Yet another owoifier. This specific one is a port of @zuzak's JavaScript [OWO](https://github.com/zuzak/owo) into Python, because we needed this to be in even more languages.
+
+# Install
+
+1.  Run the command `pip install text-to-owo` to install the package on your system.
+2.  Add `import owo` to the beginning of your script.
+
+# Usage
+
+## Constants
+
+<code>owo.**PREFIXES**</code>
+
+Tuple containing default prefixes
+
+<br/>
+
+<code>owo.**SUFFIXES**</code>
+
+Tuple containing default suffixes
+
+<br/>
+
+<code>owo.**SUBSTITUTIONS**</code>
+
+Dict containing default substitutions
+
+
+## Functions
+
+<code>owo.**add_prefix**(*input_string*, *prefixes=owo.PREFIXES*)</code>
+
+Appends a random prefix to the beginning of the string.
+Custom prefixes are optional and must be passed as either a list or tuple of strings.
+
+<br/>
+
+<code>owo.**add_suffix**(*input_string*, *suffixes=owo.SUFFIXES*)</code>
+
+Appends a random suffix to the end of the string.
+Custom suffixes are optional and must be passed as either a list or tuple of strings.
+
+<br/>
+
+<code>owo.**add_affixes**(*input_string*, *prefixes=owo.PREFIXES*, *suffixes=owo.SUFFIXES*)</code>
+
+Appends a random prefix and suffix to the beginning and end of the string.
+Custom prefixes and suffixes are optional and must be passed as either a list or tuple of strings.
+
+<br/>
+
+<code>owo.**substitute**(*input_string*, *substitutions=owo.SUBSTITUTIONS*)</code>
+
+Turns the text into owo speak without adding any prefixes or suffixes.
+Custom substitutions are optional and must be passed as dictionary in the form `{"key":"replacement"}`.
+
+<br/>
+
+<code>owo.**owo**(*input_string*, *prefixes=owo.PREFIXES*, *suffixes=owo.SUFFIXES*, *substitutions=owo.SUBSTITUTIONS*)</code>
+
+Both substitutes and adds prefixes and suffixes.
+Optional prefixes, suffixes, and substitutions are the same as in other methods.
+
+<br/>
+
+For all optional arguments the given iterable/dict will replace the default values, not add to them.
+
+All functions take a single string as input and output a string.
+
+
+# Example
+
+```python
+import owo
+
+print(owo.owo("I have no mouth and I must scream"))
+# HIIII! I haz nu mouth and I must scweam XDDD
+
+print(owo.substitute("I have no mouth and I must scream"))
+# I haz nu mouth and I must scweam
+```
---
+++ packages/text-to-owo/text-to-owo-2.0.1.tar.gz/text-to-owo-2.0.1/owo/__init__.py
@@ -0,0 +1,149 @@
+'''owofies text'''
+
+import random
+
+__version__ = "2.0.1"
+__author__ = "Pie Thrower"
+
+
+PREFIXES = (
+    "<3 ",
+    "0w0 ",
+    "H-hewwo?? ",
+    "HIIII! ",
+    "Haiiii! ",
+    "Huohhhh. ",
+    "OWO ",
+    "OwO ",
+    "UwU ",
+    "Whats this? "
+)
+
+SUFFIXES = (
+    " ( ˘ ³˘)♥",
+    " ( ͡° ᴥ ͡°)",
+    " (´・ω・｀)",
+    " (ʘᗩʘ\')",
+    " (இωஇ )",
+    " (๑•́ ₃ •̀๑)",
+    " (• o •)",
+    " (⁎˃ᆺ˂)",
+    " (╯﹏╰）",
+    " (●´ω｀●)",
+    " (◠‿◠✿)",
+    " (✿ ♡‿♡)",
+    " (❁´◡`❁)",
+    " (　\'◟ \')",
+    " (人◕ω◕)",
+    " (；ω；)",
+    " (｀へ´)",
+    " ._.",
+    " :3",
+    " :D",
+    " :P",
+    " ;-;",
+    " ;3",
+    " ;_;",
+    " :O",
+    " <{^v^}>",
+    " >_<",
+    " >_>",
+    " UwU",
+    " XDDD",
+    " \\°○°/",
+    " ^-^",
+    " ^_^",
+    " x3",
+    " xD",
+    " ÙωÙ",
+    " ʕʘ‿ʘʔ",
+    " ʕ•ᴥ•ʔ",
+    " ミ(．．)ミ",
+    " ㅇㅅㅇ",
+    ", fwendo",
+    "（＾ｖ＾）",
+    " (　\"◟ \")",
+    " *nuzzles u*",
+    " x3c",
+    " nya~"
+)
+
+SUBSTITUTIONS = {
+    "love": "wuv",  # keep before l->w
+    "Love": "Wuv",
+    "loving": "wuving",
+    "Loving": "Wuving",
+    "r": "w",
+    "l": "w",
+    "R": "W",
+    "L": "W",
+    # "ow": "OwO",
+    "th ": "f ",
+    "no": "nu",
+    "No": "Nu",
+    "has": "haz",
+    "Has": "Haz",
+    "have": "haz",
+    "Have": "Haz",
+    " says": " sez",
+    " Says": " Sez",
+    "you": "uu",
+    "i've": "i",
+    "I've": "I",
+    "the ": "da ",
+    "The ": "Da ",
+    "qu": "qw",
+    "Qu": "Qw",
+    "pause ": "paws ",
+    "Pause ": "Paws ",
+    "paus": "paws",
+    "Paus": "Paws"
+}
+
+
+def add_prefix(input_string, prefixes=PREFIXES):
+    '''Appends a random prefix to the beginning of the string'''
+    if not isinstance(input_string, str):
+        raise TypeError("input_string must be a string")
+    if not isinstance(prefixes, (list, tuple)):
+        raise TypeError("Prefixes must be passed as a list or tuple")
+    return random.choice(prefixes) + input_string
+
+
+def add_suffix(input_string, suffixes=SUFFIXES):
+    '''Appends a random suffix to the end of the string'''
+    if not isinstance(input_string, str):
+        raise TypeError("input_string must be a string")
+    if not isinstance(suffixes, (list, tuple)):
+        raise TypeError("Suffixes must be passed as a list or tuple")
+    return input_string + random.choice(suffixes)
+
+
+def add_affixes(input_string, prefixes=PREFIXES, suffixes=SUFFIXES):
+    '''Appends a random prefix and suffix to the beginning and end of the string'''
+    if not isinstance(input_string, str):
+        raise TypeError("input_string must be a string")
+    if not isinstance(prefixes, (list, tuple)):
+        raise TypeError("Prefixes must be passed as a list or tuple")
+    if not isinstance(suffixes, (list, tuple)):
+        raise TypeError("Suffixes must be passed as a list or tuple")
+    return random.choice(prefixes) + input_string + random.choice(suffixes)
+
+
+def substitute(input_string, substitutions=None):
+    '''owofies the string without adding affixes'''
+    if not isinstance(input_string, str):
+        raise TypeError("input_string must be a string")
+    if substitutions is None:
+        substitutions = SUBSTITUTIONS
+    if not isinstance(substitutions, dict):
+        raise TypeError("Suffixes must be passed as a dictionary")
+    for sub in substitutions:
+        input_string = input_string.replace(sub, substitutions[sub])
+    return input_string
+
+
+def owo(input_string, substitutions=None, prefixes=PREFIXES, suffixes=SUFFIXES):
+    '''owofies the string and adds a random prefix and suffix'''
+    return add_affixes(substitute(input_string, substitutions=substitutions),
+                       prefixes=prefixes, suffixes=suffixes)
