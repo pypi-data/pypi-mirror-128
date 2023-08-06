6169a793f4475931aedc32bf895f1ada6a0fa7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:18 +0000

    Add package lisa-admin/0.0.0/lisa-admin-0.0.0.tar.gz

---
+++ packages/lisa-admin/lisa-admin-0.0.0.tar.gz/lisa-admin-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='lisa-admin',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://linktr.ee/marawan6569',
+    author='marawan mohamed',
+    author_email='marawan6569@gmail.com',
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
