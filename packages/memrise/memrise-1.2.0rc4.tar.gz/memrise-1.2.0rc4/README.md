90be0404f36d6763d063a200acf897555922fe3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:33 +0000

    Add package memrise/1.2.0rc4/memrise-1.2.0rc4.tar.gz

---
+++ packages/memrise/memrise-1.2.0rc4.tar.gz/memrise-1.2.0rc4/README.md
@@ -0,0 +1,127 @@
+<p align="center">
+    <img src=".github/memrise.svg?sanitize=true" />
+</p>
+
+<p align="center"> 
+    <img src="https://img.shields.io/github/license/tquangsdh20/memrise"> <img src = "https://img.shields.io/bitbucket/issues-raw/tquangsdh20/memrise"> <img src = "https://img.shields.io/pypi/pyversions/memrise"> <img src="https://img.shields.io/pypi/implementation/memrise"> <img src="https://img.shields.io/github/last-commit/tquangsdh20/memrise">
+</p>
+
+## Features:
+- Support scraping the courses in MEM to take the vocabulary
+
+## Appplication Requires
+
+### Install DB Browser : [SQLite](https://sqlitebrowser.org/dl/)
+
+### Install Library: 
+<b>Window</b>
+```
+ python -m pip install memrise
+```
+<b>Linux</b>
+  ```
+  pip install memrise
+  ```
+ <b>macOS</b>
+ ```
+ sudo pip3 install memrise
+```
+## Guidelines
+
+### How to take Course ID?
+
+Access the Website: [Memrise](https://app.memrise.com/course/) and copy the Course ID as the following picture:
+
+<p align="center">
+  <img src=".github/CourseID.PNG?sanitize=true">
+</p>
+
+### Import library and initialize database
+
+```python
+from memrise import Course, Data
+#Create file database output
+db = Data('English.sqlite') #Other format is .db
+#Connect to file database and init
+db.init_database()
+```
+
+### Scraping course with ID
+
+Regarding to Module Course with two paramemters:
+- `CourseID`: Get the Course ID as above
+- `LanguageID`: The Language ID of the Course which you study.
+
+Where, the LanguageID is define as the followings:
+The output will give you the List Language's ID of the Course, remember the ID for next step. 
+
+```
+Language IDs:        
+    1. English UK    
+    2. English US    
+    3. Chinese       
+    4. Janpanese     
+    5. French        
+    6. Spanish Mexico
+    7. Italian
+    8. German
+    9. Russian
+    10. Dutch
+    11. Korean
+    12. Arabic
+    13. Spanish Spain
+
+```
+
+The following example is scraping the English course for Vietnamese with IPA of English US, so the Language ID is 2.
+```python
+#Connect the course to scraping info this maybe take a few momment.
+course = Course(1658724,2)
+#Update information about the course
+db.update_course(course)
+```
+
+### Update course with your language meaning
+
+Use the method `update_db_en()` if the LANGUAGE COURSE is **English** for scraping IPA.  
+Use the method `update_db()` if the Language Course is the others.  
+About the parameters of two above methods are the same:  
+- `CourseID` : the ID of the course
+- `Language` : your mother language with format <i>'en', 'fr', 'ko', 'vi'...</i>
+
+```python
+#If your Course is English language use `update_db_en()`, otherwise use `update_db()` method.
+db.update_db_en(1658724,'fr')
+```
+### Check the output with SQLite
+
+File output
+
+<p align="center">
+  <img src=".github/OUTPUT.PNG?sanitize=true" height=200 width=600 />
+</p>
+
+Show the words table as the following steps: **Browse Data > Table > Word**
+
+<p align="center">
+  <img src=".github/OUTPUT2.PNG?sanitize=true" height=500 width=800 />
+</p>
+
+If you want to choose the raw meaning, you could run the following SQL statement.
+
+```SQL
+SELECT word, sub, IPA FROM words
+```
+Steps : **Execute SQL > Typing SQL Statements > Run**
+
+<p align="center">
+  <img src=".github/OUTPUT3.PNG?sanitize=true" height=500 width=800 />
+</p>
+
+[<b>Github:</b> https://github.com/tquangsdh20/memrise](https://github.com/tquangsdh20/memrise)
+
+### Log changes:
+
+**v1.0.0**: Implementation Scrapping Vocabulary  
+**v1.1.0**: Update IPA Function   
+**v1.2.0** : Update new TRANSLATE FUNCTION  
---
+++ packages/memrise/memrise-1.2.0rc4.tar.gz/memrise-1.2.0rc4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['memrise', 'memrise.data', 'memrise.extract', 'memrise.googletrans']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'memrise',
+    'version': '1.2.0rc4',
+    'description': 'Scraping the vocabulary from the Memrise course',
+    'long_description': '<p align="center">\n    <img src=".github/memrise.svg?sanitize=true" />\n</p>\n\n<p align="center"> \n    <img src="https://img.shields.io/github/license/tquangsdh20/memrise"> <img src = "https://img.shields.io/bitbucket/issues-raw/tquangsdh20/memrise"> <img src = "https://img.shields.io/pypi/pyversions/memrise"> <img src="https://img.shields.io/pypi/implementation/memrise"> <img src="https://img.shields.io/github/last-commit/tquangsdh20/memrise">\n</p>\n\n## Features:\n- Support scraping the courses in MEM to take the vocabulary\n\n## Appplication Requires\n\n### Install DB Browser : [SQLite](https://sqlitebrowser.org/dl/)\n\n### Install Library: \n<b>Window</b>\n```\n python -m pip install memrise\n```\n<b>Linux</b>\n  ```\n  pip install memrise\n  ```\n <b>macOS</b>\n ```\n sudo pip3 install memrise\n```\n## Guidelines\n\n### How to take Course ID?\n\nAccess the Website: [Memrise](https://app.memrise.com/course/) and copy the Course ID as the following picture:\n\n<p align="center">\n  <img src=".github/CourseID.PNG?sanitize=true">\n</p>\n\n### Import library and initialize database\n\n```python\nfrom memrise import Course, Data\n#Create file database output\ndb = Data(\'English.sqlite\') #Other format is .db\n#Connect to file database and init\ndb.init_database()\n```\n\n### Scraping course with ID\n\nRegarding to Module Course with two paramemters:\n- `CourseID`: Get the Course ID as above\n- `LanguageID`: The Language ID of the Course which you study.\n\nWhere, the LanguageID is define as the followings:\nThe output will give you the List Language\'s ID of the Course, remember the ID for next step. \n\n```\nLanguage IDs:        \n    1. English UK    \n    2. English US    \n    3. Chinese       \n    4. Janpanese     \n    5. French        \n    6. Spanish Mexico\n    7. Italian\n    8. German\n    9. Russian\n    10. Dutch\n    11. Korean\n    12. Arabic\n    13. Spanish Spain\n\n```\n\nThe following example is scraping the English course for Vietnamese with IPA of English US, so the Language ID is 2.\n```python\n#Connect the course to scraping info this maybe take a few momment.\ncourse = Course(1658724,2)\n#Update information about the course\ndb.update_course(course)\n```\n\n### Update course with your language meaning\n\nUse the method `update_db_en()` if the LANGUAGE COURSE is **English** for scraping IPA.  \nUse the method `update_db()` if the Language Course is the others.  \nAbout the parameters of two above methods are the same:  \n- `CourseID` : the ID of the course\n- `Language` : your mother language with format <i>\'en\', \'fr\', \'ko\', \'vi\'...</i>\n\n```python\n#If your Course is English language use `update_db_en()`, otherwise use `update_db()` method.\ndb.update_db_en(1658724,\'fr\')\n```\n### Check the output with SQLite\n\nFile output\n\n<p align="center">\n  <img src=".github/OUTPUT.PNG?sanitize=true" height=200 width=600 />\n</p>\n\nShow the words table as the following steps: **Browse Data > Table > Word**\n\n<p align="center">\n  <img src=".github/OUTPUT2.PNG?sanitize=true" height=500 width=800 />\n</p>\n\nIf you want to choose the raw meaning, you could run the following SQL statement.\n\n```SQL\nSELECT word, sub, IPA FROM words\n```\nSteps : **Execute SQL > Typing SQL Statements > Run**\n\n<p align="center">\n  <img src=".github/OUTPUT3.PNG?sanitize=true" height=500 width=800 />\n</p>\n\n[<b>Github:</b> https://github.com/tquangsdh20/memrise](https://github.com/tquangsdh20/memrise)\n\n### Log changes:\n\n**v1.0.0**: Implementation Scrapping Vocabulary  \n**v1.1.0**: Update IPA Function   \n**v1.2.0** : Update new TRANSLATE FUNCTION  \n',
+    'author': 'Joseph Quang',
+    'author_email': 'tquangsdh20@hcmut.edu.vn',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tquangsdh20/memrise',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
