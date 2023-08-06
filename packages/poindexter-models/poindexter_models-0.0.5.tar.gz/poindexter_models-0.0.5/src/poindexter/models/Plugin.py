ff8fab1f21877f60fb2a21bfa864ad77684b6608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:52 +0000

    Add package poindexter-models/0.0.5/poindexter_models-0.0.5.tar.gz

---
+++ packages/poindexter-models/poindexter_models-0.0.5.tar.gz/poindexter_models-0.0.5/src/poindexter/models/Plugin.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from pydantic import BaseModel 
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+from poindexter.models import Base
+from poindexter.models.Tag import Tag
+
+
+Plugin_Tag_Table = Table(
+  'plugin_tag_association',
+  Base.metadata,
+  Column('plugin_id', ForeignKey('plugins.id')),
+  Column('tag_id', ForeignKey('tags.id'))
+)
+
+
+class PluginCreate(BaseModel):
+  urn: str
+  description: str
+  author: str
+  path: str
+
+
+class Plugin(PluginCreate):
+  id: int
+  tags: List[Tag]
+  class Config:
+    orm_mode = True
+
+
+class PluginModel(Base):
+  __tablename__ = 'plugins'
+  id = Column(Integer, primary_key=True, index=True)
+  urn = Column(String)
+  description = Column(String)
+  author = Column(String)
+  path = Column(String)
+  tags = relationship("TagModel", secondary=Plugin_Tag_Table)
