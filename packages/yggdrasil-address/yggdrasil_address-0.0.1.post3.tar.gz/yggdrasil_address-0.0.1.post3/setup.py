7253c88abb27626901b10556e5875106c96e442b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:19 +0000

    Add package yggdrasil-address/0.0.1.post3/yggdrasil_address-0.0.1.post3.tar.gz

---
+++ packages/yggdrasil-address/yggdrasil_address-0.0.1.post3.tar.gz/yggdrasil_address-0.0.1.post3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yggdrasil_address",
+    version="0.0.1-3",
+    author="cofob",
+    author_email="c@cofob.ru",
+    description="Small tool for converting yggdrasil addresses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.cofob.ru/cofob/yggdrasil-address",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    setup_requires=["bitarray"],
+)
