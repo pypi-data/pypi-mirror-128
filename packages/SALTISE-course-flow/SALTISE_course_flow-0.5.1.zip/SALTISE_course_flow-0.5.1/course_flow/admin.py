c63588e550b0a815bf857c16d9153f6d5d52b643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:06 +0000

    Add package SALTISE-course-flow/0.5.1/SALTISE_course_flow-0.5.1.zip

---
+++ packages/SALTISE-course-flow/SALTISE_course_flow-0.5.1.zip/SALTISE_course_flow-0.5.1/course_flow/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+from .models import (
+    Node,
+    Column,
+    Week,
+    Activity,
+    Course,
+    Program,
+    NodeWeek,
+    NodeLink,
+    WeekWorkflow,
+    ColumnWorkflow,
+    Workflow,
+    Project,
+    WorkflowProject,
+    Outcome,
+    OutcomeNode,
+    OutcomeOutcome,
+    
+)
+
+admin.site.register(Node)
+admin.site.register(Column)
+admin.site.register(ColumnWorkflow)
+admin.site.register(Week)
+admin.site.register(Activity)
+admin.site.register(Course)
+admin.site.register(Program)
+admin.site.register(NodeWeek)
+admin.site.register(NodeLink)
+admin.site.register(WeekWorkflow)
+admin.site.register(Workflow)
+admin.site.register(Project)
+admin.site.register(WorkflowProject)
+admin.site.register(Outcome)
+admin.site.register(OutcomeNode)
+admin.site.register(OutcomeOutcome)
