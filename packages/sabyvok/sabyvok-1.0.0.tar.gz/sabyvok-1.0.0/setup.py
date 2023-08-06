23b05b3fc118cb160ba6c0d723db1abfeb2b310f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:56 +0000

    Add package sabyvok/1.0.0/sabyvok-1.0.0.tar.gz

---
+++ packages/sabyvok/sabyvok-1.0.0.tar.gz/sabyvok-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='sabyvok',
+    version='1.0.0',
+    description='Saby Vok API client',
+    packages=['sabyvok'],
+    install_requires=['requests'],
+    author='Igor Veselov',
+    author_email='mail@veseloff.net',
+    zip_safe=False,
+    url='https://github.com/saby/vok/tree/main/client-py',
+)
