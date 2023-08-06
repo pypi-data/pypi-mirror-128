07a8d8e7b83e12698bb66ec8cda18ef7f154b7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:44 +0000

    Add package rasa-chinese-plus/0.0.2/rasa-chinese-plus-0.0.2.tar.gz

---
+++ packages/rasa-chinese-plus/rasa-chinese-plus-0.0.2.tar.gz/rasa-chinese-plus-0.0.2/README.md
@@ -0,0 +1,9 @@
+# rasa-chinese-plus
+rasa-chinese-plus 是专门针对中文语言的 rasa 组件扩展包。
+
+## NLU组件
+### BertTokenizer
+**参数**
+- pretrained_model_name_or_path 预训练模型路径
+- cache_dir 缓存目录
+- use_fast 是否快速分词
---
+++ packages/rasa-chinese-plus/rasa-chinese-plus-0.0.2.tar.gz/rasa-chinese-plus-0.0.2/rasa_chinese_plus/info.py
@@ -0,0 +1,18 @@
+# -*- coding:UTF-8 -*-
+
+# author:yizheng dai
+# contact: 387942239@qq.com
+# datetime:2021/11/19 13:28
+# software: PyCharm
+
+"""
+文件说明：
+    
+"""
+# info.py
+
+import datetime
+
+
+def info(msg):
+    print(datetime.datetime.now(), msg)
