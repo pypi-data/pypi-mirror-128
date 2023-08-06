b119654c8f0886183051ed05f97ea9ae26145857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:58 +0000

    Add package EduNLP/0.0.7/EduNLP-0.0.7.tar.gz

---
+++ packages/EduNLP/EduNLP-0.0.7.tar.gz/EduNLP-0.0.7/EduNLP/Vector/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# 2021/5/29 @ tongshiwei
+
+from .gensim_vec import W2V, D2V, BowLoader, TfidfLoader
+from .const import *
+from .rnn import RNNModel
+from .t2v import T2V, get_pretrained_t2v, PRETRAINED_MODELS
+from .embedding import Embedding
+from .bert_vec import BertModel
