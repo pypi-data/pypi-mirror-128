74c44dea5eca8d6c25914ff44c60293dc46bed3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:03 +0000

    Add package codeforlife-portal/5.18.0/codeforlife-portal-5.18.0.tar.gz

---
+++ packages/codeforlife-portal/codeforlife-portal-5.18.0.tar.gz/codeforlife-portal-5.18.0/portal/strings/privacy_policy.py
@@ -0,0 +1,6 @@
+PRIVACY_POLICY_BANNER = {
+    "title": "Privacy policy",
+    "subtitle": "",
+    "text": "",
+    "image_class": "banner--picture--terms",
+}
---
+++ packages/codeforlife-portal/codeforlife-portal-5.18.0.tar.gz/codeforlife-portal-5.18.0/portal/strings/terms.py
@@ -0,0 +1,6 @@
+TERMS_BANNER = {
+    "title": "Terms of use",
+    "subtitle": "",
+    "text": "",
+    "image_class": "banner--picture--terms",
+}
