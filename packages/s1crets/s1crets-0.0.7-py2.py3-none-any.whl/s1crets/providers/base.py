cc51324d36e8d78b9b0053c5aec7035b6b1e13b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:15 +0000

    Add package s1crets/0.0.7/s1crets-0.0.7-py2.py3-none-any.whl

---
+++ packages/s1crets/s1crets-0.0.7-py2.py3-none-any.whl/s1crets/providers/base.py
@@ -0,0 +1,52 @@
+from abc import ABCMeta, abstractmethod
+from s1crets.cache import Cache
+
+
+# try to make a hashable key for caching
+def args_cache_key(*args, **kw):
+    args = list(args)
+    for k, v in kw.items():
+        if isinstance(v, list):
+            v = tuple(v)
+        try:
+            hash(v)
+        except Exception:
+            continue
+        args.append((k, v))
+    return tuple(args)
+
+
+class DefaultValue(object):
+    """Indicates a default value, which is distinguishable from None (which
+    can also be a default value)
+    """
+    pass
+
+
+class BaseProvider(object, metaclass=ABCMeta):
+    def __init__(self, cache_args={}, **kwargs):
+        self.cache = Cache(**cache_args)
+        self.kwargs = kwargs
+
+    @staticmethod
+    def dict_filt(d, keys):
+        """filter dictionary d to keys keys"""
+        return dict([(i, d[i]) for i in d if i in set(keys)])
+
+    @abstractmethod
+    def get(self, path, keypath=None, default=DefaultValue, decrypt=True, cached=True,
+            retry=None, timeout=None):
+        pass
+
+    @abstractmethod
+    def get_by_path(self, path, decrypt=True, recursive=True, cached=True,
+                    retry=None, timeout=None):
+        pass
+
+    @abstractmethod
+    def update(self, path, value, retry=None, timeout=None):
+        pass
+
+    @abstractmethod
+    def path_exists(self, path, keypath=None, cached=True, retry=None, timeout=None):
+        pass
