753364416ae6154a35b0cc242bbb8c5b226ae8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:08 +0000

    Add package straattaal-bijbel/97867967678689734589734258973489579438579384579384.0/straattaal_bijbel-97867967678689734589734258973489579438579384579384.0-py3-none-any.whl

---
+++ packages/straattaal-bijbel/straattaal_bijbel-97867967678689734589734258973489579438579384579384.0-py3-none-any.whl/straattaal_bijbel/verse.py
@@ -0,0 +1,24 @@
+import requests
+
+FEED = "https://feed.dagelijkswoord.nl/api/json/1.0/"
+
+class Verse():
+    """ 
+        
+    """
+    def __init__(self, language='bgt', day=0, username="", password=""):
+        self.language = language
+        self.day = day
+        self.username = username
+        self.password = password
+
+        self.text = self.__get()
+
+    def __get(self) -> str:
+        """
+            Get a the daily quote.
+        """
+        
+        r = requests.get(FEED, auth=(self.username, self.password))
+
+        return r.json()['data'][self.day]['text'][self.language]
