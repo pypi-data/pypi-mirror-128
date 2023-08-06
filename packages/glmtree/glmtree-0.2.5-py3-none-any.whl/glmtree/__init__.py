3aaa5ff8fb14be744484bb13df95c3231e8c21ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:08 +0000

    Add package glmtree/0.2.5/glmtree-0.2.5-py3-none-any.whl

---
+++ packages/glmtree/glmtree-0.2.5-py3-none-any.whl/glmtree/__init__.py
@@ -0,0 +1,7 @@
+from .glmtree import GLMTreeRegressor, GLMTreeClassifier
+from .glmtreeboost import GLMTreeBoostRegressor
+
+__all__ = ["GLMTreeRegressor", "GLMTreeClassifier", "GLMTreeBoostRegressor"]
+
+__version__ = '0.2.5'
+__author__ = 'Zebin Yang'
