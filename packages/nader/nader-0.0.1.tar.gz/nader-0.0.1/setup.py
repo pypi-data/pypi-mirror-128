1e3d95eea877cfae11195c7a753e8a4d1d2492fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:24 +0000

    Add package nader/0.0.1/nader-0.0.1.tar.gz

---
+++ packages/nader/nader-0.0.1.tar.gz/nader-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='nader',
+  version='0.0.1',
+  description='simple code for nader',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='hashemshaiban',
+  author_email='hashemshaiban@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='nader',
+  packages=find_packages(),
+  install_requires=[''] 
+)
