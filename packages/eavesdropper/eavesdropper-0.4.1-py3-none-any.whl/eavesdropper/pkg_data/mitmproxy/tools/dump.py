6a0abc294bc7a621e5b186074bab66289b014b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package eavesdropper/0.4.1/eavesdropper-0.4.1-py3-none-any.whl

---
+++ packages/eavesdropper/eavesdropper-0.4.1-py3-none-any.whl/eavesdropper/pkg_data/mitmproxy/tools/dump.py
@@ -0,0 +1,35 @@
+from eavesdropper.pkg_data.mitmproxy import addons
+from eavesdropper.pkg_data.mitmproxy import options
+from eavesdropper.pkg_data.mitmproxy import master
+from eavesdropper.pkg_data.mitmproxy.addons import dumper, termlog, termstatus, keepserving, readfile
+
+
+class ErrorCheck:
+    def __init__(self):
+        self.has_errored = False
+
+    def log(self, e):
+        if e.level == "error":
+            self.has_errored = True
+
+
+class DumpMaster(master.Master):
+
+    def __init__(
+        self,
+        options: options.Options,
+        with_termlog=True,
+        with_dumper=True,
+    ) -> None:
+        super().__init__(options)
+        self.errorcheck = ErrorCheck()
+        if with_termlog:
+            self.addons.add(termlog.TermLog(), termstatus.TermStatus())
+        self.addons.add(*addons.default_addons())
+        if with_dumper:
+            self.addons.add(dumper.Dumper())
+        self.addons.add(
+            keepserving.KeepServing(),
+            readfile.ReadFileStdin(),
+            self.errorcheck
+        )
