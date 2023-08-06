69d8e14f900dffb7d70b1b805f7fb78b94c3f5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:10 +0000

    Add package LegendBot/3.0/LegendBot-3.0.tar.gz

---
+++ packages/LegendBot/LegendBot-3.0.tar.gz/LegendBot-3.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='LegendBot',
+      version='3.0',
+      description='LEGENDBOT 3.0',
+      packages=['LegendBot'],
+      author_email='Krishna045Jaiswal@gmail.com',
+      zip_safe=False)
