405879557119e628e8e78ed3ed69758f850d93f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:59 +0000

    Add package unpackai/0.1.9.2/unpackai-0.1.9.2-py3-none-any.whl

---
+++ packages/unpackai/unpackai-0.1.9.2-py3-none-any.whl/test/common/utils_4_tests.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import pytest
+from pytest import LogCaptureFixture
+
+TEST_DIR = Path(__file__).parent.parent
+IMG_DIR = TEST_DIR  / "img"
+DATA_DIR = TEST_DIR / "test_data"
+
+
+def check_only_warning(caplog: LogCaptureFixture, sub_str: str):
+    """Check that logging only contain warning and it contains a given substring"""
+    assert caplog.records, f"There shall be at least one logged message"
+    assert all(
+        r.levelname == "WARNING" for r in caplog.records
+        if r.levelname != "DEBUG"
+    ), f"There shall be only warning (got {caplog.record_tuples})"
+    assert sub_str in caplog.text, f"{sub_str} not found in {caplog.text}"
+
+
+def check_no_log(caplog: LogCaptureFixture):
+    """Check that there is nothing in logging (except potentially DEBUG)"""
+    
+    assert all(
+        r.levelname == "DEBUG" for r in caplog.records
+    ), f"There should be no message but got {caplog.record_tuples}"
