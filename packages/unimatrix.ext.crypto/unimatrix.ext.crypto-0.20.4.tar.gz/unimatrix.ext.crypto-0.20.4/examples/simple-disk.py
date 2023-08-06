9ff0ea9995a1ef47050aa87d676545a26f6564d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:42 +0000

    Add package unimatrix.ext.crypto/0.20.4/unimatrix.ext.crypto-0.20.4.tar.gz

---
+++ packages/unimatrix.ext.crypto/unimatrix.ext.crypto-0.20.4.tar.gz/unimatrix.ext.crypto-0.20.4/examples/simple-disk.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Set the environment variables ``AZURE_KEYVAULT_NAME``
+and ``AZURE_KEY_NAME`` before running this script.
+"""
+import os
+
+import unimatrix.runtime
+from unimatrix.ext import crypto
+
+
+async def main():
+    name = 'noop.rsa'
+    crypto.load({
+        'keys': {
+            name: {
+                'name': name,
+                'usage': ['sign', 'encrypt'],
+                'loader': {
+                    'backend': 'pem:disk',
+                    'opts': {
+                        'dirname': 'pki/pkcs'
+                    }
+                }
+            }
+        }
+    })
+
+    signer = crypto.get_signer(crypto.RSAPKCS1v15SHA256, keyid=name)
+    message = b'Hello world!'
+    digest = signer.digest(message)
+    sig = await signer.sign(digest)
+    key = signer.get_public_key()
+
+    assert sig.verify(key, digest, True)
+    assert not sig.verify(key, signer.digest(b'wrong'), True)
+    assert signer.public_key.verify(bytes(sig), digest)
+
+
+if __name__ == '__main__':
+    unimatrix.runtime.execute(main)
