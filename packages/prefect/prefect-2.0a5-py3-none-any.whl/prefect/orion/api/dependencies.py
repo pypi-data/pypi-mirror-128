3148f3b226b76f4d94a51c2cb57f1593a1f93fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package prefect/2.0a5/prefect-2.0a5-py3-none-any.whl

---
+++ packages/prefect/prefect-2.0a5-py3-none-any.whl/prefect/orion/api/dependencies.py
@@ -0,0 +1,22 @@
+"""
+Utilities for injecting FastAPI dependencies.
+"""
+
+from prefect.orion.database.dependencies import provide_database_interface
+
+
+async def get_session():
+    """
+    Dependency-injected database session.
+    The context manager will automatically handle commits,
+    rollbacks, and closing the connection.
+    """
+    # we cant directly inject into FastAPI dependencies because
+    # they are converted to async_generator objects
+    db = provide_database_interface()
+
+    # load engine with API timeout setting
+    session_factory = await db.session_factory()
+    async with session_factory() as session:
+        async with session.begin():
+            yield session
