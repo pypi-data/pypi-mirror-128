473c4e23fea2b7fea593f808133d98303f1f986a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:09 +0000

    Add package hij5-topdesk-api/1.0.1/hij5_topdesk_api-1.0.1-py3-none-any.whl

---
+++ packages/hij5-topdesk-api/hij5_topdesk_api-1.0.1-py3-none-any.whl/hij5_topdesk_api/exceptions/exceptions.py
@@ -0,0 +1,9 @@
+class IncidentNotExistsException(Exception):
+	pass
+
+
+class ProcessingStatusDoesNotExistException(Exception):
+	"""
+	Raise deze exception wanneer een processing status niet bestaat.
+	"""
+	pass
