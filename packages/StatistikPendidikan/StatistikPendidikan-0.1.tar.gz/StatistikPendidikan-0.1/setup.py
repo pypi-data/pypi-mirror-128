79aff9af073cc9ff89757ca4ea618fdc8023ae6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:54 +0000

    Add package StatistikPendidikan/0.1/StatistikPendidikan-0.1.tar.gz

---
+++ packages/StatistikPendidikan/StatistikPendidikan-0.1.tar.gz/StatistikPendidikan-0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'StatistikPendidikan',
+  packages = ['StatistikPendidikan'],
+  version = '0.1',
+  license='MIT',
+  description = 'Library ini digunakan untuk memudahkan ekstraksi data dari http://statistik.data.kemdikbud.go.id/',
+  author = 'Aryya Widigdha',
+  author_email = 'aryya.widigdha@yahoo.com',
+  url = 'https://github.com/adwisatya/reponame',   
+  download_url = 'https://github.com/adwisatya/StatistikPendidikan/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['kemendikbud', 'statistik pendidikan', 'KEYWORDS'],
+  install_requires=[
+          'json',
+          'requests',
+	  'lxml.html',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
