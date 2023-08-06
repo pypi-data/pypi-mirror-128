cf601fd210dd54145866baa9f5a7512c2854dfc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:28 +0000

    Add package vtb-django-utils/1.0.33/vtb_django_utils-1.0.33-py3-none-any.whl

---
+++ packages/vtb-django-utils/vtb_django_utils-1.0.33-py3-none-any.whl/vtb_django_utils/model_versions/utils/models.py
@@ -0,0 +1,32 @@
+import operator
+from typing import Tuple, Iterable, List, Set
+
+from django.db import models
+
+from vtb_django_utils.model_versions.utils.consts import REL_VERSION_FIELD_END, REL_VERSION_PATTERN_FIELD_END
+
+
+def get_all_available_major_version(versions: Iterable[List[int]]) -> List[Tuple[str, str]]:
+    all_version: Set[tuple] = set()
+    for version in versions:
+        all_version.add((f'{version[0]}.', f'{version[0]}.x.x'))
+        all_version.add((f'{version[0]}.{version[1]}.', f'{version[0]}.{version[1]}.x'))
+    return sorted((version for version in all_version), key=operator.itemgetter(1))
+
+
+def get_rel_model_version_from_dict(instance_dict: dict, rel_field: models.Field):
+    """ Возвращает инстанс версии с учетом выбранной версии и шаблона """
+    rel_field_name = rel_field.name
+    if rel_version_model_id := instance_dict.get(f'{rel_field_name}_id', instance_dict.get(rel_field_name)):
+        rel_instance = rel_field.related_model.objects.get(pk=rel_version_model_id)
+        rel_version = instance_dict.get(f'{rel_field_name}{REL_VERSION_FIELD_END}')
+        rel_version_pattern = instance_dict.get(f'{rel_field_name}{REL_VERSION_PATTERN_FIELD_END}')
+        return rel_instance.get_version_by_pattern(rel_version or rel_version_pattern)
+    return None
+
+
+def get_rel_model_version_str_from_dict(instance_dict: dict, rel_field: models.Field):
+    """ Возвращает инстанс версии с учетом выбранной версии и шаблона """
+    if rel_version_instance := get_rel_model_version_from_dict(instance_dict, rel_field):
+        return rel_version_instance.version
+    return ''
