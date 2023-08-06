7f844a2ed940017b9363f1628b5c808585c6df5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:29 +0000

    Add package PyBetween/1.0.0/PyBetween-1.0.0.tar.gz

---
+++ packages/PyBetween/PyBetween-1.0.0.tar.gz/PyBetween-1.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='PyBetween',
+    version='1.0.0',
+    license='MIT',
+    author='lill74',
+    author_email='a010393223@gmail.com',
+    description='Python wrapper for Between app',
+    url='https://github.com/lill74/PyBetween',
+    packages = setuptools.find_packages(),
+)
