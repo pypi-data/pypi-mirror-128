186fcf8f76d5c19c7f0e48138068d1040f0fa9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:29 +0000

    Add package divinegift/2.10.0/divinegift-2.10.0-py3-none-any.whl

---
+++ packages/divinegift/divinegift-2.10.0-py3-none-any.whl/divinegift/__init__.py
@@ -0,0 +1,23 @@
+name = "DivineGift"
+
+version = '2.10.0'
+
+__all__ = [
+    "main",
+    "logger",
+    "banner",
+    "config",
+    "sender",
+    "db",
+    "translit",
+    "excel",
+    "templator",
+    "kafka_client",
+    "cipher",
+    "zabbix_agent",
+    "kthreads",
+    "app",
+    "aims",
+    "timer",
+    "smb"
+]
