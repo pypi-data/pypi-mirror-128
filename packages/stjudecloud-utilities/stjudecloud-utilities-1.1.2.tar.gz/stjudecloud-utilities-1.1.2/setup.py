d2f41ba75a33eed582a664a48fcfa5a9e18dcf72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:46 +0000

    Add package stjudecloud-utilities/1.1.2/stjudecloud-utilities-1.1.2.tar.gz

---
+++ packages/stjudecloud-utilities/stjudecloud-utilities-1.1.2.tar.gz/stjudecloud-utilities-1.1.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['stjudecloud_utilities']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['deduplicate-feature-counts = '
+                     'stjudecloud_utilities.deduplicate_feature_counts:main',
+                     'gtf-to-rseqc-bed = '
+                     'stjudecloud_utilities.gtf_to_rseqc_bed:main',
+                     'warden-counts-utils = '
+                     'stjudecloud_utilities.warden_counts_utils:main']}
+
+setup_kwargs = {
+    'name': 'stjudecloud-utilities',
+    'version': '1.1.2',
+    'description': 'A set of utilities used on the St. Jude Cloud project.',
+    'long_description': None,
+    'author': 'St. Jude Cloud',
+    'author_email': 'support@stjude.cloud',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
