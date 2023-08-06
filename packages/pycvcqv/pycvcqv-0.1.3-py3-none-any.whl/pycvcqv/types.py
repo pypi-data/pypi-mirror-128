a84da8279d502329b8a1e8ed37658d2c01ee71d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:35 +0000

    Add package pycvcqv/0.1.3/pycvcqv-0.1.3-py3-none-any.whl

---
+++ packages/pycvcqv/pycvcqv-0.1.3-py3-none-any.whl/pycvcqv/types.py
@@ -0,0 +1,14 @@
+"""The types used in the package."""
+# --------------------------- Import libraries and functions --------------------------
+from typing import List, Tuple
+
+import numpy as np
+from numpy import typing as npt
+
+# ---------------------------------- types definition ---------------------------------
+ListFloat = List[float]
+ListInt = List[int]
+TupleFloat = Tuple[float]
+TupleInt = Tuple[int]
+ArrayFloat = npt.NDArray[np.float_]
+ArrayInt = npt.NDArray[np.int_]
