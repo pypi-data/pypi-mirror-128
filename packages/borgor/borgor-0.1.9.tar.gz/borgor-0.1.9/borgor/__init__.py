548f3996a87dcf52230e4af84eb1ae1774a3dd9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:40 +0000

    Add package borgor/0.1.9/borgor-0.1.9.tar.gz

---
+++ packages/borgor/borgor-0.1.9.tar.gz/borgor-0.1.9/borgor/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'borgor'
+__author__ = 'coverosu'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 coverosu'
+
+from . import simplebeatmap
+from . import akatsukiapi
+from . import osuapiv2
+from . import beatmap
+from . import replay
+from . import osuapi
+from . import utils
