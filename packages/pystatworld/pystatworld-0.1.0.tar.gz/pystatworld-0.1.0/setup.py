66cc524adfa6c059eb2f64ea4c8bebc0a495f66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:57 +0000

    Add package pystatworld/0.1.0/pystatworld-0.1.0.tar.gz

---
+++ packages/pystatworld/pystatworld-0.1.0.tar.gz/pystatworld-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pystatworld',
+      version='0.1.0',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      packages=['pystatworld'],
+      author_email='ai.sumitsagar@gmail.com',
+      zip_safe=False)
