845a718f481f7d1e4ec1b4df0328289d2d958a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:45 +0000

    Add package poe-client/0.4.0/poe_client-0.4.0-py3-none-any.whl

---
+++ packages/poe-client/poe_client-0.4.0-py3-none-any.whl/poe_client/schemas/account.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+from poe_client.schemas import CamelModel
+
+
+class Realm(Enum):
+    """Enum to describe the different realms of an Account."""
+
+    pc = "pc"
+    xbox = "xbox"
+    sony = "sony"
+
+
+class Guild(CamelModel):
+    """Dataclass to describe a Guild."""
+
+    id: int
+    name: str
+    tag: str
+    points: Optional[int]
+    status_message: Optional[str]
+    created_at: datetime
+
+
+class Challenge(CamelModel):
+    """Dataclass to describe a Challenge."""
+
+    total: int
+
+
+class Stream(CamelModel):
+    """Dataclass to describe the Stream data of Twitch."""
+
+    name: str
+    image: str
+    status: str
+
+
+class Twitch(CamelModel):
+    """Dataclass to describe a Twitch stream."""
+
+    name: str
+    stream: Optional[Stream]
+
+
+class Account(CamelModel):
+    """Dataclass to describe an Account."""
+
+    uuid: Optional[str]
+    name: str
+    realm: Optional[Realm]
+    guild: Optional[Guild]
+    challenges: Optional[Challenge]
+    twitch: Optional[Twitch]
