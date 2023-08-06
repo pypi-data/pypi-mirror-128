40e9b72cc556e68883b59a093de447e8998569b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:54 +0000

    Add package prize/1.0.1a0/prize-1.0.1a0-py2.py3-none-any.whl

---
+++ packages/prize/prize-1.0.1a0-py2.py3-none-any.whl/prize/util/ui_placer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2021/11/21 2:21 下午
+# @Author : LeiXueWei
+# @CSDN/Juejin/Wechat: 雷学委
+# @XueWeiTag: CodingDemo
+# @File : ui_centralizer.py
+# @Project : prize
+import math
+from tkinter import Misc, Tk
+
+
+def centralize(win: Misc, width, height):
+    """
+    centralize a tk/Toplevel window
+    """
+    screen_width = win.winfo_screenwidth()
+    screen_height = win.winfo_screenheight()
+    x = math.ceil((screen_width - width) / 2)
+    y = math.ceil((screen_height - height) / 2)
+    win.geometry("%dx%d+%d+%d" % (width, height, x, y))
+
+
+def left(win: Misc, base_win: Tk, width, height):
+    """
+    display the win on the left hand side of base_win
+    """
+    base_x = base_win.winfo_pointerx()
+    base_y = base_win.winfo_pointery()
+    x = base_x - math.ceil(width / 2)
+    y = base_y + math.ceil(height / 2)
+    win.geometry("%dx%d+%d+%d" % (width, height, x, y))
+
+
+def right(win: Misc, base_win: Tk, width, height):
+    """
+    display the win on the right hand side of base_win
+    """
+    base_x = base_win.winfo_pointerx()
+    base_y = base_win.winfo_pointery()
+    delta_width = base_win.winfo_width()
+    x = base_x + delta_width + math.ceil(width / 2)
+    y = base_y + math.ceil(height / 2)
+    win.geometry("%dx%d+%d+%d" % (width, height, x, y))
