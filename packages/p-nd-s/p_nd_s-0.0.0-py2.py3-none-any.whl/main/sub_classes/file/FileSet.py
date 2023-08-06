b36470fb27073c2e1afd0a34defc3273e60b478d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:53 +0000

    Add package p-nd-s/0.0.0/p_nd_s-0.0.0-py2.py3-none-any.whl

---
+++ packages/p-nd-s/p_nd_s-0.0.0-py2.py3-none-any.whl/main/sub_classes/file/FileSet.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class FileSet:
+    var1: str = 'lala'
---
+++ packages/p-nd-s/p_nd_s-0.0.0-py2.py3-none-any.whl/main/sub_classes/file/FileStore.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class FileStore:
+    var1: str = 'lala'
