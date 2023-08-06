23d8b4dad0b94207a59498ccfdbcf33d626fc810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:04 +0000

    Add package kiwitcms/10.5/kiwitcms-10.5-py3-none-any.whl

---
+++ packages/kiwitcms/kiwitcms-10.5-py3-none-any.whl/tcms/testcases/static/testcases/js/mutable.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $('#text_templates').change(function () {
+    markdownEditor.codemirror.setValue($(this).val())
+  })
+
+  if ($('#id_category').find('option').length === 0) {
+    populateProductCategory()
+  }
+
+  $('#add_id_product').click(function () {
+    return showRelatedObjectPopup(this)
+  })
+
+  $('#add_id_category').click(function () {
+    return showRelatedObjectPopup(this)
+  })
+
+  document.getElementById('id_product').onchange = function () {
+    $('#id_product').selectpicker('refresh')
+    populateProductCategory()
+  }
+
+  document.getElementById('id_category').onchange = function () {
+    $('#id_category').selectpicker('refresh')
+  }
+
+  $('.selectpicker').selectpicker()
+  $('.bootstrap-switch').bootstrapSwitch()
+})
+
+function populateProductCategory () {
+  const href = $('#add_id_category')[0].href
+  $('#add_id_category')[0].href = href.slice(0, href.indexOf('&product'))
+  $('#add_id_category')[0].href += `&product=${$('#id_product').val()}`
+
+  $('#id_category').find('option').remove()
+
+  updateCategorySelectFromProduct()
+}
