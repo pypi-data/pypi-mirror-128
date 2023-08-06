7f9b0a1206c0497ef63da3737439773cd5636b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:14 +0000

    Add package hamlet/9.8.2.dev7/hamlet-9.8.2.dev7.tar.gz

---
+++ packages/hamlet/hamlet-9.8.2.dev7.tar.gz/hamlet-9.8.2.dev7/tests/system/cmdb/product/step_1_create_cmdb.py
@@ -0,0 +1,14 @@
+import os
+from hamlet.backend.generate.cmdb import account, tenant, product
+from .conftest import conf
+
+
+def run(cmdb, clear_cmdb):
+    clear_cmdb()
+    with cmdb() as path:
+        os.mknod("root.json")
+        tenant.run(**conf["cmdb"]["tenant"], output_dir=path())
+    with cmdb("accounts") as path:
+        account.run(**conf["cmdb"]["account"], output_dir=path())
+    with cmdb() as path:
+        product.run(**conf["cmdb"]["product"], output_dir=path())
