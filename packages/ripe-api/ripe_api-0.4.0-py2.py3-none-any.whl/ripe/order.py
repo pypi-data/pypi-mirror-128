86db12476c0e27b1ad029d28e9dad1ab55eb4da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:23 +0000

    Add package ripe-api/0.4.0/ripe_api-0.4.0-py2.py3-none-any.whl

---
+++ packages/ripe-api/ripe_api-0.4.0-py2.py3-none-any.whl/ripe/order.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+
+class OrderAPI(object):
+
+    @classmethod
+    def load_order(cls, order):
+        structure = order.get("structure", None)
+        if structure: order["details"] = json.loads(structure)
+        else: order["details"] = dict()
+        return order
+
+    def list_orders(self, *args, **kwargs):
+        url = self.base_url + "orders"
+        contents = self.get(url, **kwargs)
+        return contents
+
+    def create_order(self, order):
+        url = self.base_url + "orders"
+        contents = self.post(url, data_j = order)
+        return contents
+
+    def get_order(self, number):
+        url = self.base_url + "orders/%d" % number
+        contents = self.get(url)
+        return contents
+
+    def delete_order(self, number):
+        url = self.base_url + "orders/%d" % number
+        contents = self.delete(url)
+        return contents
+
+    def price_order(self, number, currency = None):
+        url = self.base_url + "orders/%d/price" % number
+        contents = self.get(url, currency = currency)
+        return contents
+
+    def produce_order(
+        self,
+        number,
+        justification = None,
+        notify = False,
+        strict = True
+    ):
+        url = self.base_url + "orders/%d/produce" % number
+        contents = self.put(
+            url,
+            justification = justification,
+            notify = notify,
+            strict = strict
+        )
+        return contents
+
+    def ready_order(
+        self,
+        number,
+        justification = None,
+        notify = False,
+        strict = True
+    ):
+        url = self.base_url + "orders/%d/ready" % number
+        contents = self.put(
+            url,
+            justification = justification,
+            notify = notify,
+            strict = strict
+        )
+        return contents
+
+    def send_order(
+        self,
+        number,
+        justification = None,
+        notify = False,
+        strict = True,
+        tracking_number = None,
+        tracking_url = None
+    ):
+        url = self.base_url + "orders/%d/send" % number
+        contents = self.put(
+            url,
+            justification = justification,
+            notify = notify,
+            strict = strict,
+            tracking_number = tracking_number,
+            tracking_url = tracking_url
+        )
+        return contents
+
+    def report_pdf(self, number):
+        url = self.base_url + "orders/%d/report.pdf" % number
+        contents = self.get(url, number)
+        return contents
+
+    def import_order(
+        self,
+        ff_order_id,
+        contents,
+        currency = None,
+        country = None,
+        meta = None,
+        safe = True,
+        notify = False,
+        pending = False,
+        *args,
+        **kwargs
+    ):
+        url = self.base_url + "orders/import"
+        contents = self.post(
+            url,
+            ff_order_id = ff_order_id,
+            contents = contents,
+            currency = currency,
+            country = country,
+            meta = meta,
+            safe = safe,
+            notify = notify,
+            pending = pending,
+            **kwargs
+        )
+        return contents
+
+    def create_farfetch_order(
+        self,
+        ff_order_id,
+        contents,
+        currency = None,
+        country = None,
+        meta = None,
+        safe = True,
+        notify = False,
+        *args,
+        **kwargs
+    ):
+        url = self.base_url + "orders/farfetch"
+        contents = self.post(
+            url,
+            ff_order_id = ff_order_id,
+            contents = contents,
+            currency = currency,
+            country = country,
+            meta = meta,
+            safe = safe,
+            notify = notify,
+            **kwargs
+        )
+        return contents
