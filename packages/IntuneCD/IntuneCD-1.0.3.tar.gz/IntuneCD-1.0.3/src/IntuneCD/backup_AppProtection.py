eafc84f5483da8a7aac2d1b29e9d4400108e24de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:04 +0000

    Add package IntuneCD/1.0.3/IntuneCD-1.0.3.tar.gz

---
+++ packages/IntuneCD/IntuneCD-1.0.3.tar.gz/IntuneCD-1.0.3/src/IntuneCD/backup_AppProtection.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""
+This module backs up all App Protection Polices in Intune.
+
+Parameters
+----------
+path : str
+    The path to save the backup to
+output : str
+    The format the backup will be saved as
+token : str
+    The token to use for authenticating the request
+"""
+
+import json
+import os
+import yaml
+
+from .clean_filename import clean_filename
+from .graph_request import makeapirequest
+
+## Set MS Graph endpoint
+endpoint = "https://graph.microsoft.com/beta/deviceAppManagement/managedAppPolicies"
+
+## Get all App Protection policies and save them in specified path
+def savebackup(path,output,token):
+    configpath = path+"/"+"App Protection/"
+    data = makeapirequest(endpoint,token)
+
+    for profile in data['value']:
+        remove_keys = {'id','createdDateTime','version','lastModifiedDateTime'}
+        for k in remove_keys:
+            profile.pop(k, None)
+        print("Backing up App Protection: " + profile['displayName'])
+        if os.path.exists(configpath)==False:
+            os.mkdir(configpath)
+
+        ## Get filename without illegal characters
+        fname = clean_filename(profile['displayName'])
+        ## Save App Protection as JSON or YAML depending on configured value in "-o"
+        if output != "json":
+            with open(configpath+fname+".yaml",'w') as yamlFile:
+                yaml.dump(profile, yamlFile, sort_keys=False, default_flow_style=False)
+        else:
+            with open(configpath+fname+".json",'w') as jsonFile:
+                json.dump(profile, jsonFile, indent=10)
