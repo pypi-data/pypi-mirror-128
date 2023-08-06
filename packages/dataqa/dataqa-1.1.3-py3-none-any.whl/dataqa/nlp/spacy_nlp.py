2ad650bcbd74d8139f04a2679d61fd1ec386755f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:54 +0000

    Add package dataqa/1.1.3/dataqa-1.1.3-py3-none-any.whl

---
+++ packages/dataqa/dataqa-1.1.3-py3-none-any.whl/dataqa/nlp/spacy_nlp.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import subprocess
+import sys
+
+import spacy
+import spacy.cli
+
+from dataqa.constants import ROOT_PATH
+
+try:
+    _ = spacy.cli.info('en-core-web-sm')
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install",
+                           str(Path(ROOT_PATH, "nlp", "en_core_web_sm-2.3.1.tar.gz"))])
+
+# this is a pointer to the module object instance itself.
+this = sys.modules[__name__]
+
+# we can explicitly make assignments on it
+import en_core_web_sm
+
+# this.nlp = spacy.load("en_core_web_sm", disable=["textcat"])
+this.nlp = en_core_web_sm.load(disable=["textcat"])
+sentencizer = this.nlp.create_pipe("sentencizer")
+this.nlp.add_pipe(sentencizer)
+
+infixes = this.nlp.Defaults.infixes
+infixes = tuple([i for i in infixes if i != "#"])
+infix_regex = spacy.util.compile_infix_regex(infixes)
+this.nlp.tokenizer.infix_finditer = infix_regex.finditer
