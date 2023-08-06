93d70aab84842c421f12d84bd82b67a54e6c3589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:32 +0000

    Add package shrike/1.13.0/shrike-1.13.0-py3-none-any.whl

---
+++ packages/shrike/shrike-1.13.0-py3-none-any.whl/shrike/_core/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+Internal utilities for testing, build, pipelines, and logging.
+"""
+
+from .testing import stream_handler  # noqa: F401
+from .eyesoff import is_eyesoff_helper  # noqa: F401
