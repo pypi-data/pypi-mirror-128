878a3c54a7ba352a43dc683735e84f3e0e0f0170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:35 +0000

    Add package mikro/0.1.47/mikro-0.1.47.tar.gz

---
+++ packages/mikro/mikro-0.1.47.tar.gz/mikro-0.1.47/mikro/graphql/mutations/table.py
@@ -0,0 +1,28 @@
+from herre.wards.graphql import ParsedQuery
+
+
+CREATE_TABLE = ParsedQuery(
+    """
+mutation CreateTable($sample: ID, $name: String, $tags: [String], $creator: String, $rep: ID, $experiment: ID){
+  createTable(sample: $sample, name: $name, rep: $rep, exp: $exp, tags: $tags, creator: $creator){
+    name
+    id
+    tags
+    store
+  }
+}
+"""
+)
+
+UPDATE_TABLE = ParsedQuery(
+    """
+mutation UpdateTable($id: ID!){
+  updateTable(id: $id){
+    name
+    id
+    tags
+    store
+  }
+}
+"""
+)
