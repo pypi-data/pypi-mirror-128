774a42cfee31ce7620be72888512e1eb74ffe47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:11 +0000

    Add package pyjion/1.1.1/pyjion-1.1.1-cp310-cp310-macosx_10_16_x86_64.whl

---
+++ packages/pyjion/pyjion-1.1.1-cp310-cp310-macosx_10_16_x86_64.whl/pyjion/__main__.py
@@ -0,0 +1,45 @@
+import runpy
+import pyjion
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='pyjion', description='Python JIT Compiler')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('script', help='script file', nargs='?')
+
+    group.add_argument('-m', action='store', type=str, dest="module", help="Execute module")
+
+    parser.add_argument('--graph',
+                        action='store_true',
+                        help='Enable instruction graph generation')
+
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='Enable debuggable JIT methods')
+
+    parser.add_argument('--no-pgc',
+                        action='store_true',
+                        help='Disable PGC')
+
+    parser.add_argument('-o',
+                    '--opt-level',
+                    action='store', type=int, default=1,
+                    help='Optimization level (default 1)')
+
+    args = parser.parse_args()
+
+    pyjion.enable()
+
+    pyjion.config(graph=args.graph, debug=args.debug, pgc=not args.no_pgc, level=args.opt_level)
+
+    if args.module:
+        runpy.run_module(args.module, alter_sys=True)
+    else:
+        runpy.run_path(args.script, run_name="__main__")
+    pyjion.disable()
+
+
+if __name__ == "__main__":
+    main()
