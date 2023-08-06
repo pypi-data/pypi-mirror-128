50e7103fc1ec1df73bf638a366423820f6836095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:19 +0000

    Add package pax3/0.5.0/pax3-0.5.0.tar.gz

---
+++ packages/pax3/pax3-0.5.0.tar.gz/pax3-0.5.0/pax/experimental/__init__.py
@@ -0,0 +1,22 @@
+"""Experimental API"""
+
+
+from pax._src.core import Flattener, LazyModule
+from pax._src.utils import (
+    apply_scaled_gradients,
+    default_mp_policy,
+    save_weights_to_dict,
+    load_weights_from_dict,
+)
+
+from . import graph
+
+__all__ = (
+    "apply_scaled_gradients",
+    "default_mp_policy",
+    "Flattener",
+    "graph",
+    "LazyModule",
+    "load_weights_from_dict",
+    "save_weights_to_dict",
+)
