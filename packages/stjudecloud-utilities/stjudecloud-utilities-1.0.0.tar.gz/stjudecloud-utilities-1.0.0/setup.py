bbf4b1a6b7275a525a263acdc33390112d9409aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:02 +0000

    Add package stjudecloud-utilities/1.0.0/stjudecloud-utilities-1.0.0.tar.gz

---
+++ packages/stjudecloud-utilities/stjudecloud-utilities-1.0.0.tar.gz/stjudecloud-utilities-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['stjudecloud-utilities']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['deduplicate-feature-counts = '
+                     'stjudecloud-utilities.deduplicate_feature_counts:main',
+                     'gtf-to-rseqc-bed = '
+                     'stjudecloud-utilities.gtf_to_rseqc_bed:main']}
+
+setup_kwargs = {
+    'name': 'stjudecloud-utilities',
+    'version': '1.0.0',
+    'description': 'A set of utilities used on the St. Jude Cloud project.',
+    'long_description': None,
+    'author': 'St. Jude Cloud',
+    'author_email': 'support@stjude.cloud',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
