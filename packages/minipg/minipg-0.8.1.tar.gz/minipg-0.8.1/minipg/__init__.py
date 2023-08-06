102c74ddf60b04ebc365a429d1be220596f429b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:49 +0000

    Add package minipg/0.8.1/minipg-0.8.1.tar.gz

---
+++ packages/minipg/minipg-0.8.1.tar.gz/minipg-0.8.1/minipg/__init__.py
@@ -0,0 +1,9 @@
+from .err import *      # noqa
+from .pgcore import *   # noqa
+
+
+VERSION = (0, 8, 1)
+__version__ = '%s.%s.%s' % VERSION
+apilevel = '2.0'
+threadsafety = 1
+paramstyle = 'format'
