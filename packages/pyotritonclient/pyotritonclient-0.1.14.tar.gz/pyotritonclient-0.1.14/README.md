113f8c49b2fa10d339139710d087f20e9b9dccae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:51 +0000

    Add package pyotritonclient/0.1.14/pyotritonclient-0.1.14.tar.gz

---
+++ packages/pyotritonclient/pyotritonclient-0.1.14.tar.gz/pyotritonclient-0.1.14/README.md
@@ -0,0 +1,55 @@
+# Triton HTTP Client for Pyodide
+
+A Pyodide python http client library and utilities for communicating with Triton Inference Server (based on tritonclient from NVIDIA).
+
+
+This is a simplified implemetation of the triton client from NVIDIA, it works both in the browser with Pyodide Python or the native Python.
+It only implement the http client, and most of the API remains the similar but changed into async and with additional utility functions.
+
+## Usage
+
+To use it in native CPython, you can install the package by running:
+```
+pip install pyotritonclient[native]
+```
+
+For Pyodide-based Python environment, for example: [JupyterLite](https://jupyterlite.readthedocs.io/en/latest/_static/lab/index.html) or [Pyodide console](https://pyodide-cdn2.iodide.io/dev/full/console.html), you can install the client by running the following python code:
+```python
+import micropip
+micropip.install("pyotritonclient")
+```
+
+To execute the model, we provide utility functions to make it much easier:
+```python
+import numpy as np
+from pyotritonclient import get_config, execute_model
+
+# obtain the model config
+config = await get_config('https://triton.imjoy.io', 'cellpose-cyto')
+
+# create fake input tensors
+input0 = np.zeros([2, 349, 467], dtype='float32')
+input1 = np.array([30], dtype='float32')
+# run inference
+results = await execute_model([input0, input1], config=config)
+```
+
+You can access the lower level api, see the [test example](./tests/test_client.py).
+
+You can also find the official [client examples](https://github.com/triton-inference-server/client/tree/main/src/python/examples) demonstrate how to use the 
+package to issue request to [triton inference server](https://github.com/triton-inference-server/server). However, please notice that you will need to
+change the http client code into async style. For example, instead of doing `client.infer(...)`, you need to do `await client.infer(...)`.
+
+The http client code is forked from [triton client git repo](https://github.com/triton-inference-server/client) since commit [b3005f9db154247a4c792633e54f25f35ccadff0](https://github.com/triton-inference-server/client/tree/b3005f9db154247a4c792633e54f25f35ccadff0).
+
+
+## Server setup
+Since we access the server from the browser environment which typically has more security restrictions, it is important that the server is configured to enable browser access.
+
+Please make sure you configured following aspects:
+ * The server must provide HTTPS endpoints instead of HTTP
+ * The server should send the following headers:
+    - `Access-Control-Allow-Headers: Inference-Header-Content-Length,Accept-Encoding,Content-Encoding,Access-Control-Allow-Headers`
+    - `Access-Control-Expose-Headers: Inference-Header-Content-Length,Range,Origin,Content-Type`
+    - `Access-Control-Allow-Methods: GET,HEAD,OPTIONS,PUT,POST`
+    - `Access-Control-Allow-Origin: *` (This is optional depending on whether you want to support CORS)
---
+++ packages/pyotritonclient/pyotritonclient-0.1.14.tar.gz/pyotritonclient-0.1.14/pyotritonclient/__init__.py
@@ -0,0 +1,154 @@
+import json
+from pathlib import Path
+
+import numpy as np
+
+import pyotritonclient.http as httpclient
+from pyotritonclient.utils import np_to_triton_dtype, triton_to_np_dtype
+
+
+async def get_config(server_url, model_name, verbose=False):
+    """
+    Function for getting model config
+    """
+    with httpclient.InferenceServerClient(server_url, verbose=verbose) as client:
+        response = await client.get_model_config(model_name)
+        response["server_url"] = server_url
+        response["model_name"] = model_name
+        return response
+
+def _encode_input(data):
+    # automatically encode string and dict into np.bytes_
+    if isinstance(data, str):
+        bytes_data = str.encode(data, "utf-8")
+        in_data = np.array([bytes_data], dtype=np.object_)
+    elif isinstance(data, dict):
+        # encode the dictionary as as np.object_
+        bytes_data = str.encode(json.dumps(data), "utf-8")
+        in_data = np.array([bytes_data], dtype=np.object_)
+    elif isinstance(data, (tuple, list)):
+        in_data = np.stack(list(map(_encode_input, data)), axis=0)
+    else:
+        in_data = data
+    return in_data
+
+async def execute_model(
+    inputs,
+    server_url=None,
+    model_name=None,
+    config=None,
+    select_outputs=None,
+    request_id="",
+    model_version="",
+    compression_algorithm="deflate",
+    decode_bytes=False,
+    verbose=False,
+    **kwargs,
+):
+    """
+    Function for execute the model by passing a list of input tensors
+    """
+    if config is None:
+        if server_url is None or model_name is None:
+            raise Exception("Please provide config or server_url + model_name")
+
+        config = await get_config(server_url, model_name)
+    else:
+        server_url = config["server_url"]
+        model_name = config["model_name"]
+
+    inputc, outputc = config["input"], config["output"]
+    assert len(inputc) == len(
+        inputs
+    ), f"Invalid inputs number: {len(inputs)}, it should be {len(inputc)}"
+    input_names = [inputc[i]["name"] for i in range(len(inputc))]
+    output_names = [outputc[i]["name"] for i in range(len(outputc))]
+    input_types = [
+        inputc[i]["data_type"].lstrip("TYPE_").replace("STRING", "BYTES")
+        for i in range(len(inputc))
+    ]
+
+    for i in range(len(inputs)):
+        inputs[i] = _encode_input(inputs[i])
+
+        if inputs[i].dtype != triton_to_np_dtype(input_types[i]):
+            if not (
+                inputs[i].dtype == np.bytes_
+                and triton_to_np_dtype(input_types[i]) == np.object_
+            ):
+                raise TypeError(
+                    f"Input ({i}: {input_names[i]}) data type mismatch: {inputs[i].dtype} (should be {triton_to_np_dtype(input_types[i])})"
+                )
+
+    if select_outputs:
+        for out in select_outputs:
+            if out not in output_names:
+                raise Exception(f"Output name {out} does not exist.")
+    else:
+        select_outputs = output_names
+
+    with httpclient.InferenceServerClient(server_url, verbose=verbose) as client:
+        infer_inputs = [
+            httpclient.InferInput(input_names[i], inputs[i].shape, input_types[i])
+            for i in range(len(inputs))
+        ]
+
+        for i in range(len(inputs)):
+            infer_inputs[i].set_data_from_numpy(inputs[i], binary_data=True)
+
+        outputs = [
+            httpclient.InferRequestedOutput(output_names[i])
+            for i in range(len(output_names))
+            if output_names[i] in select_outputs
+        ]
+
+        response = await client.infer(
+            model_name,
+            infer_inputs,
+            model_version=model_version,
+            request_id=request_id,
+            outputs=outputs,
+            request_compression_algorithm=compression_algorithm,
+            response_compression_algorithm=compression_algorithm,
+            **kwargs,
+        )
+
+        info = response.get_response()
+        results = {
+            output["name"]: response.as_numpy(output["name"])
+            for output in info["outputs"]
+        }
+
+        if decode_bytes:
+            for k in results:
+                # decode bytes to utf-8
+                if results[k].dtype == np.object_:
+                    results[k] = results[k].astype(np.bytes_)
+                    results[k] = [
+                        str(np.char.decode(results[k][i], "UTF-8"))
+                        for i in range(len(results[k]))
+                    ]
+
+        results["__info__"] = info
+        return results
+
+_config_cache = {}
+async def execute(*inputs, server_url=None, model_name=None, cache_config=True, **kwargs):
+    """
+    Function for execute the model by passing a list of input tensors and using cached config
+    """
+    if cache_config:
+        if (server_url, model_name) not in _config_cache:
+            config = await get_config(server_url, model_name)
+            _config_cache[(server_url, model_name)] = config
+        else:
+            config = _config_cache[(server_url, model_name)]
+    else:
+        config = await get_config(server_url, model_name)
+    return await execute_model(*inputs, config=config, server_url=server_url, model_name=model_name, **kwargs)
+
+# read version information from file
+VERSION_INFO = json.loads(
+    (Path(__file__).parent / "VERSION").read_text(encoding="utf-8").strip()
+)
+__version__ = VERSION_INFO["version"]
