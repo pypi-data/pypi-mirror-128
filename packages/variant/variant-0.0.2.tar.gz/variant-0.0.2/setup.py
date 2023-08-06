434720af94b2a8ff330a63eed5b67d397c1177c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:41 +0000

    Add package variant/0.0.2/variant-0.0.2.tar.gz

---
+++ packages/variant/variant-0.0.2.tar.gz/variant-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['variant']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyensembl>=1.9.4,<2.0.0', 'varcode']
+
+entry_points = \
+{'console_scripts': ['variant-effect = variant.effect:run']}
+
+setup_kwargs = {
+    'name': 'variant',
+    'version': '0.0.2',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Chang Ye',
+    'author_email': 'yech1990@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
