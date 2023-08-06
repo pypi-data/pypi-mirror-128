9f1daaf993b231715874ddff6c20b671a3ea97db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:03 +0000

    Add package seno/0.0.1/seno-0.0.1.tar.gz

---
+++ packages/seno/seno-0.0.1.tar.gz/seno-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="seno",
+    version="0.0.1",
+    author="Jonatan",
+    author_email="contact@pornhub.com",
+    description="se no",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
