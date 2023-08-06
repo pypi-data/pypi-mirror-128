5a44ea1b0961665df5c305bc657e5d5fca86cf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:29 +0000

    Add package pyvitemadose/0.3.0/pyvitemadose-0.3.0.tar.gz

---
+++ packages/pyvitemadose/pyvitemadose-0.3.0.tar.gz/pyvitemadose-0.3.0/README.md
@@ -0,0 +1,35 @@
+# :syringe: pyvitemadose
+
+pyvitemadose displays available schedule of covid vaccine for your departement in France
+# 💫 Demo
+
+![demo_vitemadose](https://user-images.githubusercontent.com/45128847/137623852-d8e32778-cff4-4849-9a0c-af430aa78e75.gif)
+
+# 🚀 How to use **pyvitemadose**
+
+pyvitemadose \[DPT\] with DPT the department
+
+example : ``pyvitemadose 75`` to display available schedule availables in dpt 75
+
+## Other options
+
+  - ``-h/--help``    show the help message and exit
+  - ``-u/--update``  update pyvitemadose
+
+# ⚙️ Install
+
+See [this page](INSTALL.md)
+# :construction_worker: Contribution
+
+See [this page](CONTRIBUTING.md)
+
+# :package: Changelog
+
+See [this page](CHANGELOG.md)
+# License
+
+MIT License
+
+Copyright (c) 2021 [thib1984](https://github.com/thib1984)
+
+See [this page](LICENSE.txt) for details
---
+++ packages/pyvitemadose/pyvitemadose-0.3.0.tar.gz/pyvitemadose-0.3.0/pyvitemadose/pyvitemadose.py
@@ -0,0 +1,26 @@
+"""
+pyvitemadose use case
+"""
+
+import argparse
+import sys
+import requests
+
+def find(dept):
+    trouve = False
+    r = requests.get("https://vitemadose.gitlab.io/vitemadose/"+dept.zfill(2)+".json")
+    if str(r) != "<Response [200]>":
+        print(str(r) + " : no data found, verify the departement")
+        sys.exit(1)
+    for centre in r.json().get("centres_disponibles",[]):
+        if centre.get("appointment_count","") != 0:
+            trouve = True
+            print("1er rdv disp. : " + str(centre.get("prochain_rdv")[0:10]))    
+            print("url           : " + centre.get("url"))
+            print("adresse       : " + centre.get("metadata").get("address"))
+            print("type          : " + str(centre.get("vaccine_type")))
+            print("doses         : " + str(centre.get("appointment_count")))    
+            print("")                          
+    if not trouve:
+        print("pas de creneaux trouves...")
+        sys.exit(2)
