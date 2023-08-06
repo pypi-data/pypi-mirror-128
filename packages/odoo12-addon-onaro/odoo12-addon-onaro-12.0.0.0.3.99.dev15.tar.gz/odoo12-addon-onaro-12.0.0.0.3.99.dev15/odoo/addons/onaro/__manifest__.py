dce0d5016ba4665d4602e7381284c140c8e98335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:30 +0000

    Add package odoo12-addon-onaro/12.0.0.0.3.99.dev15/odoo12-addon-onaro-12.0.0.0.3.99.dev15.tar.gz

---
+++ packages/odoo12-addon-onaro/odoo12-addon-onaro-12.0.0.0.3.99.dev15.tar.gz/odoo12-addon-onaro-12.0.0.0.3.99.dev15/odoo/addons/onaro/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Odoo customizations for Onaro",
+    'version': '12.0.0.0.3',
+    'author': "Coopdevs Treball SCCL",
+    'depends': [
+        'auth_api_key',
+        'base',
+        'base_rest',
+        'crm_lead_product',
+        'helpdesk_mgmt',
+    ],
+    'description': """
+    Odoo Onaro customizations.
+    """,
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'license': "AGPL-3",
+    'data': [
+        'views/crm_lead.xml',
+        'reports/crm_lead_line_creation_email_template.xml',
+        'security/res_users.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
