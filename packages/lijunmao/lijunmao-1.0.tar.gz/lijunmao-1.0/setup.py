843e52e65700b72b002f6d0b8422d6ed0dfb1419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:34 +0000

    Add package lijunmao/1.0/lijunmao-1.0.tar.gz

---
+++ packages/lijunmao/lijunmao-1.0.tar.gz/lijunmao-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import setuptools
+packages = ['lijunmao']
+setup(name='lijunmao',
+        version='1.0',
+        author='yuesf',
+        packages=packages,
+        package_dir={'requests': 'requests'},)
