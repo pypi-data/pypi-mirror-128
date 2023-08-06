5db355e2450b3af47678861555a73ae3c7db4f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:27 +0000

    Add package pysnmp-pysmi/1.1.4/pysnmp_pysmi-1.1.4-py3-none-any.whl

---
+++ packages/pysnmp-pysmi/pysnmp_pysmi-1.1.4-py3-none-any.whl/pysmi/reader/url.py
@@ -0,0 +1,57 @@
+#
+# This file is part of pysmi software.
+#
+# Copyright (c) 2015-2019, Ilya Etingof <etingof@gmail.com>
+# License: http://snmplabs.com/pysmi/license.html
+#
+import sys
+
+from urllib import parse as urlparse
+from pysmi.reader.localfile import FileReader
+from pysmi.reader.zipreader import ZipReader
+from pysmi.reader.httpclient import HttpReader
+from pysmi import error
+
+
+def getReadersFromUrls(*sourceUrls, **options):
+    readers = []
+    for sourceUrl in sourceUrls:
+        mibSource = urlparse.urlparse(sourceUrl)
+
+        if sys.version_info[0:2] < (2, 5):
+            class ParseResult(tuple):
+                pass
+
+            mibSource = ParseResult(mibSource)
+
+            for k, v in zip(('scheme', 'netloc', 'path', 'params',
+                             'query', 'fragment', 'username', 'password',
+                             'hostname', 'port'), mibSource + ('', '', '', None)):
+                if k == 'scheme':
+                    if not mibSource[0] or mibSource[0] == 'file':
+                        if mibSource[2].endswith('.zip') or mibSource[2].endswith('.ZIP'):
+                            v = 'zip'
+
+                setattr(mibSource, k, v)
+
+        if mibSource.scheme in ('', 'file', 'zip'):
+            scheme = mibSource.scheme
+            if scheme != 'file' and (mibSource.path.endswith('.zip') or
+                                     mibSource.path.endswith('.ZIP')):
+                scheme = 'zip'
+
+            else:
+                scheme = 'file'
+
+            if scheme == 'file':
+                readers.append(FileReader(mibSource.path).setOptions(**options))
+            else:
+                readers.append(ZipReader(mibSource.path).setOptions(**options))
+
+        elif mibSource.scheme in ('http', 'https'):
+            readers.append(HttpReader(sourceUrl).setOptions(**options))
+
+        else:
+            raise error.PySmiError('Unsupported URL scheme %s' % sourceUrl)
+
+    return readers
