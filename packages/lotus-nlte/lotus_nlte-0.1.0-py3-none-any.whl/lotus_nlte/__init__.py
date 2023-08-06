3fb78909a6e62c841a6210d91da8e5ac6859714d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:11 +0000

    Add package lotus-nlte/0.1.0/lotus_nlte-0.1.0-py3-none-any.whl

---
+++ packages/lotus-nlte/lotus_nlte-0.1.0-py3-none-any.whl/lotus_nlte/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__all__ = ["__version__", "gcogs", "optimize", "interpolation", "utils", "sampling", "plot", "config"]
+from .config import *
+from . import utils
+from .interpolation import *
+from .gcogs.multigcogs import PolyMultiCGOG
+from . import optimize
+from . import sampling
+from . import plot
+#from .LOTUS_version import __version__
+from .lotus_nlte_version import __version__
+
+__author__ = "Yangyang Li"
+__email__ = "ly95astro@gmail.com"
+__url__ = "https://github.com/Li-Yangyang/LOTUs"
+__license__ = "MIT"
+__description__ = "Determine atmospheric stellar parameters in non-LTE"
+__copyright__ = "Copyright 2021 Yangyang Li"
+__contributors__ = "https://github.com/Li-Yangyang/LOTUs/graphs/contributors"
+__bibtex__ = __citation__ = """TBD"""
+#__version__ = "0.0.1a1"
