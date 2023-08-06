93797f19c0bce2c9fcf4a94ac9e24c7e9f461021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:05 +0000

    Add package water-masses/2021.3.2/water-masses-2021.3.2.tar.gz

---
+++ packages/water-masses/water-masses-2021.3.2.tar.gz/water-masses-2021.3.2/src/water_masses/filtering.py
@@ -0,0 +1,39 @@
+from typing import Callable, Dict, List
+
+import cf_xarray as cfxr  # noqa
+import numpy as np
+import xarray as xr
+from scipy import signal
+
+
+def apply_filter(
+    filter_func: Callable[..., np.ndarray],
+    data: xr.DataArray,
+    filter_args: List[float],
+    filter_kwgs: Dict[str, int],
+) -> xr.DataArray:
+    """Apply filter to xarray DataArray."""
+    data = data.copy()
+    data.values = xr.apply_ufunc(
+        filter_func,
+        data,
+        *filter_args,
+        kwargs=filter_kwgs,
+        input_core_dims=[["time"], *[[] for _ in range(len(filter_args))]],
+        output_core_dims=[["time"]],
+        vectorize=True,
+    ).cf.transpose("time", "latitude", "lonngitude")
+
+    return data
+
+
+def butter_lowpass_filter(
+    data: np.ndarray, cutlen: int = 15, fs: int = 12, order=5
+) -> np.ndarray:
+    """1D lowpass butterworth filter."""
+    return signal.sosfiltfilt(
+        signal.butter(
+            order, 1 / cutlen / (0.5 * fs), analog=False, btype="lowpass", output="sos"
+        ),
+        data,
+    )
