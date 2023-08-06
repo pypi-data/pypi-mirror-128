d02ed8c2961484bc71bc100a0805889692a640d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:36 +0000

    Add package prototype-python-library/0.1.0/prototype-python-library-0.1.0.tar.gz

---
+++ packages/prototype-python-library/prototype-python-library-0.1.0.tar.gz/prototype-python-library-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['prototype_python_library']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'prototype-python-library',
+    'version': '0.1.0',
+    'description': 'A prototype python library.',
+    'long_description': None,
+    'author': 'Nuno Costa',
+    'author_email': '91nunocosta@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
