7234bfd5183e076849e9ccaeaddd1968c95d4799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:42 +0000

    Add package parametric-builder/0.3.1/parametric-builder-0.3.1.tar.gz

---
+++ packages/parametric-builder/parametric-builder-0.3.1.tar.gz/parametric-builder-0.3.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['parametric_builder']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'parametric-builder',
+    'version': '0.3.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Bidimpata-Kerim Ntumba Tshimanga',
+    'author_email': 'bk.tshimanga@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
