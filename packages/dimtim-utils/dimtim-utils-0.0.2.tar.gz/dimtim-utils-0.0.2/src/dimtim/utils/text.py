a8b21ef0a21c11b6a2bca071753951ef7a17fbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:55 +0000

    Add package dimtim-utils/0.0.2/dimtim-utils-0.0.2.tar.gz

---
+++ packages/dimtim-utils/dimtim-utils-0.0.2.tar.gz/dimtim-utils-0.0.2/src/dimtim/utils/text.py
@@ -0,0 +1,24 @@
+import re
+import unicodedata
+
+
+def slugify(value: str, allow_unicode: bool = False):
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', str(value))
+    else:
+        value = unicodedata.normalize('NFKD', str(value)).encode('ascii', 'ignore').decode('ascii')
+    return re.sub(r'[-\s]+', '-', re.sub(r'[^\w\s-]', '', value.lower()).strip())
+
+
+def strip_markdown(value: str):
+    try:
+        from bs4 import BeautifulSoup
+        import markdown
+    except ImportError:
+        raise ModuleNotFoundError(f'Mthod "{strip_markdown.__name__}" requred "beautifulsoup4" and "markdown" libraries')
+
+    return BeautifulSoup(markdown.markdown(value), 'lxml').text
+
+
+def to_snake_case(text: str):
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)).lower()
