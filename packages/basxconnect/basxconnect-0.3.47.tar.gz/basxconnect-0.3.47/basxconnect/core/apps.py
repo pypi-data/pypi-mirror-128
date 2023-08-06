7cdaeb12885633c59251ae7c7350dd60fd764679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:27 +0000

    Add package basxconnect/0.3.47/basxconnect-0.3.47.tar.gz

---
+++ packages/basxconnect/basxconnect-0.3.47.tar.gz/basxconnect-0.3.47/basxconnect/core/apps.py
@@ -0,0 +1,31 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class CoreConfig(AppConfig):
+    name = "basxconnect.core"
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self):
+        from .models import Vocabulary
+
+        pre_installed_vocabulary = {
+            "tag": _("Tags"),
+            "title": _("Titles"),
+            "salutation": _("Salutations"),
+            "form_of_address": _("Forms Of Address"),
+            "gender": _("Genders"),
+            "naturaltype": _("Natural Person Types"),
+            "legaltype": _("Legal Person Types"),
+            "associationtype": _("Association Person Types"),
+            "addressstatus": _("Address statuses"),
+            "emailtype": _("Email Types"),
+            "urltype": _("URL Types"),
+            "phonetype": _("Phone Types"),
+            "addresstype": _("Address Types"),
+        }
+        try:
+            for slug, name in pre_installed_vocabulary.items():
+                Vocabulary.objects.get_or_create(slug=slug, defaults={"name": name})
+        except Exception:  # nosec this is trivial, no service interuption
+            pass
