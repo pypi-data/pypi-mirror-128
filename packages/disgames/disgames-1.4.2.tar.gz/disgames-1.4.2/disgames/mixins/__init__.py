cb3c4fa6872ce27df518d6c983ea3613c43c64a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:33 +0000

    Add package disgames/1.4.2/disgames-1.4.2.tar.gz

---
+++ packages/disgames/disgames-1.4.2.tar.gz/disgames-1.4.2/disgames/mixins/__init__.py
@@ -0,0 +1,17 @@
+from .hangman import Hangman
+from .madlib import MadLib
+from .ttt import TicTacToe
+from .chess import Chess
+from .minesweeper import Minesweeper
+from .soko import Sokoban
+from .rps import RPS
+
+__all__ = (
+    "Hangman",
+    "MadLib",
+    "TicTacToe",
+    "Chess",
+    "Minesweeper",
+    "Sokoban",
+    "RPS"
+)
