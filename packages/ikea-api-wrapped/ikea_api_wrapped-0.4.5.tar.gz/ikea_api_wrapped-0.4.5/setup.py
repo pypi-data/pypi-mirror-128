78fa381b1a1e28c0dd6dc5a3498deaec235c39e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:01 +0000

    Add package ikea-api-wrapped/0.4.5/ikea_api_wrapped-0.4.5.tar.gz

---
+++ packages/ikea-api-wrapped/ikea_api_wrapped-0.4.5.tar.gz/ikea_api_wrapped-0.4.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="ikea_api_wrapped",
+        install_requires=[
+            "ikea-api==0.8.0",
+            "python-box==5.4.1",
+        ],
+        extras_require={
+            "dev": [
+                "black==21.10b0",
+                "pre-commit==2.15.0",
+            ]
+        },
+    )
---
+++ packages/ikea-api-wrapped/ikea_api_wrapped-0.4.5.tar.gz/ikea_api_wrapped-0.4.5/src/ikea_api_wrapped/__init__.py
@@ -0,0 +1,19 @@
+from ikea_api_wrapped._utils import format_item_code, parse_item_codes
+from ikea_api_wrapped._wrappers import (
+    add_items_to_cart,
+    get_delivery_services,
+    get_items,
+    get_purchase_history,
+    get_purchase_info,
+)
+
+__version__ = "0.4.5"
+__all__ = [
+    "parse_item_codes",
+    "format_item_code",
+    "get_purchase_history",
+    "get_purchase_info",
+    "get_delivery_services",
+    "add_items_to_cart",
+    "get_items",
+]
