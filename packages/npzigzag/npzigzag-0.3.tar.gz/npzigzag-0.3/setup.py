6e4992d479c6fcd65a1f05df877c84467d82b5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:29 +0000

    Add package npzigzag/0.3/npzigzag-0.3.tar.gz

---
+++ packages/npzigzag/npzigzag-0.3.tar.gz/npzigzag-0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+import pathlib
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "readme.md").read_text()
+setup(
+    name='npzigzag',
+    packages=find_packages(include=['npzigzag']),
+    version='0.3',
+    description='Vanilla numpy implementation of zigzag indicator.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Julian van Meggelen',
+    license='MIT',
+    install_requires = [
+        'numpy==1.19.1',
+        'pandas==1.1.0'
+        ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==6.2.5'],
+    test_suite='npzigzag_tests',
+    author_email = 'julianvanmeggelen@icloud.com',      
+    url = 'https://github.com/julianvanmeggelen/npzigzag',  
+    download_url = 'https://github.com/julianvanmeggelen/npzigzag/archive/refs/tags/v01.tar.gz',    # I explain this later on
+    keywords = ['zigzag', 'zigzag indicator'],   
+    classifiers=[
+        'Development Status :: 3 - Alpha',      #  "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   #license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
