c4e81660e2b20ea9643a00491e34ea2587eb21c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:22 +0000

    Add package veriftools/1.0/veriftools-1.0.tar.gz

---
+++ packages/veriftools/veriftools-1.0.tar.gz/veriftools-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='veriftools',
+      version='1.0',
+      description='Generating images',
+      packages=['veriftools'],
+      author_email='veriftools@gmail.com',
+      zip_safe=False)
