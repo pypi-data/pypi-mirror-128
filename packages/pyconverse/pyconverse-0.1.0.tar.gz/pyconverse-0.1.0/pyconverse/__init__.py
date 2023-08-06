6ca0263e49bbbd1831e12ec3d1e8f764000bfe89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:43 +0000

    Add package pyconverse/0.1.0/pyconverse-0.1.0.tar.gz

---
+++ packages/pyconverse/pyconverse-0.1.0.tar.gz/pyconverse-0.1.0/pyconverse/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1.0'
+from .insights import Callyzer
+from .speaker_stats import SpeakerStats
+from .zeroshot_topic_model import ZeroShotTopicFinder
+from .segmentation import SemanticTextSegmention
