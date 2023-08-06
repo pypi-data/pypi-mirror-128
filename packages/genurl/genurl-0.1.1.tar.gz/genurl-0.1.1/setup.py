07e1caeb78a284cb659aaa5e64a74613d60bd7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:07 +0000

    Add package genurl/0.1.1/genurl-0.1.1.tar.gz

---
+++ packages/genurl/genurl-0.1.1.tar.gz/genurl-0.1.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['genurl']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Faker>=9.8.2,<10.0.0',
+ 'PyYAML>=6.0,<7.0',
+ 'Random-Word>=1.0.7,<2.0.0',
+ 'tqdm>=4.62.3,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['genurl = genurl.genurl:main']}
+
+setup_kwargs = {
+    'name': 'genurl',
+    'version': '0.1.1',
+    'description': 'GENerate random URLs',
+    'long_description': None,
+    'author': 'Sam Havens',
+    'author_email': 'sam.havens@writer.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
