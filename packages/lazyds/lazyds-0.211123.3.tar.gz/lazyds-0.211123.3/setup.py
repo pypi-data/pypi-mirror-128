69017f1b926da732e798039591e995ba8febda34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package lazyds/0.211123.3/lazyds-0.211123.3.tar.gz

---
+++ packages/lazyds/lazyds-0.211123.3.tar.gz/lazyds-0.211123.3/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['lazyds',
+ 'lazyds.data',
+ 'lazyds.evaluation',
+ 'lazyds.learning',
+ 'lazyds.learning.supervised',
+ 'lazyds.learning.supervised.classification']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['arff2pandas>=1.0.1,<2.0.0',
+ 'idict>=1.211123.4,<2.0.0',
+ 'pandas>=1.3.2,<2.0.0',
+ 'scikit-learn>=1.0.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'lazyds',
+    'version': '0.211123.3',
+    'description': 'Useful Data Science libraries wrapped by laziness',
+    'long_description': None,
+    'author': 'davips',
+    'author_email': 'dpsabc@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.10',
+}
+
+
+setup(**setup_kwargs)
