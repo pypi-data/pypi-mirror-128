a3808cc2ca29777e6bac6390178c2805b5cd0ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:29 +0000

    Add package cmdMenuFramework/1.0.2/cmdMenuFramework-1.0.2.tar.gz

---
+++ packages/cmdMenuFramework/cmdMenuFramework-1.0.2.tar.gz/cmdMenuFramework-1.0.2/cmdMenuFramework/Menu.py
@@ -0,0 +1,60 @@
+
+
+from cmdMenuFramework.Printer import Printer
+from cmdMenuFramework.Context import Context
+from cmdMenuFramework.instruction.Instruction import Instruction
+import yaml
+
+class Menu:
+    
+    
+    def __init__(self, file = 'yaml/index.yaml', context:Context = None):
+        self.context = Context(file)
+        if context is not None:
+            self.context.vars = context.vars
+            self.context.controllerFile = context.controllerFile
+            self.context.controllerClass = context.controllerClass
+            self.context.input = context.input
+            self.context.exit = context.exit
+            self.context.exception = context.exception
+            self.context.finish = context.finish
+            self.context.choicePrompt = context.choicePrompt
+            self.context.inputPrompt = context.inputPrompt
+            self.context.indent = context.indent
+            self.context.showTrace = context.showTrace
+            self.context.indentWithPipes = context.indentWithPipes
+            self.context.availableChoices = context.availableChoices
+            self.context.path = context.path
+            
+        
+    
+    def start(self):
+        # loads then executes the base instructions, one by one.
+        try: 
+            with open(self.context.file) as f:
+                self.instructionsAsDict = yaml.load(f, Loader=yaml.FullLoader)
+                self.execute()
+                return self.context.output
+        except FileNotFoundError: 
+            Printer.printError("File of menu to launch does not exist: "+str(self.context.file), self.context)
+    
+    
+    def execute(self):
+        for id in self.instructionsAsDict:
+            
+            if self.context.exit == True:
+                self.context.exit = False
+                break
+                
+            
+            i = Instruction.letterToInstruction(id[:1], id, self.instructionsAsDict[id], self.context)
+            
+            if i is None: 
+                continue
+            i.execute()
+    
+    
+    
+    
+    
+    
---
+++ packages/cmdMenuFramework/cmdMenuFramework-1.0.2.tar.gz/cmdMenuFramework-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'cmdMenuFramework',         
+  packages = ['cmdMenuFramework'],  
+  version = '1.0.2',
+  license='MIT',
+  description = 'This is a library/framework specialised in creating a command line menus, through the use of yaml files.',   # Give a short description about your library
+  author = 'ARAGON Esteban',             
+  author_email = 'aragonstban@gmail.com', 
+  url = 'https://github.com/Este2013/command_line_menu_python',   
+  download_url = 'https://github.com/Este2013/command_line_menu_python/archive/refs/tags/v1.0.2.tar.gz', 
+  keywords = ['command', 'cmd', 'line', 'menu', 'framework'], 
+  install_requires=[  
+          'colorama',
+          'pyyaml',
+          'importlib'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',   # Choices are ["3 - Alpha", "4 - Beta", "5 - Production/Stable"]
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+)
