735a4a2303321a1f542ebc5a85167da17c9a9f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:34 +0000

    Add package AoE2ScenarioParser/0.1.28/AoE2ScenarioParser-0.1.28.tar.gz

---
+++ packages/AoE2ScenarioParser/AoE2ScenarioParser-0.1.28.tar.gz/AoE2ScenarioParser-0.1.28/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AoE2ScenarioParser",
+    version="0.1.28",
+    author="Kerwin Sneijders",
+    author_email="ksneijders@hotmail.com",
+    description="This is a project for editing parts of an 'aoe2scenario' file from Age of Empires 2 Definitive Edition",
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KSneijders/AoE2ScenarioParser",
+    packages=setuptools.find_namespace_packages(),
+
+    # According to the docs glob patterns are allowed.
+    # But when using: "versions/**/*.json", it does not work.
+    package_data={
+        'AoE2ScenarioParser': [
+            'datasets/sources/*.json',
+            'versions/*/*/*.json'
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'deprecation'
+    ]
+)
