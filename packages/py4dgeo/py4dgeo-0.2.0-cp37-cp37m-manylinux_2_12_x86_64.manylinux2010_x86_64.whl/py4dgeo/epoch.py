11b3c0a2fe0003da33e9b7b9b7190557a3ab8f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package py4dgeo/0.2.0/py4dgeo-0.2.0-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/py4dgeo/py4dgeo-0.2.0-cp37-cp37m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/py4dgeo/epoch.py
@@ -0,0 +1,41 @@
+from py4dgeo.util import (
+    Py4DGeoError,
+    as_double_precision,
+    make_contiguous,
+)
+
+import numpy as np
+import py4dgeo._py4dgeo as _py4dgeo
+
+
+class Epoch(_py4dgeo.Epoch):
+    def __init__(self, cloud: np.ndarray):
+        # Check the given array shapes
+        if len(cloud.shape) != 2 or cloud.shape[1] != 3:
+            raise Py4DGeoError("Clouds need to be an array of shape nx3")
+
+        # Make sure that cloud is double precision and contiguous in memory
+        cloud = as_double_precision(cloud)
+        cloud = make_contiguous(cloud)
+
+        # Call base class constructor
+        super().__init__(cloud)
+
+        # Build the KDTree index
+        # TODO: When exactly should this be done and how do we want to
+        #       the user interface to look like?
+        self.kdtree.build_tree(10)
+
+
+def as_epoch(cloud):
+    """Create an epoch from a cloud
+
+    Idempotent operation to create an epoch from a cloud.
+    """
+
+    # If this is already an epoch, this is a no-op
+    if isinstance(cloud, Epoch):
+        return cloud
+
+    # Initialize an epoch from the given cloud
+    return Epoch(cloud)
