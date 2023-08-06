3e5b0e8b968c0d79ef293e4293b7af887458bdaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:36 +0000

    Add package light-curve/0.4.0_rc.1/light_curve-0.4.0_rc.1.tar.gz

---
+++ packages/light-curve/light_curve-0.4.0_rc.1.tar.gz/light_curve-0.4.0_rc.1/local_dependencies/light-curve-common/src/lib.rs
@@ -0,0 +1,8 @@
+mod allclose;
+pub use allclose::all_close;
+
+mod linspace;
+pub use linspace::linspace;
+
+mod sort_multiple;
+pub use sort_multiple::sort_multiple;
---
+++ packages/light-curve/light_curve-0.4.0_rc.1.tar.gz/light_curve-0.4.0_rc.1/local_dependencies/light-curve/src/lib.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let x = 2;
+        let y = 4;
+        assert_eq!(2 * x, y);
+    }
+}
