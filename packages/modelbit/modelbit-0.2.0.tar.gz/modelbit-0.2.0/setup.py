da9626e20e0888db46c0df952e83208abec8d7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:49 +0000

    Add package modelbit/0.2.0/modelbit-0.2.0.tar.gz

---
+++ packages/modelbit/modelbit-0.2.0.tar.gz/modelbit-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='modelbit',
+    version='0.2.0',    
+    description='Python package to connect Jupyter notebooks to Modelbit',
+    url='https://www.modelbit.com',
+    author='Modelbit',
+    author_email='tom@modelbit.com',
+    license='MIT',
+    packages=['modelbit'],
+    install_requires=['timeago',
+                      'pycryptodome',
+                      'pandas'
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft',
+        'Programming Language :: Python :: 3',
+    ],
+)
