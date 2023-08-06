45343ec8b90d188149dbbfafb4ec3ba6d4c60680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:35 +0000

    Add package easy-data-explorer-amandeepfj/0.0.5/easy-data-explorer-amandeepfj-0.0.5.tar.gz

---
+++ packages/easy-data-explorer-amandeepfj/easy-data-explorer-amandeepfj-0.0.5.tar.gz/easy-data-explorer-amandeepfj-0.0.5/README.md
@@ -0,0 +1,18 @@
+# Easy Data Explorer
+Python library to do common data exploring tasks 
+
+## To build
+
+Generate dist
++ `py -m pip install --upgrade build`
++ `py -m build`
+
+Upload dist
++ `py -m pip install --upgrade twine`
++ `py -m twine upload  dist/*`
+
+Install library
++ `py -m pip install easy-data-explorer-amandeepfj`
+
+Upgrade to latest version library
++ `py -m pip install easy-data-explorer-amandeepfj -U`
---
+++ packages/easy-data-explorer-amandeepfj/easy-data-explorer-amandeepfj-0.0.5.tar.gz/easy-data-explorer-amandeepfj-0.0.5/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Easy Data Explorer'
+LONG_DESCRIPTION = 'Python library to do common data exploring tasks.'
+
+# Setting up
+setuptools.setup(
+    name="easy-data-explorer-amandeepfj",
+    version=VERSION,
+    author="amandeepfj (Amandeep Jiddewar)",
+    author_email="<amandeep.jiddewar@alumni.emory.edu>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/amandeepfj/easy_data_explorer",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['pandas', 'numpy', 'matplotlib', 'seaborn', 'pydataset'],
+    keywords=['python', 'data explore'],
+    project_urls={
+        "Bug Tracker": "https://github.com/amandeepfj/easy_data_explorer/issues",
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
