204fe265d4ed82f9b4f371d5dc3c044f0778149a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:25 +0000

    Add package caffoa/0.33.0/caffoa-0.33.0-py3-none-any.whl

---
+++ packages/caffoa/caffoa-0.33.0-py3-none-any.whl/caffoa/data/base/CaffoaClientError.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace {NAMESPACE}
+{{
+    public abstract class CaffoaClientError : Exception
+    {{
+        public CaffoaClientError() : base(){{}}
+        public CaffoaClientError(string msg) : base(msg){{}}
+        public abstract IActionResult Result {{ get; }}
+    }}
+
+    public class CaffoaJsonParseError : CaffoaClientError
+    {{
+        public CaffoaJsonParseError(string msg) : base("Error during JSON parsing of payload: " + msg){{}}
+
+        public static CaffoaJsonParseError NoContent()
+        {{
+            return new CaffoaJsonParseError("no body found");
+        }}
+
+        public static CaffoaJsonParseError FromException(Exception err)
+        {{
+            return new CaffoaJsonParseError(err.GetType().Name + ": " + err.Message);
+        }}
+
+        public static Exception WrongContent(string type, object value)
+        {{
+            return new CaffoaJsonParseError($"Could not find type to parse for discriminator {{value}}");
+        }}
+        public override IActionResult Result {{ get => new ContentResult {{Content = Message, StatusCode = 400}}; }}
+    }}
+}}
---
+++ packages/caffoa/caffoa-0.33.0-py3-none-any.whl/caffoa/data/base/ModelEnumPropertyTemplate.cs
@@ -0,0 +1,22 @@
+        // constant values for "{NAMELOWER}"
+        {ENUMS}
+
+        /// <summary>
+        /// immutable array containing all allowed values for "{NAMELOWER}"
+        /// </summary>
+        public static readonly ImmutableArray<{TYPE}> {ENUM_LIST_NAME}
+            = new ImmutableArray<{TYPE}>() {{ {ENUM_NAMES} }};
+
+        [JsonIgnore]
+        private {TYPE} _{NAMELOWER}{DEFAULT};
+
+        {DESCRIPTION}{JSON_EXTRA}[JsonProperty("{NAMELOWER}"{JSON_PROPERTY_EXTRA})]
+        public virtual {TYPE} {NAMEUPPER} {{
+            get {{
+                return _{NAMELOWER};
+            }}
+            set {{
+                {ENUM_CHECK}
+                _{NAMELOWER} = value;
+            }}
+        }}
