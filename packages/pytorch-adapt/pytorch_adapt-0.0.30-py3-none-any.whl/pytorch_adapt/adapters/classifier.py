a463fc95717600e8d5ebe276a0f16719349ad8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package pytorch-adapt/0.0.30/pytorch_adapt-0.0.30-py3-none-any.whl

---
+++ packages/pytorch-adapt/pytorch_adapt-0.0.30-py3-none-any.whl/pytorch_adapt/adapters/classifier.py
@@ -0,0 +1,34 @@
+import torch
+
+from ..containers import KeyEnforcer, MultipleContainers, Optimizers
+from ..hooks import ClassifierHook, FinetunerHook
+from .base_adapter import BaseGCAdapter
+from .utils import default_optimizer_tuple
+
+
+class Classifier(BaseGCAdapter):
+    """
+    Wraps [ClassifierHook][pytorch_adapt.hooks.classification.ClassifierHook].
+    """
+
+    hook_cls = ClassifierHook
+
+    def init_hook(self, hook_kwargs):
+        self.hook = self.hook_cls(opts=list(self.optimizers.values()), **hook_kwargs)
+
+
+class Finetuner(Classifier):
+    """
+    Wraps [FinetunerHook][pytorch_adapt.hooks.classification.FinetunerHook].
+    """
+
+    hook_cls = FinetunerHook
+
+    def get_default_containers(self):
+        optimizers = Optimizers(default_optimizer_tuple(), keys=["C"])
+        return MultipleContainers(optimizers=optimizers)
+
+    def get_key_enforcer(self):
+        ke = super().get_key_enforcer()
+        ke.requirements["optimizers"].remove("G")
+        return ke
