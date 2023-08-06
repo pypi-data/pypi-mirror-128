08cdb7bd067de2196c3182c2892999aa0043a1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:04 +0000

    Add package minos-microservice-saga/0.3.2/minos_microservice_saga-0.3.2-py3-none-any.whl

---
+++ packages/minos-microservice-saga/minos_microservice_saga-0.3.2-py3-none-any.whl/minos/saga/__init__.py
@@ -0,0 +1,73 @@
+__author__ = """Clariteia Devs"""
+__email__ = "devs@clariteia.com"
+__version__ = "0.3.2"
+
+from .context import (
+    SagaContext,
+)
+from .definitions import (
+    ConditionalSagaStep,
+    ElseThenAlternative,
+    IfThenAlternative,
+    LocalSagaStep,
+    RemoteSagaStep,
+    Saga,
+    SagaOperation,
+    SagaStep,
+)
+from .exceptions import (
+    AlreadyCommittedException,
+    AlreadyOnSagaException,
+    EmptySagaException,
+    EmptySagaStepException,
+    MultipleElseThenException,
+    MultipleOnErrorException,
+    MultipleOnExecuteException,
+    MultipleOnFailureException,
+    MultipleOnSuccessException,
+    SagaException,
+    SagaExecutionAlreadyExecutedException,
+    SagaExecutionException,
+    SagaExecutionNotFoundException,
+    SagaFailedCommitCallbackException,
+    SagaFailedExecutionException,
+    SagaFailedExecutionStepException,
+    SagaNotCommittedException,
+    SagaNotDefinedException,
+    SagaPausedExecutionStepException,
+    SagaResponseException,
+    SagaRollbackExecutionException,
+    SagaRollbackExecutionStepException,
+    SagaStepException,
+    SagaStepExecutionException,
+    UndefinedOnExecuteException,
+)
+from .executions import (
+    ConditionalSagaStepExecution,
+    Executor,
+    LocalExecutor,
+    LocalSagaStepExecution,
+    RemoteSagaStepExecution,
+    RequestExecutor,
+    ResponseExecutor,
+    SagaExecution,
+    SagaExecutionStorage,
+    SagaStatus,
+    SagaStepExecution,
+    SagaStepStatus,
+    TransactionCommitter,
+)
+from .manager import (
+    SagaManager,
+)
+from .messages import (
+    SagaRequest,
+    SagaResponse,
+    SagaResponseStatus,
+)
+from .middleware import (
+    transactional_command,
+)
+from .services import (
+    SagaService,
+)
