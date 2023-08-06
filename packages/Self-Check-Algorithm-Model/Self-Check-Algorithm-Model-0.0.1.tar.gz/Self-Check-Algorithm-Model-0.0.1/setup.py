77216a473f60ccd84aa139b30f6c597abec3e1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:50 +0000

    Add package Self-Check-Algorithm-Model/0.0.1/Self-Check-Algorithm-Model-0.0.1.tar.gz

---
+++ packages/Self-Check-Algorithm-Model/Self-Check-Algorithm-Model-0.0.1.tar.gz/Self-Check-Algorithm-Model-0.0.1/setup.py
@@ -0,0 +1,30 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Self-Check-Algorithm-Model",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
