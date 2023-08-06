6f8a3ec830d6712a7f129ba8ffa2cceb336a1ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:48 +0000

    Add package blenderless/0.1.8/blenderless-0.1.8-py3-none-any.whl

---
+++ packages/blenderless/blenderless-0.1.8-py3-none-any.whl/blenderless/cli.py
@@ -0,0 +1,65 @@
+import logging
+import pathlib
+
+import click
+from tqdm import tqdm
+
+from blenderless import Blenderless
+
+l = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option('--verbose/--no-verbose', '-v', default=False, help="Verbose output")
+@click.option('--export-blend-path', '-b', default=None, help="Path to export the generated .blend file to")
+def main(verbose, export_blend_path):
+    """Rendering geometries from the cli using blender"""
+    level = logging.DEBUG if verbose else logging.INFO
+    logging.basicConfig(level=level, format='%(processName)s %(message)s')
+    logging.addLevelName(logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+    logging.addLevelName(logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+
+    Blenderless.export_blend_path = export_blend_path
+    if export_blend_path:
+        logging.info(f'Generated .blend file will be exported to: {export_blend_path}')
+
+
+@main.command()
+@click.argument("file_path", required=True, type=str)
+@click.argument("root", default=".", required=False, type=str)
+def image(file_path, root):
+    """Render geometries to image"""
+    geometry_files = list(pathlib.Path(root).glob(file_path))
+    l.info(f'found {len(geometry_files)} geometry files')
+    for geometry_file in tqdm(geometry_files):
+        l.debug(f'render: {geometry_file}')
+        Blenderless.render(geometry_file, geometry_file.parent / f'{geometry_file.stem}.png')
+        l.info((geometry_file.parent / f'{geometry_file.stem}.png').absolute())
+        l.debug(f'render successful')
+
+
+@main.command()
+@click.argument("file_path", required=True, type=str)
+@click.argument("root", default=".", required=False, type=str)
+def gif(file_path, root):
+    """Render geometries to gif"""
+    geometry_files = list(pathlib.Path(root).glob(file_path))
+    l.info(f'found {len(geometry_files)} geometry files')
+    for geometry_file in tqdm(geometry_files):
+        l.debug(f'render: {geometry_file}')
+        Blenderless.gif(geometry_file, geometry_file.parent / f'{geometry_file.stem}.gif')
+        l.debug(f'render successful')
+
+
+@main.command()
+@click.argument("config_path", required=True, type=click.Path(exists=True))
+@click.argument("output_file", default="render.png", required=False, type=str)
+def config(config_path, output_file):
+    """Render scene from config file"""
+    Blenderless.render_from_config(config_path, output_file)
+    l.info(pathlib.Path(output_file).absolute())
+    l.debug(f'render successful')
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/blenderless/blenderless-0.1.8-py3-none-any.whl/blenderless/material.py
@@ -0,0 +1,75 @@
+import pathlib
+from dataclasses import dataclass
+from typing import List
+
+import numpy as np
+
+DEFAULT_MATERIAL_PATH = pathlib.Path(__file__).parent / 'data/materials.blend'
+
+
+def load_default_materials(bpy):
+    load_materials(bpy, DEFAULT_MATERIAL_PATH)
+
+
+def load_materials(bpy, filepath):
+    """Load materials from materials file.
+
+    Use the MaterialFromName class to load materials from this file.
+    """
+    with bpy.data.libraries.load(str(filepath.absolute())) as (data_from, data_to):
+        data_to.materials = data_from.materials
+
+
+@dataclass
+class Material:
+    """Material base class."""
+    material_name: str = ''
+    _blender_material = None
+
+
+@dataclass
+class MaterialRGBA(Material):
+    """Create diffuse single color material."""
+    rgba: List[float] = (0, 0, 255, 255)  # default color blue
+
+    def blender_material(self, bpy):
+        if self._blender_material is None:
+            self._blender_material = bpy.data.materials.new(name=self.material_name)
+            self._blender_material.diffuse_color = self.rgba
+        return self._blender_material
+
+    @staticmethod
+    def material_list_from_colormap(colormap: np.ndarray) -> List[Material]:
+        """Create list of materials based on colormap.
+
+        Args:
+            colormap (np.ndarray): row-wise colors, Shape (?, 3) or (?, 4)
+        """
+        n, d = colormap.shape
+        if d == 3:
+            colormap = np.concatenate((colormap, np.ones((n, 1))), axis=1)
+
+        materials = [None] * n
+        for i in range(n):
+            materials[i] = MaterialRGBA(rgba=tuple(colormap[i, :]), material_name=f'label{i}')
+        return materials
+
+
+@dataclass
+class MaterialFromName(Material):
+    """Material loader using string identifier.
+
+    Load material from preset file, see load_materials().
+    """
+    _blender_material = None
+
+    def blender_material(self, bpy):
+        if self._blender_material is None:
+            self._blender_material = bpy.data.materials[self.material_name]
+        return self._blender_material
+
+
+def add_material(blender_object, blender_material):
+    """Add material to blender object."""
+    blender_object.data.materials.clear()
+    blender_object.data.materials.append(blender_material)
