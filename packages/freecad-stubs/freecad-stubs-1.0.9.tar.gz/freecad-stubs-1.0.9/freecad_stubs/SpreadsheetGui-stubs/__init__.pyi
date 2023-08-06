4b6dc8fc3c679e10acc796b75e7d5d4aa0fccc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:37 +0000

    Add package freecad-stubs/1.0.9/freecad-stubs-1.0.9.tar.gz

---
+++ packages/freecad-stubs/freecad-stubs-1.0.9.tar.gz/freecad-stubs-1.0.9/freecad_stubs/SpreadsheetGui-stubs/__init__.pyi
@@ -0,0 +1,13 @@
+import FreeCADGui
+
+
+# ViewProviderSpreadsheetPy.xml
+class ViewProviderSpreadsheet(FreeCADGui.ViewProviderDocumentObject):
+    """ViewProviderSheet class"""
+
+    def getView(self):
+        """Get access to the sheet view"""
+
+
+# AppSpreadsheetGui.cpp
+def open(arg1: str, arg2: str = None, /) -> None: ...
