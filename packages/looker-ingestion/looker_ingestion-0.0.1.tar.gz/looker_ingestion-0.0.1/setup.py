9b8404d6879f81ff5dbbb5c1ae85390a562c67f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:00 +0000

    Add package looker-ingestion/0.0.1/looker_ingestion-0.0.1.tar.gz

---
+++ packages/looker-ingestion/looker_ingestion-0.0.1.tar.gz/looker_ingestion-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='looker_ingestion',
+    version='0.0.1',
+    description='Extracts adhoc queries from the Looker API to S3',
+    packages=find_packages(include=['looker_ingestion', 'looker_ingestion.*']),
+    install_requires=[
+        'boto3',
+        'looker_sdk'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+        classifiers= [
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ]
+)
