ce95623405666e02d8228eda3e8a22648b96e789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:05 +0000

    Add package metacity/0.1.3/metacity-0.1.3.tar.gz

---
+++ packages/metacity/metacity-0.1.3.tar.gz/metacity-0.1.3/metacity/datamodel/object.py
@@ -0,0 +1,39 @@
+from typing import Callable, Dict, List
+from metacity.geometry import MultiPoint, MultiLine, MultiPolygon, BaseModel
+
+types: Dict[str, Callable[[],BaseModel]] = {
+    MultiPoint().type: MultiPoint,
+    MultiLine().type: MultiLine,
+    MultiPolygon().type: MultiPolygon
+}
+
+def desermodel(model):
+    type = model["type"]
+    if model["type"] in types:
+        m = types[type]()
+    else:
+        raise RuntimeError(f"Unknown model type: {type}")
+    m.deserialize(model)
+    return m
+
+
+class Object:
+    def __init__(self):
+        self.meta = {}
+        self.models: List[BaseModel] = []
+
+    def serialize(self):
+        models = []
+        for model in self.models:
+            models.append(model.serialize())
+
+        return {
+            'meta': self.meta,
+            'models': models
+        }
+
+    def deserialize(self, models, meta):
+        self.meta = meta
+        for model in models:
+            self.models.append(desermodel(model))
+        
