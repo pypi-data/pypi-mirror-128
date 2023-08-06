496ae69c4dec19525e00afb26d41353f1dae3801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:57 +0000

    Add package pysnver/0.2.3/pysnver-0.2.3-py3-none-any.whl

---
+++ packages/pysnver/pysnver-0.2.3-py3-none-any.whl/pysnver/sn.py
@@ -0,0 +1,24 @@
+from ctypes import *
+import os
+import platform
+
+lib_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'so/libsv.so'
+)
+c_sn_verifier = cdll.LoadLibrary(lib_path)
+
+c_sn_verifier.c_verify_token.argtypes = []
+c_sn_verifier.c_verify_token.restype = c_bool
+
+
+class SerialNum(object):
+
+    @staticmethod
+    def verify():
+        val = c_sn_verifier.c_verify_token()
+        return val
+
+
+if __name__ == '__main__':
+    print(SerialNum.verify())
