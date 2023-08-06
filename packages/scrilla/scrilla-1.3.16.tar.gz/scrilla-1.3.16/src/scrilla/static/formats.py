1a675bc335c7ddb99e1c2f9343b80a4a4e2a8852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:03 +0000

    Add package scrilla/1.3.16/scrilla-1.3.16.tar.gz

---
+++ packages/scrilla/scrilla-1.3.16.tar.gz/scrilla-1.3.16/src/scrilla/static/formats.py
@@ -0,0 +1,9 @@
+formats = {
+    'separator': '-',
+    'TAB': '     ',
+    'LINE_LENGTH': 100,
+    'BAR_WIDTH': 0.10,
+    'INDENT': 10,
+    'RISK_FREE_TITLE': "{} US Treasury",
+    'BINS': 20
+}
