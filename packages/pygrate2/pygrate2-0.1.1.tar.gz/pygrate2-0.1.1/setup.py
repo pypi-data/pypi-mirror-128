2a789c7e4793bfdd1f2fc80a627d998ac196155a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:56 +0000

    Add package pygrate2/0.1.1/pygrate2-0.1.1.tar.gz

---
+++ packages/pygrate2/pygrate2-0.1.1.tar.gz/pygrate2-0.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+import pygrate2 
+
+setup(
+    name="pygrate2",
+    version=pygrate2.__version__,
+    packages=find_packages(),
+    author="Laurent Evrard",
+    description="Migration tools for python",
+    long_description=open("README.md").read(),
+    install_requires=[],
+    url="https://git.fenrys.io/fenrys/back/python_ddd",
+    scripts=["bin/pygrate2"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications",
+    ],
+    license="Apache-2.0 License",
+)
