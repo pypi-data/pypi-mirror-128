a4013a31446b249db0939db3d729db8c1954f149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:28 +0000

    Add package lndmanage/0.14.0/lndmanage-0.14.0-py3-none-any.whl

---
+++ packages/lndmanage/lndmanage-0.14.0-py3-none-any.whl/test/test_itest.py
@@ -0,0 +1,21 @@
+"""
+Integration tests for lndmanage.
+"""
+from test.testnetwork import TestNetwork
+from test.testing_common import test_graphs_paths
+
+
+class NewNode(TestNetwork):
+    """
+    NewNode tests behavior of lndmanage under a blank new node without any
+    channels.
+    """
+    network_definition = test_graphs_paths['empty_graph']
+
+    def graph_test(self):
+        self.assertEqual(0, len(self.master_node_graph_view))
+
+    def test_empty(self):
+        # LND interface of lndmanage is initialized in setUp method of super
+        # class, so nothing is needed here.
+        pass
