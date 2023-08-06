3c18b15fd6bbb8002b2b30f3fbc61d0cc0f83b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:35 +0000

    Add package falcon-error-handling/0.0.2/falcon-error-handling-0.0.2.tar.gz

---
+++ packages/falcon-error-handling/falcon-error-handling-0.0.2.tar.gz/falcon-error-handling-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='falcon-error-handling',
+    packages=['falcon_error_handling'],
+    version='0.0.2',
+    author="Develper Junio",
+    author_email='developer@junio.in',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="falcon error handling",
+    license="MIT license",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "marshmallow==3.13.0",
+        "pyparsing==2.4.7",
+        "typing-extensions==3.10.0.2",
+        "falcon==3.0.1",
+        "falcon-jsonify==1.2",
+        "ptyprocess==0.7.0"
+    ]
+)
