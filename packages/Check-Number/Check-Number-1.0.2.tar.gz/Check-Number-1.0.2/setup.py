63f2e3fa94b09ba89d9566b2a9065b75b5dd9d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:13 +0000

    Add package Check-Number/1.0.2/Check-Number-1.0.2.tar.gz

---
+++ packages/Check-Number/Check-Number-1.0.2.tar.gz/Check-Number-1.0.2/setup.py
@@ -0,0 +1,48 @@
+import os
+import setuptools
+
+
+def requirements(file="requirements.txt"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf-8") as r:
+            return [i.strip() for i in r]
+    else:
+        return []
+
+
+def readme(file="README.md"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf8") as r:
+            return r.read()
+    else:
+        return ""
+
+
+setuptools.setup(
+    name="Check-Number",
+    version="1.0.2",
+    description="Number Checker",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/FayasNoushad/Check-Number",
+    download_url="https://github.com/FayasNoushad/Check-Number/releases/latest",
+    license="MIT",
+    author="Fayas Noushad",
+    author_email="contact@fayas.me",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    project_urls={
+        "Tracker": "https://github.com/FayasNoushad/Check-Number/issues",
+        "Source": "https://github.com/FayasNoushad/Check-Number",
+        "Documentation": "https://check-number.projects.fayas.me",
+    },
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=requirements()
+)
