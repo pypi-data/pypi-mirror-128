fa75c474466d868f47b119c16cc1d885d2f411b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:49 +0000

    Add package djmyframework/0.1.36/djmyframework-0.1.36-py3-none-any.whl

---
+++ packages/djmyframework/djmyframework-0.1.36-py3-none-any.whl/djmyframework/framework_bak/conf.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021-04-14 18:00
+# @Author  : xzr
+# @File    : conf
+# @Software: PyCharm
+# @Contact : xzregg@gmail.com
+# @Desc    :
+
+from dynamic_settings.conf import *
