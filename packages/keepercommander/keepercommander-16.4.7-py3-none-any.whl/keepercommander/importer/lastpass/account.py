80a79f356ad19bc1c56420e22d484679b8a09bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:36 +0000

    Add package keepercommander/16.4.7/keepercommander-16.4.7-py3-none-any.whl

---
+++ packages/keepercommander/keepercommander-16.4.7-py3-none-any.whl/keepercommander/importer/lastpass/account.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+class Account(object):
+    def __init__(self, id, name, username, password, url, group, notes=None, shared_folder=None, attach_key=None):
+        self.id = id
+        self.name = name
+        self.username = username
+        self.password = password
+        self.url = url
+        self.group = group
+        self.notes = notes
+        self.shared_folder = shared_folder
+        self.attach_key = attach_key
+        self.attachments = []
