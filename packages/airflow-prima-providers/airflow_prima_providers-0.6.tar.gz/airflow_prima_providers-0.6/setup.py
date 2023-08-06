64ef0f7f797895f09f9c9328ad6a0388b40ebeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:43 +0000

    Add package airflow-prima-providers/0.6/airflow_prima_providers-0.6.tar.gz

---
+++ packages/airflow-prima-providers/airflow_prima_providers-0.6.tar.gz/airflow_prima_providers-0.6/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+  name = 'airflow_prima_providers',
+  packages = ['airflow_prima_providers'],
+  version = '0.6',
+  license='MIT',
+  description = 'Airflow utility providers developed by Prima Assicurazioni',
+  author = 'Prima Assicurazioni',
+  author_email = 'team-data@prima.it',
+  url = 'https://github.com/albanovito/providers-prima',
+  download_url = 'https://github.com/albanovito/providers-prima/archive/refs/tags/0.6.tar.gz',
+  keywords = ['airflow', 'prima', 'providers'],
+  install_requires=[
+          'apache-airflow',
+          'boto3',
+          'botocore',
+          'cached_property'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+)
