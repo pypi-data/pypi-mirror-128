9d934c9046729138380a393b31c2657a8351ef72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:56 +0000

    Add package pytorch-adapt/0.0.34/pytorch-adapt-0.0.34.tar.gz

---
+++ packages/pytorch-adapt/pytorch-adapt-0.0.34.tar.gz/pytorch-adapt-0.0.34/setup.py
@@ -0,0 +1,43 @@
+import sys
+
+import setuptools
+
+sys.path.insert(0, "src")
+import pytorch_adapt
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+extras_require_ignite = ["pytorch-ignite"]
+extras_require_record_keeper = ["record-keeper >= 0.9.31.dev6"]
+
+
+setuptools.setup(
+    name="pytorch-adapt",
+    version=pytorch_adapt.__version__,
+    author="Kevin Musgrave",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KevinMusgrave/pytorch-adapt",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+    install_requires=[
+        "numpy",
+        "torch",
+        "torchvision",
+        "torchmetrics",
+        "pytorch-metric-learning >= 1.0.0.dev5",
+    ],
+    extras_require={
+        "ignite": extras_require_ignite,
+        "record-keeper": extras_require_record_keeper,
+    },
+)
