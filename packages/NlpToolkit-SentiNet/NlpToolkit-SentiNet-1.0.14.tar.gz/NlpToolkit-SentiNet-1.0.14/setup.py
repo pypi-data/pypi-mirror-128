e1da86c0f0dbd47b4c8818823d1f233c470ab497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:02 +0000

    Add package NlpToolkit-SentiNet/1.0.14/NlpToolkit-SentiNet-1.0.14.tar.gz

---
+++ packages/NlpToolkit-SentiNet/NlpToolkit-SentiNet-1.0.14.tar.gz/NlpToolkit-SentiNet-1.0.14/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='NlpToolkit-SentiNet',
+    version='1.0.14',
+    packages=['SentiNet'],
+    url='https://github.com/StarlangSoftware/TurkishSentiNet-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Turkish SentiNet',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
