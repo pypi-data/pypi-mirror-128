c72ffdac8550297f30b04fdf7729e30edb2b3d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:45 +0000

    Add package endaq/1.2.0/endaq-1.2.0.tar.gz

---
+++ packages/endaq/endaq-1.2.0.tar.gz/endaq-1.2.0/README.md
@@ -0,0 +1,28 @@
+# `endaq-python`: A comprehensive, user-centric Python API for working with enDAQ data and devices
+
+
+## Installation
+
+endaq is available on PYPI via `pip`:
+
+    pip install endaq
+
+For the most recent features that are still under development, you can also use `pip` to install endaq directly from GitHub:
+
+    pip install git+https://github.com/MideTechnology/endaq-python.git@development
+
+## Contents
+
+This package consists of several submodules, you can read more about how these are used in their respective readme files:
+* `endaq.calc` ([readme](https://github.com/MideTechnology/endaq-python/tree/main/endaq/calc)): A computational backend for vibration analysis.
+* `endaq.cloud` ([readme](https://github.com/MideTechnology/endaq-python/tree/main/endaq/cloud)): Tools for interacting with enDAQ Cloud services.
+* `endaq.ide` ([readme](https://github.com/MideTechnology/endaq-python/tree/main/endaq/ide)): High-level utility functions to aid in importing and inspecting enDAQ IDE recording files.
+* `endaq.plot` ([readme](https://github.com/MideTechnology/endaq-python/tree/main/endaq/plot)):  A package comprising a collection of plotting utilities for sensor data analysis.
+
+## Docs
+
+The docs for this package can be found [here](https://docs.endaq.com/en/latest/).
+
+## License 
+
+The endaq-python repository is licensed under the MIT license. The full text can be found in the [LICENSE file](https://github.com/MideTechnology/endaq-python/blob/main/LICENSE).
---
+++ packages/endaq/endaq-1.2.0.tar.gz/endaq-1.2.0/setup.py
@@ -0,0 +1,68 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES = [
+    "backports.cached-property; python_version<'3.8'",
+    "ebmlite>=3.2.0",
+    "idelib>=3.2.3",
+    "jinja2",
+    "numpy>=1.19.5",
+    "pandas>=1.3",
+    "plotly>=5.3.1",
+    "python-dotenv>=0.18.0",
+    "requests>=2.25.1",
+    "scipy>=1.7.1",
+    ]
+
+TEST_REQUIRES = [
+    "hypothesis",
+    "pytest",
+    "pytest-cov",
+    "pytest-xdist[psutil]",
+    "sympy",
+    ]
+
+DOCS_REQUIRES = [
+    "sphinx",
+    "pydata-sphinx-theme",
+    ]
+
+EXAMPLE_REQUIRES = [
+    ]
+
+setuptools.setup(
+        name='endaq',
+        version='1.2.0',
+        author='Mide Technology',
+        author_email='help@mide.com',
+        description='A comprehensive, user-centric Python API for working with enDAQ data and devices',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/MideTechnology/endaq-python',
+        license='MIT',
+        classifiers=['Development Status :: 4 - Beta',
+                     'License :: OSI Approved :: MIT License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
+                     'Programming Language :: Python :: 3.9',
+                     'Topic :: Scientific/Engineering',
+                     ],
+        keywords='ebml binary ide mide endaq',
+        packages=setuptools.find_packages(exclude=('tests',)),
+        package_dir={'endaq': './endaq'},
+        # package_data={
+        #     'idelib': ['schemata/*'],
+        # },
+        # test_suite='./testing',
+        install_requires=INSTALL_REQUIRES,
+        extras_require={
+            'test': INSTALL_REQUIRES + TEST_REQUIRES,
+            'docs': INSTALL_REQUIRES + DOCS_REQUIRES,
+            'example': INSTALL_REQUIRES + EXAMPLE_REQUIRES,
+            },
+)
