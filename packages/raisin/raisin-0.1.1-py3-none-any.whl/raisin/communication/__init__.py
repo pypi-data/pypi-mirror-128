ede6ac445506af0456ecd6b70f763b7d0a13e71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:02 +0000

    Add package raisin/0.1.1/raisin-0.1.1-py3-none-any.whl

---
+++ packages/raisin/raisin-0.1.1-py3-none-any.whl/raisin/communication/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+|==========================================|
+| Permet de gerer les connections reseaux. |
+|==========================================|
+
+On peut y trouver les elements principaux suivants:
+* Le code des serveurs.
+* Le code des clients.
+* Les verifications assurant une securite maximale.
+* L'implementation du protocol 'bas niveau' de communication.
+"""
+
+__all__ = ["checks", "dns", "dropbox", "mail",
+    "request", "routing", "tcp_client", "tcp_server"]
