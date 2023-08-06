c1bc089558a3c1ed2a54ad3652e584b5bfcc6e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:47 +0000

    Add package GenerateCores/1.0.4/GenerateCores-1.0.4.tar.gz

---
+++ packages/GenerateCores/GenerateCores-1.0.4.tar.gz/GenerateCores-1.0.4/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='GenerateCores',
+    version='1.0.4',
+    description='The data generated',
+    packages=['GenerateCores'],
+    author='Zhou Guangrong',
+    author_email='1971987925@qq.com',
+    long_description='The simulation data generateion, including simulation data and synthetic data'
+)
