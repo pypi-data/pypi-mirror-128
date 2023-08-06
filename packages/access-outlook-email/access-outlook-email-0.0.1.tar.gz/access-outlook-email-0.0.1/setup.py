59ea5e737002a42ded504645d3cfa0442d68226e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:50 +0000

    Add package access-outlook-email/0.0.1/access-outlook-email-0.0.1.tar.gz

---
+++ packages/access-outlook-email/access-outlook-email-0.0.1.tar.gz/access-outlook-email-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Send Emails via Outlook'
+LONG_DESCRIPTION = 'A package that allows to send emails from an outlook account'
+
+# Setting up
+setup(
+    name="access-outlook-email",
+    version=VERSION,
+    author="Valentin Baier",
+    author_email="valentin_baier@gmx.de",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['exchangelib'],
+    keywords=['python', 'email', 'outlook', 'send', 'send email'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
