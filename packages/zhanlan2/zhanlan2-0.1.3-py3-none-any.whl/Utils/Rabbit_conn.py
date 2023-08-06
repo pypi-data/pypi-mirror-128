b242137813779925d3b0fab3827cd163ba008d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:33 +0000

    Add package zhanlan2/0.1.3/zhanlan2-0.1.3-py3-none-any.whl

---
+++ packages/zhanlan2/zhanlan2-0.1.3-py3-none-any.whl/Utils/Rabbit_conn.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/2 14:46
+# @Author  : Adyan
+# @File    : Rabbit_conn.py
+
+
+import json
+import pika
+
+from Utils import rabitmq_config
+
+
+class RabbitClient:
+    def __init__(self, queue_name):
+        self.queue_name = queue_name
+
+    def rabbit_conn(self):
+        """
+        创建连接
+        :return:
+        """
+        user_pwd = pika.PlainCredentials(
+            rabitmq_config.get("mq_username"),
+            rabitmq_config.get("mq_pwd")
+        )
+        params = pika.ConnectionParameters(
+            host=rabitmq_config.get("mq_ip"),
+            port=rabitmq_config.get('mq_port'),
+            virtual_host=rabitmq_config.get("mq_virtual_host"),
+            credentials=user_pwd
+        )
+        self.conn = pika.BlockingConnection(parameters=params)
+        self.col = self.conn.channel()
+        self.col.queue_declare(
+            queue=self.queue_name,
+            durable=True
+        )
+
+    def push_rabbit(self, item):
+        self.rabbit_conn()
+        self.col.basic_publish(
+            exchange='',
+            routing_key=self.queue_name,
+            body=json.dumps(item, ensure_ascii=False)
+        )
+
+    def get_rabbit(self, fun):
+        self.rabbit_conn()
+        self.col.queue_declare(self.queue_name, durable=True, passive=True)
+        self.col.basic_consume(self.queue_name, fun)
+        self.col.start_consuming()
+
+
+if __name__ == '__main__':
+    # # 一键搬家
+    # RabbitClient('TEST_SMT_COPY_PRODUCT').push_rabbit(
+    #     {"request_type": "SMT_COPY_PRODUCT", "request_id": "2cac500ba49c4fb97d9a80eb3f9cb216",
+    #      "secret_key": "_QXSYYXGJQUQS", "biz_id": "https:\\/\\/detail.1688.com\\/offer\\/614651326996.html",
+    #      "send_at": 1629164414,
+    #      "data": {"productUrl": "https:\\/\\/detail.1688.com\\/offer\\/614651326996.html", "type": 1}}
+    # )
+    # 查排名
+    RabbitClient('TEST_SMT_PRODUCT_RANKING').push_rabbit(
+        {"send_at": 1619520635,
+         "data": {"keyword": "衣服", "shopName": "东莞市汇百商网络科技有限公司", "shopUrl": "shop085o885b77228.1688.com",
+                  "productUrl": "", "type": "3", "startPage": "1", "endPage": "3", "requestId": 8703}}
+
+    )
---
+++ packages/zhanlan2/zhanlan2-0.1.3-py3-none-any.whl/Utils/Redis_conn.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/2 17:43
+# @Author  : Adyan
+# @File    : Redis_conn.py
+
+
+import redis
+
+from datetime import datetime
+
+from Utils import redis_config
+
+HOST = redis_config.get('HOST', 'localhost')
+PORT = redis_config.get('PORT', 6379)
+PASSWORD = redis_config.get('PAW', None)
+DB = redis_config.get('DB', 0)
+
+
+class ReidsClient(object):
+
+    def __init__(self, host=HOST, port=PORT, password=PASSWORD, db=None, NAME=None):
+        if db == None:
+            db = DB
+        if password:
+            self.__conn = redis.Redis(host=host, port=port, password=password)
+        else:
+            self.__conn = redis.Redis(host=host, port=port, db=db)
+        self.NAME = NAME
+
+    def redis_conn(self):
+        return self.__conn
+
+    def get(self, count):
+        lst = self.__conn.lrange(self.NAME, 0, count - 1)
+        self.__conn.ltrim(self.NAME, count, -1)
+        return lst
+
+    def put(self, param):
+        self.__conn.rpush(self.NAME, param)
+
+    @property
+    def queue_len(self):
+        return self.__conn.llen(self.NAME)
+
+    def prox(self):
+        ip_list = []
+        data_list = self.__conn.hgetall("proxy")
+        for ip_item in data_list:
+            proxy_expire_time = int(data_list[ip_item].decode())
+            now_time = int(datetime.now().timestamp())
+            time_dif = proxy_expire_time - now_time
+            if time_dif < 10:
+                print(ip_item.decode(), "过期 删除这个代理")
+                self.__conn.hdel('proxy', ip_item)
+            else:
+                ip_list.append(ip_item.decode())
+        return ip_list
