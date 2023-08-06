bdcb92458a23d1e53c0958f7d303757f05729160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:13 +0000

    Add package juspy/0.0.2b12/juspy-0.0.2b12.tar.gz

---
+++ packages/juspy/juspy-0.0.2b12.tar.gz/juspy-0.0.2b12/src/juspy/greet.py
@@ -0,0 +1,24 @@
+def hello():
+    return "Hello World!!"
+    
+def namastey():
+    print("नमस्ते मित्र")
+    # return "Namastey Mitra!!"
+
+def ni_hao():
+    print("朋友你好 ")
+
+def hola():
+    print("Hola, cariño ")
+
+def bonjour():
+    print("Bonjour chéri")
+    
+def schatz():
+    print("Hallo, Schatz")
+    
+def ahlan():
+    print("مرحبا عزيزتي")
+    
+def privet():
+    print("Privet, dorogoy")
---
+++ packages/juspy/juspy-0.0.2b12.tar.gz/juspy-0.0.2b12/src/juspy/linear_models/LinearRegression.py
@@ -0,0 +1,4 @@
+def fit_predict(X_train, X_test, y_train, y_test):
+    predictions="predictions"
+    rmse="rmse"
+    return predictions, rmse
