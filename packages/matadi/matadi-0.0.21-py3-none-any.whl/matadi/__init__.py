73cf4d9fe0603df1c062580f29da783e0e983c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:08 +0000

    Add package matadi/0.0.21/matadi-0.0.21-py3-none-any.whl

---
+++ packages/matadi/matadi-0.0.21-py3-none-any.whl/matadi/__init__.py
@@ -0,0 +1,23 @@
+from .__about__ import __version__
+
+import casadi
+
+Variable = casadi.SX.sym
+
+from ._material import (
+    Material,
+    Material as MaterialScalar,
+    MaterialTensor,
+)
+from . import models
+from . import math
+from ._templates import (
+    ThreeFieldVariation,
+    MaterialHyperelastic,
+    MaterialComposite,
+)
+from ._lab import Lab
+
+__all__ = [
+    "__version__",
+]
