fb687aa46bd34e76cfab5d84c737248269962783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:53 +0000

    Add package nowcasting-dataloader/1.5.0/nowcasting_dataloader-1.5.0-py3-none-any.whl

---
+++ packages/nowcasting-dataloader/nowcasting_dataloader-1.5.0-py3-none-any.whl/nowcasting_dataloader/data_sources/__init__.py
@@ -0,0 +1,7 @@
+""" Various DataSources """
+from .gsp.gsp_model import GSPML
+from .nwp.nwp_model import NWPML
+from .pv.pv_model import PVML
+from .satellite.satellite_model import SatelliteML
+from .sun.sun_model import SunML
+from .topographic.topographic_model import TopographicML
