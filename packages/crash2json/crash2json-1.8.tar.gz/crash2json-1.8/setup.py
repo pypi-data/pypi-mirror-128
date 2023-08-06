90155538733c7ccdb3e97bad0e0b5b83e1ff9353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:02 +0000

    Add package crash2json/1.8/crash2json-1.8.tar.gz

---
+++ packages/crash2json/crash2json-1.8.tar.gz/crash2json-1.8/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/21 10:26 下午
+# @Author  : yanbo92
+# @File    : setup.py
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='crash2json',
+      version='1.8',
+      description='Parse appple crash report to json file',
+      author='yanbo92',
+      author_email='yanbo92@139.com',
+      url='https://github.com/yanbo92/crash2json',
+      package_dir={'': 'crash2json'},
+      packages = find_packages('crash2json'),
+      entry_points={
+          'console_scripts': [
+              'crash2json = crash2json.__main__:main',
+
+          ]
+      }
+      )
