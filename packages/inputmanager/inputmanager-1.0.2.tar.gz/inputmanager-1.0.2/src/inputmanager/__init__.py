65aea76f217fac2fd5951cb0de4f7ecb0cc8ebd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:26 +0000

    Add package inputmanager/1.0.2/inputmanager-1.0.2.tar.gz

---
+++ packages/inputmanager/inputmanager-1.0.2.tar.gz/inputmanager-1.0.2/src/inputmanager/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Created on Fri Jul 17 08:50:55 2020.
+
+@author: Tristan Muscat
+"""
+__version__ = "1.0.2"
+
+from .inputmanager import force_read
+from .inputmanager import InputException
+from .inputmanager import read_line
+from .inputmanager import read_numeric
+from .inputmanager import read_yes_no
