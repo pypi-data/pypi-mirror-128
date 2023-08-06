490f74dde80f4f1a1044e262a706035a878bfacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:58 +0000

    Add package ert/2.30.0b0/ert-2.30.0b0-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-2.30.0b0-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert3/workspace/__init__.py
@@ -0,0 +1,11 @@
+"""
+API reference documentation for the ert3 `workspace` module.
+"""
+
+from ert3.workspace._workspace import Workspace
+from ert3.workspace._workspace import initialize
+
+__all__ = [
+    "Workspace",
+    "initialize",
+]
