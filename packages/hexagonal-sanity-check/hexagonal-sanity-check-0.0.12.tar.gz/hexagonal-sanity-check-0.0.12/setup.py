d2183b1c2b6bd758541c4f34c7345dfd4ae2b835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:29 +0000

    Add package hexagonal-sanity-check/0.0.12/hexagonal-sanity-check-0.0.12.tar.gz

---
+++ packages/hexagonal-sanity-check/hexagonal-sanity-check-0.0.12.tar.gz/hexagonal-sanity-check-0.0.12/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['src', 'src.domain', 'src.services', 'src.use_cases']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0', 'diagrams>=0.20.0,<0.21.0']
+
+setup_kwargs = {
+    'name': 'hexagonal-sanity-check',
+    'version': '0.0.12',
+    'description': 'Hexagonal Sanity Check',
+    'long_description': None,
+    'author': 'rfrezino',
+    'author_email': 'rodrigofrezino@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
