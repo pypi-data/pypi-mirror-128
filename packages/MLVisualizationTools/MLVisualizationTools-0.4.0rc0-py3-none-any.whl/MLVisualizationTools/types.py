8392f1b93e201123a9fad0b21fef206d91428a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:02 +0000

    Add package MLVisualizationTools/0.4.0rc0/MLVisualizationTools-0.4.0rc0-py3-none-any.whl

---
+++ packages/MLVisualizationTools/MLVisualizationTools-0.4.0rc0-py3-none-any.whl/MLVisualizationTools/types.py
@@ -0,0 +1,13 @@
+class GraphDataTypes:
+    Grid = 'Grid'
+    Animation = 'Animation'
+
+class GraphOutputTypes:
+    Auto = 'Auto'
+    Matplotlib = 'Matplotlib'
+    Plotly = 'Plotly'
+
+class ColorizerModes:
+    NotColorized = 'NotColorized'
+    Simple = 'Simple'
+    Binary = 'Binary'
