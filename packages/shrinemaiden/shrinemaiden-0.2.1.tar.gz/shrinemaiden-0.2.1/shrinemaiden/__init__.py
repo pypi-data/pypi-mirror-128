7f113b0fc0fdebc4d5045c27149d68366db2a955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:52 +0000

    Add package shrinemaiden/0.2.1/shrinemaiden-0.2.1.tar.gz

---
+++ packages/shrinemaiden/shrinemaiden-0.2.1.tar.gz/shrinemaiden-0.2.1/shrinemaiden/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+import shrinemaiden.transcript
+import shrinemaiden.transcript_class
+import shrinemaiden.train
+import shrinemaiden.utils
+
+# Version of the package
+__version__ = "0.2.1"
