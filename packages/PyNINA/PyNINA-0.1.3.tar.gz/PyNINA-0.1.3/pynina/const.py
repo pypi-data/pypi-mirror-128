1501f4d5f248502131d3a19c3a1f2b20fc86643e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:35 +0000

    Add package PyNINA/0.1.3/PyNINA-0.1.3.tar.gz

---
+++ packages/PyNINA/PyNINA-0.1.3.tar.gz/PyNINA-0.1.3/pynina/const.py
@@ -0,0 +1,7 @@
+ENDPOINT_REGIONAL_CODE: str = "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:rs_2021-07-31/download/Regionalschl_ssel_2021-07-31.json"
+ENDPOINT_NINA_BASE: str = "https://warnung.bund.de/api31/dashboard/"
+
+
+class ReadOnlyClass(type):
+    def __setattr__(self, name, value):
+        raise ValueError(name)
