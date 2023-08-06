7e8ac655ae66129c1086a34565292d399f16679c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:12 +0000

    Add package hindiwsd/1.0.6/hindiwsd-1.0.6.tar.gz

---
+++ packages/hindiwsd/hindiwsd-1.0.6.tar.gz/hindiwsd-1.0.6/src/hindiwsd/hindi_wsd.py
@@ -0,0 +1,11 @@
+from hindiwsd import wsd, lesks
+
+
+def wordsense(sentence):
+
+    hinglish, hindi = wsd.preprocess_transliterate(sentence)
+    pos = wsd.POS_tagger(hindi)
+
+    for i in pos:
+        lesks.lesk(i[0], i[1], hindi)
+
