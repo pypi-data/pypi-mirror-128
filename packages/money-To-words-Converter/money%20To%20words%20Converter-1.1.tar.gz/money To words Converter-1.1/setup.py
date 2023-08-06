e9c929037e6aed424d8f8fcf1d893736990094df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package money-To-words-Converter/1.1/money%20To%20words%20Converter-1.1.tar.gz

---
+++ packages/money-To-words-Converter/money%20To%20words%20Converter-1.1.tar.gz/money To words Converter-1.1/setup.py
@@ -0,0 +1,79 @@
+from setuptools import setup
+
+setup(name='money To words Converter',
+      version='1.1',
+      description='convert int numbers (money , price ...) to words',
+      url='https://github.com/zzainoo/money-to-words',
+      author='zain alabdeen albaghdadi',
+      author_email='ta99kr@gmail.com',
+      license='MIT',
+      packages=['money_to_words'],
+      zip_safe=False,
+      long_description="""
+      
+      
+      # Money to Words Converter
+## _حول الارقام الى كتابة باللغة العربية_
+
+
+Money to Words Converter :->
+مكتبة بسيطة تساعد في تحويل الارقام الى كتابة عربية ويمكن استخدامه في البرامج المحاسبية والتعاملات النقدية
+
+- بسيط جدا
+- لا يحتاج الى اي مكتبة ثانوية
+ - يمكن تحويل حتى رقم تريليون
+- ✨سريع جدا ✨
+
+
+
+
+
+## Installation
+
+يعمل على [python 3.x](https://www.python.org/)
+
+لتثبيت المكتبة
+
+```sh
+pip install money-To-words-Converter
+```
+
+
+## قائمة الاوامر
+
+
+
+| الامر | الفائدة |
+| ------ | ------ |
+| Convert(int) | [تحويل الارقام الى كتابة] |
+| split_list(list,nu) | [تقسيم مصفوفة الى عدد معين من المصفوفات] |
+| get_first(int) | [يحول رقم من مرتبة الاحاد الى كتابة] |
+| get_second(int) | [يحول رقم من مرتبة الاحاد والعشرات الى كتابة] |
+| get_third(int) | [يحول رقم من مرتبة الاحاد والعشرات والمئات الى كتابة] |
+
+
+
+
+## مثال
+
+
+```sh
+import money_to_words as Converter
+
+price = Converter.Convert(7999113555222)
+
+print(price)
+```
+
+
+## الترخيص
+
+MIT
+email : ta99kr@gmail.com
+
+
+      
+      
+      
+      """,
+      long_description_content_type='text/markdown')
