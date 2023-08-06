37bde83f7fb83aabad7b15a30d543052406a56c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:01 +0000

    Add package django-balloon-block-editor/1.0.1/django_balloon_block_editor-1.0.1.tar.gz

---
+++ packages/django-balloon-block-editor/django_balloon_block_editor-1.0.1.tar.gz/django_balloon_block_editor-1.0.1/core/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from balloon_block_editor.fields import BalloonBlockEditorField
+
+
+class Test(models.Model):
+	content = BalloonBlockEditorField()
+	content2 = BalloonBlockEditorField()
