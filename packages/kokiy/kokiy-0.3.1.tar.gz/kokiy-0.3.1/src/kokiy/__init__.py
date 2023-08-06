eabad471fcb90a4e63bc186daab6cfde9f679b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:44 +0000

    Add package kokiy/0.3.1/kokiy-0.3.1.tar.gz

---
+++ packages/kokiy/kokiy-0.3.1.tar.gz/kokiy-0.3.1/src/kokiy/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.3.1'
+
+from kokiy.axishell import AxiShell
+from kokiy.cartshell import CartShell
+from kokiy.thickshell import ThickShell
+from kokiy.frustum_shell import FrustumShell
