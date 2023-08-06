d5af856aaecf6533060a1a765b629ab7ed34df06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:51 +0000

    Add package polars/0.10.24/polars-0.10.24.tar.gz

---
+++ packages/polars/polars-0.10.24.tar.gz/polars-0.10.24/tests/test_exprs.py
@@ -0,0 +1,16 @@
+import polars as pl
+
+
+def test_horizontal_agg(fruits_cars: pl.DataFrame) -> None:
+    df = fruits_cars
+    out = df.select(pl.max([pl.col("A"), pl.col("B")]))
+    assert out[:, 0].to_list() == [5, 4, 3, 4, 5]
+
+    out = df.select(pl.min([pl.col("A"), pl.col("B")]))
+    assert out[:, 0].to_list() == [1, 2, 3, 2, 1]
+
+
+def test_prefix(fruits_cars: pl.DataFrame) -> None:
+    df = fruits_cars
+    out = df.select([pl.all().suffix("_reverse")])
+    assert out.columns == ["A_reverse", "fruits_reverse", "B_reverse", "cars_reverse"]
