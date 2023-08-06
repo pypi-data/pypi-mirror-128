8daf892f2fc3ee91fc570cbef8413225178e6d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:36 +0000

    Add package pytest-idempotent/0.0.4/pytest_idempotent-0.0.4.tar.gz

---
+++ packages/pytest-idempotent/pytest_idempotent-0.0.4.tar.gz/pytest_idempotent-0.0.4/tests/test_files/test_correct.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import pytest
+
+from pytest_idempotent import idempotent
+
+
+@idempotent
+def func(x: list[int]) -> None:
+    """Correctly Idempotent."""
+    if not x:
+        x += [9]
+
+
+@pytest.mark.test_idempotency
+def test_func() -> None:
+    x: list[int] = []
+
+    func(x)
+
+    assert x == [9]
+
+
+def test_func_without_idempotency_check() -> None:
+    x: list[int] = []
+
+    func(x)
+
+    assert x == [9]
