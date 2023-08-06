5dddf11d6384825c230dc7d5ed5b63207aa91394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:38 +0000

    Add package borgor/0.1.8/borgor-0.1.8-py3-none-any.whl

---
+++ packages/borgor/borgor-0.1.8-py3-none-any.whl/borgor/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'borgor'
+__author__ = 'coverosu'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 coverosu'
+
+import .simplebeatmap
+import .akatsukiapi
+import .osuapiv2
+import .beatmap
+import .replay
+import .osuapi
+import .utils
