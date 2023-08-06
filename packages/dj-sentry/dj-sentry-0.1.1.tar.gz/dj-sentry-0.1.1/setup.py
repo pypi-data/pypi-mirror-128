3778592b2fb5bc5fae5180c596e17abedbf12108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:04 +0000

    Add package dj-sentry/0.1.1/dj-sentry-0.1.1.tar.gz

---
+++ packages/dj-sentry/dj-sentry-0.1.1.tar.gz/dj-sentry-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dj_sentry']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=3.0,<4.0', 'sentry-sdk>=1.5.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dj-sentry',
+    'version': '0.1.1',
+    'description': 'A Django app to initialize Sentry client for your Django applications',
+    'long_description': None,
+    'author': 'Michael Vieira',
+    'author_email': 'dev@mvieira.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
