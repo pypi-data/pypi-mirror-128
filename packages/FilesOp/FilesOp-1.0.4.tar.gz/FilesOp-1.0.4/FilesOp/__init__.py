ffa8d46fd1606cd979f417a47ac739e48a7361b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:19 +0000

    Add package FilesOp/1.0.4/FilesOp-1.0.4.tar.gz

---
+++ packages/FilesOp/FilesOp-1.0.4.tar.gz/FilesOp-1.0.4/FilesOp/__init__.py
@@ -0,0 +1,4 @@
+__author__ = """KingKaitoKid"""
+__email__ = 'djstrix@me.com'
+__version__ = '1.0.3'
+name = "FilesOp"
---
+++ packages/FilesOp/FilesOp-1.0.4.tar.gz/FilesOp-1.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FilesOp", # Replace with your own username
+    version="1.0.4",
+    author="King Kaito Kid",
+    author_email="djstrix@me.com",
+    description="Gestionale dei file per usi interni",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KingKaitoKid/FileOperations",
+    packages=setuptools.find_packages(),
+    #install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9.6',
+)
