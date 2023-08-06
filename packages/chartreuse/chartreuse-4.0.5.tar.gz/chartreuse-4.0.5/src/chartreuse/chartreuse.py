32597e7384bd7cf469578749344cf864671298a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:32 +0000

    Add package chartreuse/4.0.5/chartreuse-4.0.5.tar.gz

---
+++ packages/chartreuse/chartreuse-4.0.5.tar.gz/chartreuse-4.0.5/src/chartreuse/chartreuse.py
@@ -0,0 +1,48 @@
+import logging
+
+import wiremind_kubernetes.kubernetes_helper
+
+from .utils import AlembicMigrationHelper
+
+
+def configure_logging() -> None:
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.INFO,
+    )
+
+
+class Chartreuse:
+    def __init__(
+        self,
+        postgresql_url: str,
+        release_name: str,
+        alembic_allow_migration_for_empty_database: bool,
+        alembic_additional_parameters: str = "",
+        kubernetes_helper: wiremind_kubernetes.kubernetes_helper.KubernetesDeploymentManager = None,
+    ):
+
+        configure_logging()
+
+        self.alembic_migration_helper = AlembicMigrationHelper(
+            postgresql_url,
+            allow_migration_for_empty_database=alembic_allow_migration_for_empty_database,
+            additional_parameters=alembic_additional_parameters,
+        )
+
+        if kubernetes_helper:
+            self.kubernetes_helper = kubernetes_helper
+        else:
+            self.kubernetes_helper = wiremind_kubernetes.kubernetes_helper.KubernetesDeploymentManager(
+                use_kubeconfig=None, release_name=release_name
+            )
+
+        self.is_migration_needed = self.check_migration_needed()
+
+    def check_migration_needed(self):
+        return self.alembic_migration_helper.is_migration_needed
+
+    def upgrade(self):
+        if self.check_migration_needed():
+            self.alembic_migration_helper.upgrade_db()
