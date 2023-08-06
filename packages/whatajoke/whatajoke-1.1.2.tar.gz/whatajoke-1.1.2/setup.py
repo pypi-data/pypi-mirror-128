1c559e15053b8d33dd308602bfa0d8af7f4f5836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:47 +0000

    Add package whatajoke/1.1.2/whatajoke-1.1.2.tar.gz

---
+++ packages/whatajoke/whatajoke-1.1.2.tar.gz/whatajoke-1.1.2/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup
+
+
+# TODO: this needs to change!
+# with open('requirements.txt') as f:
+#   required = f.read().splitlines()
+
+def requirements():
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(f'{root_dir}/requirements.txt') as f:
+        content = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    li = [x.strip() for x in content]
+    return li
+
+
+setup(
+    name='whatajoke',
+    version='1.1.2',
+    packages=['whatajoke'],
+    url='https://github.com/joaoduartepinto/whatajoke',
+    download_url='https://github.com/joaoduartepinto/whatajoke/archive/refs/tags/v1.0.tar.gz',
+    license='GPLv3',
+    author='Joao Duarte Pinto',
+    author_email='joaoduartepinto@outlook.com',
+    description='Send a joke to a Whatsapp group!',
+    entry_points={
+        'console_scripts': ['wj=whatajoke.whatajoke:main']
+    },
+    install_requires=requirements(),
+)
