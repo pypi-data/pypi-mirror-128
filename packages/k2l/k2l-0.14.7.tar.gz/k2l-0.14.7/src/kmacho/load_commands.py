ba569418c0e4fa33782a6261b7441172000934aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:51 +0000

    Add package k2l/0.14.7/k2l-0.14.7.tar.gz

---
+++ packages/k2l/k2l-0.14.7.tar.gz/k2l-0.14.7/src/kmacho/load_commands.py
@@ -0,0 +1,16 @@
+#
+#  ktool | kmacho
+#  load_commands.py
+#
+#
+#
+#  This file is part of ktool. ktool is free software that
+#  is made available under the MIT license. Consult the
+#  file "LICENSE" that is distributed together with this file
+#  for the exact licensing terms.
+#
+#  Copyright (c) kat 2021.
+#
+
+
+
