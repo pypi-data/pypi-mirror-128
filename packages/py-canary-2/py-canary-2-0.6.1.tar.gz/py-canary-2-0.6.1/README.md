4a79f6cecee01f28e95e1ea6370aaf08a7aeb264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:12 +0000

    Add package py-canary-2/0.6.1/py-canary-2-0.6.1.tar.gz

---
+++ packages/py-canary-2/py-canary-2-0.6.1.tar.gz/py-canary-2-0.6.1/README.md
@@ -0,0 +1,5 @@
+# py-canary-2
+
+Forked from https://github.com/snjoetw/py-canary
+
+Python API for Canary Security Camera.  This is used in [Home Assistant](https://home-assistant.io) but should be generic enough that can be used elsewhere.
---
+++ packages/py-canary-2/py-canary-2-0.6.1.tar.gz/py-canary-2-0.6.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='py-canary-2',
+    version='0.6.1',
+    author="Brian Marks",
+    description="Python API for the Canary App",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bm1549/canary",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+     ],
+ )
