5c33dce10c2fc31ebb5eb43344e1cf39ed70418f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:57 +0000

    Add package ucbox-plugin/0.0.13/ucbox_plugin-0.0.13.tar.gz

---
+++ packages/ucbox-plugin/ucbox_plugin-0.0.13.tar.gz/ucbox_plugin-0.0.13/ucbox_plugin/navigation.py
@@ -0,0 +1,24 @@
+from extras.plugins import PluginMenuItem
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:number_list_view',
+        link_text='Numbers',
+        buttons=()
+    ),
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:trunk_list_view',
+        link_text='Trunks',
+        buttons=()
+    ),
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:UCCluster_list_view',
+        link_text='UCClusters',
+        buttons=()
+    ),
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:devicepool_list_view',
+        link_text='Device Pools',
+        buttons=()
+    ),
+)
