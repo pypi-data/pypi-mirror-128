5140909339dd902bb702abd626eb068ebc997354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:36 +0000

    Add package pymarko/0.2.0/pymarko-0.2.0-py3-none-any.whl

---
+++ packages/pymarko/pymarko-0.2.0-py3-none-any.whl/pymarko/__init__.py
@@ -0,0 +1,13 @@
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2018 Kevin Walchko
+# see LICENSE for full details
+##############################################
+
+from importlib.metadata import version # type: ignore
+
+from .ip import get_ip
+
+__author__ = "Kevin Walchko"
+__license__ = "MIT"
+__version__ = version("pymarko")
