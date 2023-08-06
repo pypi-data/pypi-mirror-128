0cdc4c8d6e3288d8acabf88867d4d37551a1cdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:40 +0000

    Add package ghga-service-chassis-lib/0.7.0/ghga_service_chassis_lib-0.7.0.tar.gz

---
+++ packages/ghga-service-chassis-lib/ghga_service_chassis_lib-0.7.0.tar.gz/ghga_service_chassis_lib-0.7.0/tests/integration/fixtures/amqp.py
@@ -0,0 +1,33 @@
+# Copyright 2021 Universität Tübingen, DKFZ and EMBL
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""RabbitMQ fixtures"""
+
+import os
+
+import pika
+
+RABBITMQ_TEST_HOST = (
+    os.getenv("RABBITMQ_TEST_HOST") if os.getenv("RABBITMQ_TEST_HOST") else "localhost"
+)
+CONNECTION_PARAMS = pika.ConnectionParameters(host=RABBITMQ_TEST_HOST)
+
+
+class MessageSuccessfullyReceived(Exception):
+    """This Exception can be used to signal that the message
+    was successfully received.
+    """
+
+    ...
