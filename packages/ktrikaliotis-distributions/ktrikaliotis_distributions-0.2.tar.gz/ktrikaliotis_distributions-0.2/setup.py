348cee26e94360b343fad1cabf785d1058612443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:15 +0000

    Add package ktrikaliotis-distributions/0.2/ktrikaliotis_distributions-0.2.tar.gz

---
+++ packages/ktrikaliotis-distributions/ktrikaliotis_distributions-0.2.tar.gz/ktrikaliotis_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ktrikaliotis_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['ktrikaliotis_distributions'],
+      zip_safe=False)
