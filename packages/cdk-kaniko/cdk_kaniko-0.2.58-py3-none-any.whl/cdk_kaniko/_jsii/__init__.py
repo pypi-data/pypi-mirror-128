7768042a8e673346e9d1d5108edde9d98d95fa60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:52 +0000

    Add package cdk-kaniko/0.2.58/cdk_kaniko-0.2.58-py3-none-any.whl

---
+++ packages/cdk-kaniko/cdk_kaniko-0.2.58-py3-none-any.whl/cdk_kaniko/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_ecr._jsii
+import aws_cdk.aws_ecs._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.core._jsii
+import cdk_fargate_run_task._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-kaniko", "0.2.58", __name__[0:-6], "cdk-kaniko@0.2.58.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
