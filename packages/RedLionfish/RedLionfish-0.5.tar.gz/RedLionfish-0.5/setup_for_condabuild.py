3e5a9df1dc7664aebf53b84a50c3a255bb44b5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:29 +0000

    Add package RedLionfish/0.5/RedLionfish-0.5.tar.gz

---
+++ packages/RedLionfish/RedLionfish-0.5.tar.gz/RedLionfish-0.5/setup_for_condabuild.py
@@ -0,0 +1,42 @@
+'''
+Copyright (C) 2021 Rosalind Franklin Institute
+
+'''
+
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    version = '0.5',
+    name = 'RedLionfish',
+    description = 'Fast Richardson-Lucy deconvolution of 3D volume data using GPU or CPU with napari plugin.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url = 'https://github.com/rosalindfranklininstitute/RedLionfish',
+    author = 'Luis Perdigao',
+    author_email='luis.perdigao@rfi.ac.uk',
+    packages=['RedLionfishDeconv'],
+    classifiers=[
+        'Development Status :: 4 - Beta ',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'Framework :: napari'
+    ],
+    license='Apache License, Version 2.0',
+    zip_safe=False,
+    install_requires=[
+        
+    ] ,
+
+    #for the napari plugin
+    entry_points={'napari.plugin': 'RedLionfish = RedLionfishDeconv.napari_plugin'}
+)
