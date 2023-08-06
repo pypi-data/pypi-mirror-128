c1c6bc31f9d64c6d2aaa9fbb52b4e5fe9b945d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:29 +0000

    Add package ftpfcs/0.0.1/ftpfcs-0.0.1-py3-none-any.whl

---
+++ packages/ftpfcs/ftpfcs-0.0.1-py3-none-any.whl/ftpfcs/__metadata__/README.md
@@ -0,0 +1,24 @@
+# FTP Forward Chaining Server
+
+<badges>[![version](https://img.shields.io/pypi/v/ftpfcs.svg)](https://pypi.org/project/ftpfcs/)
+[![license](https://img.shields.io/pypi/l/ftpfcs.svg)](https://pypi.org/project/ftpfcs/)
+[![pyversions](https://img.shields.io/pypi/pyversions/ftpfcs.svg)](https://pypi.org/project/ftpfcs/)  
+[![powered](https://img.shields.io/badge/Say-Thanks-ddddff.svg)](https://saythanks.io/to/foxe6)
+[![donate](https://img.shields.io/badge/Donate-Paypal-0070ba.svg)](https://paypal.me/foxe6)
+[![made](https://img.shields.io/badge/Made%20with-PyCharm-red.svg)](https://paypal.me/foxe6)
+</badges>
+
+<i>Basically a FTP based MITM proxy that works as SFTP tunneling without SSH or an actual proxy to complete FTP traffic forward/relay. FTPFCS can be stacked to perform chaining using traditional FTP commands.</i>
+
+# Hierarchy
+
+```
+ftpfcs
+|---- FTPFCSS() # see http://pyftpdlib.readthedocs.io/ for class structure
+'---- FTPFCSESS() # also see test for detailed example
+```
+
+# Example
+
+## python
+See `test`.
---
+++ packages/ftpfcs/ftpfcs-0.0.1-py3-none-any.whl/ftpfcs/utils.py
@@ -0,0 +1,78 @@
+from omnitools import FTPESS, FTPS
+import threading
+import time
+import os
+
+
+class FTPRelayFO:
+    def __init__(self):
+        self.buffer: list = []
+        self.name: str = None
+        self.closed: bool = False
+        self.timeout: int = 5
+        self.parts: int = 0
+        self.length: int = 0
+
+    def read(self, n: int):
+        tried = 0
+        while True:
+            try:
+                r = None
+                try:
+                    r= self.buffer.pop(0)
+                    return r
+                except:
+                    raise
+                finally:
+                    pass
+            except IndexError:
+                if self.closed:
+                    return b""
+                if tried < self.timeout:
+                    tried += 1
+                    time.sleep(1)
+
+    def write(self, s: bytes):
+        self.buffer.append(s)
+        self.parts += 1
+        self.length += len(s)
+
+    def fileno(self):
+        raise AttributeError("do not use sendfile()")
+
+    def close(self):
+        self.closed = True
+
+
+def fake_remote_server_setup(server_type, credentials):
+    if not credentials:
+        credentials = ["foxe6"]*2
+    port = 8022
+    users = credentials+["elradfmwMT"]
+    remote_homebase = os.path.abspath(r"s2")
+    s2 = server_type(port=port)
+    s2.server.max_cons = 10
+    s2.server.max_cons_per_ip = 10
+    homedir = os.path.join(remote_homebase, users[0])
+    os.makedirs(homedir, exist_ok=True)
+    s2.handler.authorizer.add_user(*users[:2], homedir, users[-1])
+    if issubclass(server_type, FTPESS):
+        s2.handler.certfile = os.path.join(remote_homebase, "foxe2.pem")
+        s2.handler.tls_control_required = True
+        s2.handler.tls_data_required = True
+    s2.handler.passive_ports = range(51200, 52200)
+    s2.configure()
+    p2 = threading.Thread(target=s2.start)
+    p2.daemon = True
+    p2.start()
+    return ["127.0.0.1", port], credentials
+
+
+def fake_ftpes_remote_server_setup(credentials: list = None):
+    return fake_remote_server_setup(FTPESS, credentials)
+
+
+def fake_ftps_remote_server_setup(credentials: list = None):
+    return fake_remote_server_setup(FTPS, credentials)
+
+
