8983c343f4ebf63a9a4ae56a7d8454c4c14945cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:09 +0000

    Add package felya8/0.1.0/felya8-0.1.0.tar.gz

---
+++ packages/felya8/felya8-0.1.0.tar.gz/felya8-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='felya8',
+    version='0.1.0',    
+    license='BSD 2-clause',
+    packages=['test'],
+    install_requires=['numpy<1.17.3',
+                      'pandas',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
