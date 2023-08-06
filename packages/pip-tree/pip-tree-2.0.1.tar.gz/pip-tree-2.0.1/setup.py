f6edf439babd9c7a6f5fe75358973b41e6a8dcc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:38 +0000

    Add package pip-tree/2.0.1/pip-tree-2.0.1.tar.gz

---
+++ packages/pip-tree/pip-tree-2.0.1.tar.gz/pip-tree-2.0.1/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+DEV_REQUIREMENTS = [
+    'black',
+    'coveralls == 3.*',
+    'flake8',
+    'isort',
+    'pytest == 6.*',
+    'pytest-cov == 2.*',
+]
+
+setuptools.setup(
+    name='pip-tree',
+    version='2.0.1',
+    description='Get the dependency tree of your Python virtual environment via Pip.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/pip-tree',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    entry_points={
+        'console_scripts': [
+            'pip-tree=pip_tree.cli:main',
+        ],
+    },
+    python_requires='>=3.7, <4',
+)
