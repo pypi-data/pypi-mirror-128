015a8783635d8405bca1341d526cd96949526651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:34 +0000

    Add package spytec02/0.0.2/spytec02-0.0.2.tar.gz

---
+++ packages/spytec02/spytec02-0.0.2.tar.gz/spytec02-0.0.2/spytec02/__init__.py
@@ -0,0 +1,11 @@
+from .operation import add
+from .operation import subt
+from .operation import mult
+from .operation import div
+from .operation import rem
+from .operation import pow
+from .operation import sq_rt
+from .operation import gif
+from .operation import s2
+
+
