fa5ba24861b33584e8081e1c02853ba9177732cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:57 +0000

    Add package pycvcqv/0.1.1/pycvcqv-0.1.1-py3-none-any.whl

---
+++ packages/pycvcqv/pycvcqv-0.1.1-py3-none-any.whl/pycvcqv/__init__.py
@@ -0,0 +1,30 @@
+# type: ignore[attr-defined]
+"""Coefficient of Variation (CV) and Coefficient of Quartile Variation (CQV)."""
+
+from importlib import metadata as importlib_metadata
+
+from pycvcqv.cqv import cqv
+from pycvcqv.cv import coefficient_of_variation
+from pycvcqv.dataframe import processor_cqv, processor_cv
+from pycvcqv.is_numeric import is_numeric
+
+# -------------------------------- function definition --------------------------------
+
+
+def get_version() -> str:
+    """Gets the version of package."""
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
+
+__all__ = [
+    "coefficient_of_variation",
+    "is_numeric",
+    "cqv",
+    "processor_cv",
+    "processor_cqv",
+]
