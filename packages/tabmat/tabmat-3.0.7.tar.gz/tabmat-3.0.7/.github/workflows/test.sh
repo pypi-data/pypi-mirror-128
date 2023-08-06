0cd026653ab4d2a7c4c3d3481a80a085556debc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:34 +0000

    Add package tabmat/3.0.7/tabmat-3.0.7.tar.gz

---
+++ packages/tabmat/tabmat-3.0.7.tar.gz/tabmat-3.0.7/.github/workflows/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -exo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${SCRIPT_DIR}/base.sh $*
+
+mamba install -y yq
+yq -Y ". + {dependencies: [.dependencies[], \"python=${PYTHON_VERSION}\"] }" environment.yml > /tmp/environment.yml
+mamba env create -f /tmp/environment.yml
+conda activate $(yq -r .name environment.yml)
+pip install --no-use-pep517 --no-deps --disable-pip-version-check -e .
+pytest -nauto tests --doctest-modules src/
+
+# TODO: Add sphinx documentation
+# pushd $(pwd)/docs
+# make html
+# popd
