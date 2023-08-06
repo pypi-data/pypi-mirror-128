c2113c4a52aab967af2de02f4cf266240f61a7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:40 +0000

    Add package fivempy/0.2/fivempy-0.2.tar.gz

---
+++ packages/fivempy/fivempy-0.2.tar.gz/fivempy-0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fivempy",
+    version="0.2",
+    author="itasli",
+    author_email="ilyas.tasli@outlook.com",
+    description="A python wrapper for FiveM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itasli/fivempy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
