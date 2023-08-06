6c063c62d9b89e88e50114709f2c7b854f7374e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:50 +0000

    Add package sphinxcontrib.kana-text/0.30.1/sphinxcontrib.kana_text-0.30.1.tar.gz

---
+++ packages/sphinxcontrib.kana-text/sphinxcontrib.kana_text-0.30.1.tar.gz/sphinxcontrib.kana_text-0.30.1/setup.py
@@ -0,0 +1,17 @@
+import sys
+from setuptools import setup
+
+import src as kt
+
+setup(
+    version = kt.__version__,
+    license = kt.__license__,
+    author = kt.__author__,
+    url = kt.__url__,
+    download_url = 'https://pypi.org/project/sphinxcontrib.kana-text/',
+    project_urls = {
+        'Knowledge': 'https://qiita.com/tags/sphinxcotrib.kana_text',
+        'Code': 'https://github.com/KaKkouo/kana_text',
+        'Issue tracker': 'https://github.com/KaKkouo/kana_text/issues',
+    }
+)
