73a180ad44e7b20200a86c0e3c81439d029c0123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:12 +0000

    Add package pytest-homeassistant-custom-component/0.4.12/pytest-homeassistant-custom-component-0.4.12.tar.gz

---
+++ packages/pytest-homeassistant-custom-component/pytest-homeassistant-custom-component-0.4.12.tar.gz/pytest-homeassistant-custom-component-0.4.12/pytest_homeassistant_custom_component/const.py
@@ -0,0 +1,14 @@
+"""
+Constants used by Home Assistant components.
+
+This file is originally from homeassistant/core and modified by pytest-homeassistant-custom-component.
+"""
+from __future__ import annotations
+
+from typing import Final
+
+MAJOR_VERSION: Final = 2021
+MINOR_VERSION: Final = 11
+PATCH_VERSION: Final = "5"
+__short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
+__version__: Final = f"{__short_version__}.{PATCH_VERSION}"
