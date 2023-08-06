b65aafd52a2893316aa45124f6651355c78974c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:47 +0000

    Add package x-python/1.5.0/x_python-1.5.0-py37-none-any.whl

---
+++ packages/x-python/x_python-1.5.0-py37-none-any.whl/xpython/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Copyright (C) 2020-2021 Rocky Bernstein
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+See the documentation for the full license.
+"""
+
+__docformat__ = "restructuredtext"
+
+from xpython.byteop import get_byteop
+
+from xpython.pyobj import (
+    Function,
+    Method,
+    Cell,
+    Traceback,
+    traceback_from_frame,
+    Generator,
+)
+from xpython.vm import PyVM, PyVMError, PyVMRuntimeError
+
+from xpython.vmtrace import PyVMTraced, pretty_event_flags
+
+from xpython.version import __version__  # noqa
