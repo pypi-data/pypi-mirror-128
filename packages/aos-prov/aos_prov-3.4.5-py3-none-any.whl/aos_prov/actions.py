1d47b6ad5482dd37380475b31eac96d4387293f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:05 +0000

    Add package aos-prov/3.4.5/aos_prov-3.4.5-py3-none-any.whl

---
+++ packages/aos-prov/aos_prov-3.4.5-py3-none-any.whl/aos_prov/actions.py
@@ -0,0 +1,23 @@
+#
+#  Copyright (c) 2018-2021 Renesas Inc.
+#  Copyright (c) 2018-2021 EPAM Systems Inc.
+#
+import time
+
+from aos_prov.command_provision import run_provision
+from aos_prov.command_vm import new_vm, start_vm, delete_provisioning_ports
+from aos_prov.utils.user_credentials import UserCredentials
+
+from command_vm import delete_provisioning_ports
+from communication.cloud.cloud_api import CloudAPI
+
+
+def create_new_unit(vm_name):
+    uc = UserCredentials(cert_file_path=None, key_file_path=None, pkcs12=args.pkcs)
+    cloud_api = CloudAPI(uc)
+    cloud_api.check_cloud_access()
+    vm_port = new_vm(vm_name)
+    start_vm(vm_name)
+    time.sleep(10)
+    run_provision(f'127.0.0.1:{vm_port}', cloud_api, reconnect_times=20)
+    delete_provisioning_ports(vm_name)
