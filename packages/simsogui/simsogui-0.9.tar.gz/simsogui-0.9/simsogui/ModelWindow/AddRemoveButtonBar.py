153e7dac34a0c9d57a2587944f906085d76eca94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:33 +0000

    Add package simsogui/0.9/simsogui-0.9.tar.gz

---
+++ packages/simsogui/simsogui-0.9.tar.gz/simsogui-0.9/simsogui/ModelWindow/AddRemoveButtonBar.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QPushButton, QSizePolicy, QSpacerItem, QStyle, QWidget
+
+
+class AddRemoveButtonBar(QWidget):
+    def __init__(self, parent, remove_text, remove_method, add_text,
+                 add_method):
+        QWidget.__init__(self, parent)
+        layout = QHBoxLayout()
+        remove_button = QPushButton(remove_text, self)
+        remove_button.setIcon(QApplication.style().standardIcon(QStyle.SP_TrashIcon))
+        remove_button.clicked.connect(remove_method)
+        layout.addWidget(remove_button)
+        layout.setContentsMargins(8, 0, 8, 0)
+
+        horizontal_spacer = QSpacerItem(50, 20, QSizePolicy.Expanding,
+                                        QSizePolicy.Minimum)
+        layout.addItem(horizontal_spacer)
+
+        add_button = QPushButton(add_text, self)
+        add_button.clicked.connect(add_method)
+        layout.addWidget(add_button)
+        self.setLayout(layout)
