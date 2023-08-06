9e3c4ef2a28709c9ce7c00bd02a0b22c5e25cb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:20 +0000

    Add package Domain-Extract/1.0.5/Domain-Extract-1.0.5.tar.gz

---
+++ packages/Domain-Extract/Domain-Extract-1.0.5.tar.gz/Domain-Extract-1.0.5/README.md
@@ -0,0 +1,60 @@
+```
+Made with Python3
+(C) @FayasNoushad
+Copyright permission under MIT License
+License -> https://github.com/FayasNoushad/Domain-Extract/blob/main/LICENSE
+```
+
+---
+
+## Installation
+
+```
+pip install Domain-Extract
+```
+
+---
+
+## Usage
+
+```py
+import domain_extract
+```
+
+### Extract domain from url
+
+```py
+url = "https://fayas.me/"
+
+print(domain_extract.domain(url))
+# => fayas.me
+```
+
+### Extract domains from urls list
+
+```py
+urls = [
+    "https://fayas.me",
+    "https://github.com/FayasNoushad"
+]
+
+print(domain_extract.domains(urls))
+# => ["fayas.me", "github.com"]
+```
+
+### Extract domains from a string
+
+```py
+string = "Hello [Fayas](https://fayas.me/)"
+
+print(domain_extract.string_domains(string))
+# => ["fayas.me"]
+```
+
+---
+
+## Credits
+
+- [Fayas Noushad](https://github.com/FayasNoushad)
+
+---
---
+++ packages/Domain-Extract/Domain-Extract-1.0.5.tar.gz/Domain-Extract-1.0.5/setup.py
@@ -0,0 +1,48 @@
+import os
+import setuptools
+
+
+def requirements(file="requirements.txt"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf-8") as r:
+            return [i.strip() for i in r]
+    else:
+        return []
+
+
+def readme(file="README.md"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf8") as r:
+            return r.read()
+    else:
+        return ""
+
+
+setuptools.setup(
+    name="Domain-Extract",
+    version="1.0.5",
+    description="Domain extract from string",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/FayasNoushad/",
+    download_url="https://github.com/FayasNoushad/Domain-Extract/releases/latest",
+    license="MIT",
+    author="Fayas Noushad",
+    author_email="contact@fayas.me",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    project_urls={
+        "Tracker": "https://github.com/FayasNoushad/Domain-Extract/issues",
+        "Source": "https://github.com/FayasNoushad/Domain-Extract",
+        "Documentation": "https://domain-extract.projects.fayas.me",
+    },
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=requirements()
+)
