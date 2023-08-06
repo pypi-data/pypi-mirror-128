3cce7b2e3c59f155b574304b38b844d80c9ed882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:00 +0000

    Add package imio.smartweb.common/1.0a4/imio.smartweb.common-1.0a4.tar.gz

---
+++ packages/imio.smartweb.common/imio.smartweb.common-1.0a4.tar.gz/imio.smartweb.common-1.0a4/CHANGES.rst
@@ -0,0 +1,31 @@
+Changelog
+=========
+
+
+1.0a4 (2021-11-23)
+------------------
+
+- Add utility to get a vocabulary
+  [boulch]
+
+
+1.0a3 (2021-11-16)
+------------------
+
+- Avoid traceback if configure_faceted is called on non-configured type (ex: on
+  default collections at Plone install)
+  [laulaz]
+
+
+1.0a2 (2021-11-05)
+------------------
+
+- Fix setup.py classifiers & URLs
+  [laulaz]
+
+
+1.0a1 (2021-11-05)
+------------------
+
+- Initial release.
+  [boulch]
---
+++ packages/imio.smartweb.common/imio.smartweb.common-1.0a4.tar.gz/imio.smartweb.common-1.0a4/src/imio/smartweb/common/utils.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from plone.formwidget.geolocation.geolocation import Geolocation
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+
+import geopy
+
+
+def get_vocabulary(voc_name):
+    factory = getUtility(IVocabularyFactory, voc_name)
+    vocabulary = factory(api.portal.get())
+    return vocabulary
+
+
+def get_term_from_vocabulary(vocabulary, value):
+    portal = api.portal.get()
+    factory = getUtility(IVocabularyFactory, vocabulary)
+    vocabulary = factory(portal)
+    try:
+        term = vocabulary.getTerm(value)
+    except LookupError:
+        return SimpleTerm(value=value, title=value)
+    return term
+
+
+def translate_vocabulary_term(vocabulary, term):
+    if term is None:
+        return ""
+    portal = api.portal.get()
+    factory = getUtility(IVocabularyFactory, vocabulary)
+    vocabulary = factory(portal)
+    term = vocabulary.getTerm(term)
+    return term and term.title or ""
+
+
+def geocode_object(obj):
+    street_parts = [
+        obj.number and str(obj.number) or "",
+        obj.street,
+        obj.complement,
+    ]
+    street = " ".join(filter(None, street_parts))
+    entity_parts = [
+        obj.zipcode and str(obj.zipcode) or "",
+        obj.city,
+    ]
+    entity = " ".join(filter(None, entity_parts))
+    country = translate_vocabulary_term(
+        "imio.smartweb.vocabulary.Countries", obj.country
+    )
+    address = " ".join(filter(None, [street, entity, country]))
+    if not address:
+        return
+    geolocator = geopy.geocoders.Nominatim(user_agent="contact@imio.be", timeout=3)
+    location = geolocator.geocode(address)
+    if location:
+        obj.geolocation = Geolocation(
+            latitude=location.latitude, longitude=location.longitude
+        )
+        obj.reindexObject(idxs=["longitude", "latitude"])
+        return True
+    return False
