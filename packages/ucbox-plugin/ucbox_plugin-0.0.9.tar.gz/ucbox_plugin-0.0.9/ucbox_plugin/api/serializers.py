cdc937de414d1f21d358c03656805bd7f77e3180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package ucbox-plugin/0.0.9/ucbox_plugin-0.0.9.tar.gz

---
+++ packages/ucbox-plugin/ucbox_plugin-0.0.9.tar.gz/ucbox_plugin-0.0.9/ucbox_plugin/api/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+from ..models import Number, Trunk, Cluster, DevicePool
+from tenancy.api.nested_serializers import NestedTenantSerializer
+from dcim.api.nested_serializers import NestedRegionSerializer
+from circuits.api.nested_serializers import NestedProviderSerializer
+from extras.api.serializers import TagSerializer
+from .nested_serializers import NestedNumberSerializer, NestedTrunkSerializer, NestedClusterSerializer, NestedDevicePoolSerializer
+from extras.api.nested_serializers import NestedTagSerializer
+
+
+class NumberSerializer(TagSerializer, serializers.ModelSerializer):
+
+    label = serializers.CharField(source='number', read_only=True)
+    tenant = NestedTenantSerializer(required=True, allow_null=False)
+    region = NestedRegionSerializer(required=False, allow_null=True)
+    provider = NestedProviderSerializer(required=False, allow_null=True)
+    forward_to = NestedNumberSerializer(required=False, allow_null=True)
+    tags = NestedTagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Number
+        fields = [
+            "id", "label", "number", "tenant", "region", "forward_to", "description", "provider", "tags",
+        ]
+
+class TrunkSerializer(TagSerializer, serializers.ModelSerializer):
+
+    label = serializers.CharField(source='trunk', read_only=True)
+    tenant = NestedTenantSerializer(required=True, allow_null=False)
+    region = NestedRegionSerializer(required=False, allow_null=True)
+    provider = NestedProviderSerializer(required=False, allow_null=True)
+    tags = NestedTagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Trunk
+        fields = [
+            "id", "label", "trunk", "tenant", "region", "destination", "description", "provider", "tags",
+        ]
+
+class ClusterSerializer(TagSerializer, serializers.ModelSerializer):
+
+    label = serializers.CharField(source='cluster', read_only=True)
+    tenant = NestedTenantSerializer(required=True, allow_null=False)
+    tags = NestedTagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Cluster
+        fields = [
+            "id", "label", "cluster", "tenant", "publisher", "subscriber1", "subscriber2", "subscriber3", "subscriber4", "tftp1", "tftp2", "description", "tags"
+        ]
+
+class DevicePoolSerializer(TagSerializer, serializers.ModelSerializer):
+
+    label = serializers.CharField(source='devicepool', read_only=True)
+    tenant = NestedTenantSerializer(required=True, allow_null=False)
+    region = NestedRegionSerializer(required=False, allow_null=True)
+    tags = NestedTagSerializer(many=True, required=False)
+
+    class Meta:
+        model = DevicePool
+        fields = [
+            "id", "label", "devicepool", "tenant", "region", "description", "datetimegroup", "localroutegroup", "tags",
+        ]
---
+++ packages/ucbox-plugin/ucbox_plugin-0.0.9.tar.gz/ucbox_plugin-0.0.9/ucbox_plugin/navigation.py
@@ -0,0 +1,24 @@
+from extras.plugins import PluginMenuItem
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:number_list_view',
+        link_text='Numbers',
+        buttons=()
+    ),
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:trunk_list_view',
+        link_text='Trunks',
+        buttons=()
+    ),
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:cluster_list_view',
+        link_text='Clusters',
+        buttons=()
+    ),
+    PluginMenuItem(
+        link='plugins:ucbox_plugin:devicepool_list_view',
+        link_text='Device Pools',
+        buttons=()
+    ),
+)
