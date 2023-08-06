91cfcd18ce97ff73df9869a0462ef75655452a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:32 +0000

    Add package juspreet51/0.0.2/juspreet51-0.0.2.tar.gz

---
+++ packages/juspreet51/juspreet51-0.0.2.tar.gz/juspreet51-0.0.2/README.md
@@ -0,0 +1,3 @@
+# juspreet51
+### Project juspreet51 has been renamed and moved to JusPy
+Please visit [JusPy](https://pypi.org/project/juspy/) for more information
---
+++ packages/juspreet51/juspreet51-0.0.2.tar.gz/juspreet51-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="juspreet51",
+    version="0.0.2",
+    author="Jaspreet Singh",
+    description="use JusPy instead",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/juspy/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    install_requires = [],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
