9c585be0621ac8390735d1a67a703ee0d4e3c725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:30 +0000

    Add package fabdb-sdk/0.0.1/fabdb_sdk-0.0.1.tar.gz

---
+++ packages/fabdb-sdk/fabdb_sdk-0.0.1.tar.gz/fabdb_sdk-0.0.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fabdb_sdk',
+ 'fabdb_sdk.enums',
+ 'fabdb_sdk.exceptions',
+ 'fabdb_sdk.helpers',
+ 'fabdb_sdk.models',
+ 'fabdb_sdk.repositories']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'fabdb-sdk',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Rubem Mota',
+    'author_email': 'rubemmota89@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
