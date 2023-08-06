b823bd1ba9e68ea112d5b133c3173fd9c2080688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:02 +0000

    Add package chocs/1.4.2/chocs-1.4.2-py3-none-any.whl

---
+++ packages/chocs/chocs-1.4.2-py3-none-any.whl/chocs/serverless/wrapper.py
@@ -0,0 +1,24 @@
+from typing import Callable
+
+from chocs.http.http_request import HttpRequest
+from chocs.http.http_response import HttpResponse
+from chocs.middleware.middleware import MiddlewarePipeline
+from chocs.routing import Route
+from .aws import AwsServerlessFunction
+from .serverless import IS_AWS_ENVIRONMENT, ServerlessFunction
+from functools import update_wrapper
+
+
+def create_serverless_function(
+    func: Callable[[HttpRequest], HttpResponse],
+    route: Route,
+    middleware_pipeline: MiddlewarePipeline,
+) -> Callable:
+
+    if IS_AWS_ENVIRONMENT:
+        return update_wrapper(AwsServerlessFunction(func, route, middleware_pipeline), func)
+
+    return update_wrapper(ServerlessFunction(func, route, middleware_pipeline), func)
+
+
+__all__ = ["create_serverless_function"]
