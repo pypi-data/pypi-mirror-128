4137f9420333ba60da417c0f0d58091057620ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:12 +0000

    Add package cmdMenuFramework/1.0.0/cmdMenuFramework-1.0.0.tar.gz

---
+++ packages/cmdMenuFramework/cmdMenuFramework-1.0.0.tar.gz/cmdMenuFramework-1.0.0/cmdMenuFramework/Context.py
@@ -0,0 +1,34 @@
+
+from cmd_menu_framework.Path import Path
+
+class Context:
+    
+    def __init__(self, file = 'yaml/index.yaml'):
+        # path is the list of instructions leading to the one executing currently.
+        self.path = Path()
+        
+        # Default menu vars
+        self.file = file
+        self.controllerFile = 'controllers/ControllerIndex.py'
+        self.controllerClass = 'ControllerIndex'
+        self.input = None
+        self.output = None
+        self.userChoice = None
+        self.exit = False
+        self.exception = None
+        self.finish = False
+        self.choicePrompt = '%#. %$' 
+        self.inputPrompt = 'Your input > '
+        self.indent = False
+        self.availableChoices = []
+        self.showTrace = True
+        self.indentWithPipes = False
+        
+        # User menu vars: 
+        self.vars = dict()
+        
+        
+    
+    
+    
+    
---
+++ packages/cmdMenuFramework/cmdMenuFramework-1.0.0.tar.gz/cmdMenuFramework-1.0.0/cmdMenuFramework/__init__.py
@@ -0,0 +1,11 @@
+
+from Menu import Menu
+
+
+def startMenu(fileName : str):
+    """
+    Launches the menu that is described by the given yaml file, in the cmd prompt. 
+    If the file is not found, throws an exception, and terminates.
+    """
+    menu = Menu(fileName)
+    menu.start()
