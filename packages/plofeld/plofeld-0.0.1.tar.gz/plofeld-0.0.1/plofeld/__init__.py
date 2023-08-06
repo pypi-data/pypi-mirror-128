8aef819af1a1d9c7ee9ce0e847b49b6608c08d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:49 +0000

    Add package plofeld/0.0.1/plofeld-0.0.1.tar.gz

---
+++ packages/plofeld/plofeld-0.0.1.tar.gz/plofeld-0.0.1/plofeld/__init__.py
@@ -0,0 +1,7 @@
+__title__ = "plofeld"
+__author__ = "JoschD"
+__description__ = "Plotting functions for physical fields."
+__url__ = "https://github.com/JoschD/plofeld"
+__version__ = "0.0.1"
+__author_email__ = "JoschD@github.com"
+__license__ = "MIT"
