8a8133928804b9aff6ae965a98d1e13835cdd02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:03 +0000

    Add package BotSAmino/1.0.4/BotSAmino-1.0.4.tar.gz

---
+++ packages/BotSAmino/BotSAmino-1.0.4.tar.gz/BotSAmino-1.0.4/BotSAmino/__init__.py
@@ -0,0 +1,14 @@
+from .client import BotAminew
+from .local import Local
+from .socket import *
+from .acm import Acm
+from .lib import *
+from requests import get
+version = '1.0.4'
+
+try:
+    newest = get("https://pypi.python.org/pypi/BotSAmino/json").json()["info"]["version"]
+    if version != newest: print(f"\033[1;33mBotAminew New Version!: {newest} (Your Using {version})\033[1;0m")
+except: pass
+
+__version__ = version
