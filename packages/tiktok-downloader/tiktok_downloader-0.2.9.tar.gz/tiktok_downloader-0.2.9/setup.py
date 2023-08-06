5e8972d9a046cbff3a102385447ff5982ede1d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:17 +0000

    Add package tiktok-downloader/0.2.9/tiktok_downloader-0.2.9.tar.gz

---
+++ packages/tiktok-downloader/tiktok_downloader-0.2.9.tar.gz/tiktok_downloader-0.2.9/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup
+from os import path
+base_dir = path.abspath(path.dirname(__file__))
+setup(
+  name = 'tiktok_downloader',        
+  packages = ['tiktok_downloader'],
+  include_package_data=True, 
+  version = '0.2.9',    
+  license='MIT',     
+  description = 'Tiktok Downloader&Scraper using bs4&requests', 
+  author = 'Krypton Byte',                  
+  author_email = 'galaxyvplus6434@gmail.com',     
+  url = 'https://github.com/krypton-byte/tiktok_downloader',   
+  download_url = 'https://codeload.github.com/krypton-byte/tiktok-downloader/tar.gz/refs/tags/0.2.9',    
+  keywords = ['tiktok', 'downloader', 'scrapper', 'tikdok-scraper', 'tiktok-downloader'], 
+  install_requires=[           
+          'bs4',
+          'flask',
+          'cloudscraper',
+          'requests',
+          'faker',
+          'py-mini-racer'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
