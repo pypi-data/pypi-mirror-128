3f9bebfb241d4f98252d158616dc591a778397f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:32 +0000

    Add package memrise/1.2.1/memrise-1.2.1-py3-none-any.whl

---
+++ packages/memrise/memrise-1.2.1-py3-none-any.whl/memrise/__main__.py
@@ -0,0 +1,11 @@
+from .__init__ import __version__
+
+def main():
+    print("""Library Memrise Scraping - Author: Joseph Quang""")
+    print(f"Package: {__package__}")
+    print("Github: https://github.com/tquangsdh20/memrise")
+    print(f"Version: {__version__}")
+
+
+if __name__ == main():
+    main()
