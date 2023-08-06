223f608f0c5be718513c9f1608b47d0221d7ccde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:57 +0000

    Add package YTThumb/1.3.9/YTThumb-1.3.9.tar.gz

---
+++ packages/YTThumb/YTThumb-1.3.9.tar.gz/YTThumb-1.3.9/setup.py
@@ -0,0 +1,48 @@
+import os
+import setuptools
+
+
+def requirements(file="requirements.txt"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf-8") as r:
+            return [i.strip() for i in r]
+    else:
+        return []
+
+
+def readme(file="README.md"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf8") as r:
+            return r.read()
+    else:
+        return ""
+
+
+setuptools.setup(
+    name="YTThumb",
+    version="1.3.9",
+    description="YouTube video thumbnail download",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/FayasNoushad/Youtube-Video-Thumbnail",
+    download_url="https://github.com/FayasNoushad/Youtube-Video-Thumbnail/releases/latest",
+    license="MIT",
+    author="Fayas Noushad",
+    author_email="contact@fayas.me",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    project_urls={
+        "Tracker": "https://github.com/FayasNoushad/Youtube-Video-Thumbnail/issues",
+        "Source": "https://github.com/FayasNoushad/Youtube-Video-Thumbnail",
+        "Documentation": "https://ytthumb.projects.fayas.me",
+    },
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=requirements()
+)
