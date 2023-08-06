6d6c37829fe9a993f94191518c9dc28e57be8a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:38 +0000

    Add package MainWindow-Appliction2/2.1/MainWindow_Appliction2-2.1.tar.gz

---
+++ packages/MainWindow-Appliction2/MainWindow_Appliction2-2.1.tar.gz/MainWindow_Appliction2-2.1/setup.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import MainWindow_Appliction2
+
+setup(
+    name="MainWindow_Appliction2",
+    version="2.1",
+    py_modules=["MainWindow_Appliction2", "mainWindow", "background_img_rc"],
+    classifiers=["Programming Language :: Python :: 3.6", "Topic :: Software Development :: Libraries :: Python Modules"],
+    author_email="545427702@qq.com",
+    license="MIT"
+      )
