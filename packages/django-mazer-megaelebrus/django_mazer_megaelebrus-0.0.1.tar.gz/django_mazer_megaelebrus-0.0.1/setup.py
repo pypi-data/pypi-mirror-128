b0ff512c964933e11664c16171a59e362d2724dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:20 +0000

    Add package django-mazer-megaelebrus/0.0.1/django_mazer_megaelebrus-0.0.1.tar.gz

---
+++ packages/django-mazer-megaelebrus/django_mazer_megaelebrus-0.0.1.tar.gz/django_mazer_megaelebrus-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="django_mazer_megaelebrus",
+	version="0.0.1",
+	author="Nikita Marchenko",
+	author_email="megaelebrus@gmail.com",
+	description="Django Template Mazer",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/ElebrUS/DjangoMazer",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3.9",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
