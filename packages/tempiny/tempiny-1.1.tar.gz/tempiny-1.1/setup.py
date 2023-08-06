1ee5f7df67b92f5e120c5fcf74ee9ffa112f3519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:32 +0000

    Add package tempiny/1.1/tempiny-1.1.tar.gz

---
+++ packages/tempiny/tempiny-1.1.tar.gz/tempiny-1.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import setup, find_packages
+
+
+def read_file(name):
+    """
+    Read file content
+    """
+    f = open(name)
+    try:
+        return f.read()
+    except IOError:
+        print("could not read %r" % name)
+        f.close()
+
+LONG_DESC = read_file('README.md') + '\n\n' + read_file('HISTORY.md')
+
+EXTRAS = {}
+
+if sys.version_info < (3,):
+    EXTRAS['use_2to3'] = True
+
+setup(
+    name='tempiny',
+    version='1.1',
+    description='Tiny template engine',
+    long_description=LONG_DESC,
+    long_description_content_type='text/markdown',
+    author='Léo Flaventin Hauchecorne',
+    author_email='hl037.prog@gmail.com',
+    url='https://github.com/hl037/tempiny',
+    license='LGPLv2.1+',
+    packages=find_packages(),
+    test_suite=None,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    extras_require=None,
+    entry_points=None,
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Other Scripting Engines',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ],
+    **EXTRAS
+)
