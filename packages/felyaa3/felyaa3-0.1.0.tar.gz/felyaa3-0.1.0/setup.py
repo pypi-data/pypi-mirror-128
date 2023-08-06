6578f5852a04f7b36f13ef5c3a007e7a7c8dd894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:40 +0000

    Add package felyaa3/0.1.0/felyaa3-0.1.0.tar.gz

---
+++ packages/felyaa3/felyaa3-0.1.0.tar.gz/felyaa3-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='felyaa3',
+    version='0.1.0',
+    packages=['first'],
+    install_requires=['cycler>0.10', 'matplotlib>3.4.0',],
+
+)
