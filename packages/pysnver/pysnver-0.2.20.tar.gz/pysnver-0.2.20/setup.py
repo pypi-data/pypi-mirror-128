d5f707e47d63b4bba16457646af88d9dedaa8a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:41 +0000

    Add package pysnver/0.2.20/pysnver-0.2.20.tar.gz

---
+++ packages/pysnver/pysnver-0.2.20.tar.gz/pysnver-0.2.20/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="pysnver",
+    version="0.2.20",
+    description="python sn verify",
+    packages=['pysnver'],
+    package_data={'pysnver': ['so/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
+
