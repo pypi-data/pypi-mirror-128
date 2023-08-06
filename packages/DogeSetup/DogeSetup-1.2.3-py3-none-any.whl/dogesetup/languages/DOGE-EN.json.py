7917d9de2c2d968c0d400a91b87a5f16bc90d4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:19 +0000

    Add package DogeSetup/1.2.3/DogeSetup-1.2.3-py3-none-any.whl

---
+++ packages/DogeSetup/DogeSetup-1.2.3-py3-none-any.whl/dogesetup/languages/DOGE-EN.json.py
@@ -0,0 +1,105 @@
+{
+    "__JSON__": true,
+    "LANGUAGE": "English",
+    "LANGCODE": "en",
+    "AUTHOR": "@Mutlcc",
+    "STRINGS": {
+
+        "HEROKU": {
+            "KEY": "Paste API of your Heroku account on https://dashboard.heroku.com/account",
+            "LOGINING": "I'm checking your Heroku API",
+            "INVALID_KEY": "Your Heroku API is invalid",
+            "RETRYING_KEY": "I'm starting the setup again",
+            "HEROKU_LOGGED": "I was successfully connected to Heroku",
+
+            "CREATING_APP": "I create app on your Heroku account",
+            "MOSTAPP": "Your Heroku account has too many apps and no space for me",
+            "FOR_MOSTAPP_SOLVE": "For app create;\n🚮 You need to delete some apps from your Heroku account,\n🔢 Or enter a different Heroku API Key",
+            "IF_SOLVE_TRY_AGAIN": "If you solved problem, restart the setup by pasting setup code again!\n🔹 If you don't have any ideas, write #herokudel in our @DogeSup Telegram group",
+            "SUCCESS_APP": "I have successfully created app",
+            "DELETING_APP": "I deleting your Heroku app",
+            "DELETED_APP": "I deleted your Heroku app",
+            "ERROR_DELETED_APP": "An error occurred while deleting your app",
+
+            "ERROR": "Christ no!\n😪 Something went wrong.\n🚩 Error",
+            "TRY_AGAIN_SETUP": "Please restart the setup by pasting setup code again",
+            "IF_SEE_ERROR": "If you saw this error again ask for help from our @DogeSup Telegram group",
+
+            "POSTGRE": "I'm integrating PostgreSQL database into Doge",
+            "SUCCESS_POSTGRE": "I have successfully set up the database",
+
+            "OPENING_DYNO": "I turn on the Dyno switch",
+            "ERROR_DYNO": "An error occurred while turning on the Dyno switch",
+            "OPENSELF_DYNO": "Open your Dyno switch from dashboard.heroku.com"
+        },
+
+        "MAIN": {
+            "SUCCESS_SESSION": "I've successfully logged in Telegram",
+
+            "GETTING_READY": "I'm getting ready to download repository",
+
+            "DOWNLOADING": "I'm downloading repository",
+
+            "DEPLOYING": "I'm deploying...\n\n📍 This process will take a maximum of 5 minutes",
+
+            "WRITING_CONFIG": "I'm syncing basic configuration data with your Heroku account",
+            "SUCCESS_CONFIG": "I have successfully completed the sync process",
+
+            "END": "YOU HAVE COMPLETED THE SETUP IN {} SECONDS",
+
+            "ASK_ALIVENAME": "If you want me to call you by a different name write Y",
+            "ASKING_ALIVENAME": "What name should I call you",
+            "SUCCESS_ALIVENAME": "WOW {} great, buddy",
+
+            "ASK_PMLOGGER": "Do you want me to automatically save PM and group-tagged messages?\n\n🔹 As long as I'm working, you can see deleted and edited messages with this feature.\n💠️ If you open this, I'll create a secret group.\n\n💭 Write Y for turn on",
+            "SUCCESS_PMLOGGER": "WOW dude you're a hacker now",
+
+            "ASK_DOGEPLUGIN": "Do you want to activate extra plugins?\n\n🔹 Animation, fun, etc. includes plugins.\n💠️ If you open this, I'll automatically install all the plugins from our @DogePlugin channel.\n\n💭 Write Y for turn on",
+            "SUCCESS_DOGEPLUGIN": "Now you have all the plugins from @DogePlugin",
+            "FALSE_DOGEPLUGIN": "You can reach extra plugins on our @DogePlugin channel",
+
+            "ASK_DOGEHUB": "Do you want to activate extra +18 obscene plugins?\n\n🔹 H*ntai, p*rn, etc. includes plugins.\n💠️ If you open this, I'll automatically install all the +18 plugins from our @DogeHUB channel.\n\n💭 Write Y for turn on",
+            "SUCCESS_DOGEHUB": "Now you have all the +18 plugins from @DogeHUB!\n\n😘 Use it carefully dude",
+            "FALSE_DOGEHUB": "You can reach extra +18 plugins on our @DogeHUB channel",
+
+            "SLOGAN": "Have a dog in Telegram!",
+            "SEEYOU": "Waiting for you in our @SohbetDoge group!\n\n⚡ You can check my status by writing .alive in any chat.\n\n🧶 I can also teach you how to use all the plugins I have with the .doge command.\n\n🐶 I can't wait to serve you!\n\n🐾 See you"
+        },
+
+        "SESSION": {
+            "IS_DOGE_RELIABLE": "Why do I want your phone number?\n\n🐶 Because I work with a session in your Telegram account.\n\n🐾 I value your data, would never;\nharm your data and account",
+            "SAMPLE_NUMBER": "Example: +445566778899",
+            "PHONE_NUMBER": "Write your phone number with the country code at the beginning, as in the example",
+
+            "CONNECTING": "I'm connecting to Telegram",
+            "CANT_CONNECT": "I couldn't connect to Telegram",
+
+            "WRITE_CODE_FROM_TG": "Write the code from Telegram",
+            "INVALID_CODE_MY": "You probably wrote the code wrong",
+            "TRY_AGAIN_SETUP": "Please restart the setup by pasting setup code again",
+
+            "NEW_APP": "It seems that you have never created an application before.\n⏳ I'm creating one right now",
+            "CANT_CREATE_APP": "Telegram has blocked me automatic app creation",
+            "CREATED": "Your app is ready for Telegram API ID & HASH",
+
+            "GETTING_API": "Getting API ID & HASH",
+            "INFOS": "You can note them if you want",
+
+            "SESSION_GENERATING": "I'm signing into your Telegram account",
+            "ERROR": "Christ no!\n😪 Something went wrong",
+            "ANY_ERROR_SOLVING": "I continue the process with a different method",
+            "RETRYING": "An error occurred while connecting!\n\n🔄 I'm trying again",
+
+            "ERROR_FW": "Your account has been restricted by Telegram for {} seconds for this process",
+            "TRY_AGAIN_FW": "Try again after {} seconds to continue processing",
+            "INVALID_CODE": "You wrote the code wrong",
+            "TRY_AGAIN": "Please try again",
+            "WARNING": "Trying too many times may result in your account being restricted",
+
+            "EXPIRED": "Looks like you're installing me for someone else.\n\n🪐 Tell s/he not to forward the code and tell s/he to send screenshot.\n\n🔁 But since this code is no longer valid, restart the setup by pasting setup code again",
+
+            "PASS": "Write your 2-step verification password",
+            "INVALID_2FA": "You wrote your 2-step password incorrectly"
+        }
+    }
+}
