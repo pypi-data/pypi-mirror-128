f443d4a7026ac10305d860ec59618b40e5153482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:40 +0000

    Add package odoo12-addon-somconnexio/12.0.1.7.5/odoo12_addon_somconnexio-12.0.1.7.5-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-somconnexio/odoo12_addon_somconnexio-12.0.1.7.5-py2.py3-none-any.whl/odoo/addons/somconnexio/tests/models/test_hr_attendance_process.py
@@ -0,0 +1,40 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestHrAttendance(TransactionCase):
+    """Test for presence validity"""
+
+    def setUp(self):
+        super(TestHrAttendance, self).setUp()
+        self.test_employee = self.browse_ref('hr.employee_al')
+        self.place = self.env['hr.attendance.place'].create({
+            'name': 'Home',
+            'code': 'HOME'
+        })
+        self.comments = "comments"
+
+    def test_employee_state_without_comments(self):
+        # Make sure the attendance of the employee will display correctly
+        assert self.test_employee.attendance_state == 'checked_out'
+        self.test_employee.attendance_action_change(self.place.code)
+        assert self.test_employee.attendance_state == 'checked_in'
+        self.assertEquals(self.test_employee.current_place, self.place)
+        last_attendance = self.env['hr.attendance'].search(
+            [], limit=1, order='check_in DESC'
+        )
+        self.assertEquals(last_attendance.comments, False)
+        self.test_employee.attendance_action_change()
+        assert self.test_employee.attendance_state == 'checked_out'
+
+    def test_employee_state_with_comments(self):
+        # Make sure the attendance of the employee will display correctly
+        assert self.test_employee.attendance_state == 'checked_out'
+        self.test_employee.attendance_action_change(self.place.code, self.comments)
+        assert self.test_employee.attendance_state == 'checked_in'
+        self.assertEquals(self.test_employee.current_place, self.place)
+        last_attendance = self.env['hr.attendance'].search(
+            [], limit=1, order='check_in DESC'
+        )
+        self.assertEquals(last_attendance.comments, self.comments)
+        self.test_employee.attendance_action_change()
+        assert self.test_employee.attendance_state == 'checked_out'
