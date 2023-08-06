df050ec6eea64663ccdf1214718820a218937a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:37 +0000

    Add package rubrix/0.6.1a0/rubrix-0.6.1a0.tar.gz

---
+++ packages/rubrix/rubrix-0.6.1a0.tar.gz/rubrix-0.6.1a0/frontend/components/text-classifier/results/ClassifierExplorationArea.vue
@@ -0,0 +1,53 @@
+<!--
+  - coding=utf-8
+  - Copyright 2021-present, the Recognai S.L. team.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+
+<template>
+  <div>
+    <LabelPill class="predictions" :predicted-as="predictedAs" :labels="showLabels" :show-score="true" />
+  </div>
+</template>
+<script>
+
+export default {
+  props: {
+    record: {
+      type: Object,
+      required: true,
+    }
+  },
+  data: () => ({
+    maxLabels: 16,
+  }),
+  computed: {
+    showLabels() {
+      if (this.labels.length > this.maxLabels) {
+        return this.labels.slice(0, this.maxLabels);
+      }
+      return this.labels;
+    },
+    labels() {
+      return this.record.prediction ? this.record.prediction.labels : [];
+    },
+    predictedAs() {
+      return this.record.predicted_as;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+// @import "@recognai/re-commons/src/assets/scss/components/tooltip.scss";
+</style>
