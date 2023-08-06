41166e0319d68119f8b75082a2c577664f67a80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:32 +0000

    Add package juspreet51/0.0.2/juspreet51-0.0.2-py3-none-any.whl

---
+++ packages/juspreet51/juspreet51-0.0.2-py3-none-any.whl/example_package/example.py
@@ -0,0 +1,4 @@
+# example.py is an example of a module within the package that could contain 
+# the logic (functions, classes, constants, etc.) of your package
+def add_one(number):
+    return number + 1
