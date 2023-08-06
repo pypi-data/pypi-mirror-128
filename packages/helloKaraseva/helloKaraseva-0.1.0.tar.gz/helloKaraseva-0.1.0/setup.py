a72345ce6941ef7713852eafd7ef684146cf34db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:26 +0000

    Add package helloKaraseva/0.1.0/helloKaraseva-0.1.0.tar.gz

---
+++ packages/helloKaraseva/helloKaraseva-0.1.0.tar.gz/helloKaraseva-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='helloKaraseva',
+    version='0.1.0',
+    description='Lab 2 Python package',
+    url='https://github.com/Karaseva-kote',
+    author='Karaseva',
+    author_email='karaseva@niuitmo.ru',
+    packages=['helloKaraseva'],
+    install_requires=['numpy>=1.17.3'],
+)
