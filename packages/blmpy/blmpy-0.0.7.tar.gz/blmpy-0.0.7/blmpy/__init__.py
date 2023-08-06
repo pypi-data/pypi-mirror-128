db34e8fe0e9e3f45b6cd641ee2ea829e02d08343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:22 +0000

    Add package blmpy/0.0.7/blmpy-0.0.7.tar.gz

---
+++ packages/blmpy/blmpy-0.0.7.tar.gz/blmpy-0.0.7/blmpy/__init__.py
@@ -0,0 +1,1 @@
+from .blmlib import blmjson
---
+++ packages/blmpy/blmpy-0.0.7.tar.gz/blmpy-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7' 
+DESCRIPTION = 'Bluemanoid Package'
+LONG_DESCRIPTION = 'Useful Python scripts for Bluemanoid workers'
+AUTHOR = 'Gautier Rio'
+AUTHOR_MAIL = 'gautier.rio@bluemanoid.com'
+
+# Setting up
+setup(
+    name="blmpy", 
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_MAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    
+    keywords=['python', 'Bluemanoid', 'BLM'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
