a92cf301b64fbbe715a27b8b4282138056a9bdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:58 +0000

    Add package ym2021-prj/0.0.5/ym2021_prj-0.0.5-py2.py3-none-any.whl

---
+++ packages/ym2021-prj/ym2021_prj-0.0.5-py2.py3-none-any.whl/ym2021_prj/cli.py
@@ -0,0 +1,20 @@
+"""
+Python package template with command-line interface
+"""
+
+import argparse
+from . import __version__
+
+
+def cli_args():
+    """Define & examine command-line arguments & options."""
+    _desc = f"{__package__}: " + __doc__.strip().splitlines()[0]
+    p = argparse.ArgumentParser(description=_desc)
+    p.add_argument("-v", "--version", action="version", version=__version__)
+    return p.parse_args()
+
+
+def main():
+    # simple function of this package, return a string
+    # args = cli_args()  # FIXME: fails in CI
+    return f"{__name__} {__version__ = } of {__file__ = }"
